14b424836d28cd74ee071c20a6e42d130fd5e20a
Haleshot <hari.leo03@gmail.com>
2023-01-23 21:49:19 +0530

    Updated

--- Codechef/ATM/ATM.cpp
+++ Codechef/ATM/ATM.cpp
@@ -1,22 +1,40 @@
 #include <iostream>
 using namespace std;
-#define MAX 100
+#define MAX 5
 
 int main()
 {
     int T = 0, N, K = 0;
+    int L[MAX], L_2[MAX];
     cin >> T;
     while(T--)
     {
         cin >> N >> K;
-        int L[MAX];
 
         for(int i = 0; i <= N; i++)
         {
-            
+            int element;
+            cin >> element;
+            L[i] = element;
+        }
+        for(int i = 0; i < N; i++)
+        {
+            if(L[i] <= K)
+            {
+                K -= i;
+                L_2[i] = 1;
+            }
+            else
+            {
+                L_2[i] = 0;
+            }
         }
 
 
+    }
+    for(int i = 0; i < N; i++)
+    {
+        cout << L_2[i];
     }
     return 0;
 }
