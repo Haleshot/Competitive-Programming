08131fad3bc7f106db2501c2b456f7979aef62f0
Haleshot <hari.leo03@gmail.com>
2023-01-23 21:31:49 +0530

    Updated

--- Codechef/ATM/ATM.cpp
+++ Codechef/ATM/ATM.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 using namespace std;
+#define MAX 100
 
 int main()
 {
-    int T = 0;
+    int T = 0, N, K = 0;
     cin >> T;
     while(T--)
     {
+        cin >> N >> K;
+        int L[MAX];
         
+
+
     }
     return 0;
 }
