0b94d03eacf5f78bf3c0d7b21ddefdfffd0a5ad0
Haleshot <hari.leo03@gmail.com>
2023-01-23 21:13:32 +0530

    Updated

--- Codechef/ATM/ATM.py
+++ Codechef/ATM/ATM.py
@@ -3,4 +3,10 @@ while T > 0:
     T -= 1
     N, K = map(int, input().split())
     l = list(map(int, input().split()))
-    
+    if len(l) == N:
+        for i in l:
+            if i <= K:
+                K -= i
+                print(1, end = "")
+            else:
+                print(0)
