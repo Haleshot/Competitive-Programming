9f55a28b5d8993cdef624bbf90cf8ce5cdebc372
Haleshot <hari.leo03@gmail.com>
2023-01-24 09:03:44 +0530

    Updated

--- Codechef/PROGLANG/PROGLANG.cpp
+++ Codechef/PROGLANG/PROGLANG.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-#define MAX 100
+#define MAX 2
 
 
 int main()
@@ -11,6 +11,16 @@ int main()
     {
         int A, B, A1, B1, A2, B2, l1[MAX], l2[MAX];
         cin >> A >> B >> A1 >> B1 >> A2 >> B2;
+
+        l1[0] = A1;
+        l1[1] = B1;
+
+        l2[0] = A2;
+        l2[1] = B2;
+
+        
+
+
 
 
     }
