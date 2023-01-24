a999bf0df3b1972b72aa29976957c2f0568476ca
Haleshot <hari.leo03@gmail.com>
2023-01-24 07:33:04 +0530

    Updated

---
+++ Codechef/FIT/FIT.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        int X, distance;
+        cin >> X;
+        distance = 10 * X;
+        cout << distance << endl;
+
+    }
+    
+    return 0;
+
+}
