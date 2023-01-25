4c854d187218f174800c4a1e665bf2019e1b96f2
Haleshot <hari.leo03@gmail.com>
2023-01-25 23:32:46 +0530

    Updated

--- Codechef/Remove_Cards/Remove_Cards.py
+++ Codechef/Remove_Cards/Remove_Cards.py
@@ -4,4 +4,13 @@ while T:
     N = int(input())
     l = list(map(int, input().split()))
 
+    freq_dict = {}
+    for i in range(l):
+        if l[i] in freq_dict.keys():
+            freq_dict[i] += 1
+        else:
+            freq_dict[i] = 1
+
+    highest_mode = max(freq_dict, key = lambda x : freq_dict[x])
     
+
