2fc03adeb7113e922ca26564d3628dd49bebd338
Haleshot <hari.leo03@gmail.com>
2023-01-26 20:59:05 +0530

    Updated

--- Codechef/EVENTUAL/EVENTUAL.cpp
+++ Codechef/EVENTUAL/EVENTUAL.cpp
@@ -1,20 +1,47 @@
 #include <iostream>
+#include <bits/stdc++.h>
 #include <string>
 using namespace std;
+
+int Count_Freq(int l[], int limit)
+{
+    unordered_map<int, int> cf;
+
+    for(int i = 0; i < limit; i ++)
+        {
+            cf[l[i]]++;
+        }
+
+        auto pr = max_element(cf.begin(), cf.end(), [](const auto &x, const auto &y) {
+                    return x.second < y.second;
+                });
+ 
+    return pr->first;
+
+
+}
 
 
 int main()
 {
 
-    int T, N;
-    string S = "";
+    int T;
     cin >> T;
-    cin >> N;
-    cin >> S;
-
-    if(S.length() == N)
+    while(T--)
     {
-        
+        int N;        
+        string S = "";
+        cin >> N;
+        cin >> S;
+
+        if(S.length() == N)
+        {
+
+
+        }
+
+
+
     }
 
 
