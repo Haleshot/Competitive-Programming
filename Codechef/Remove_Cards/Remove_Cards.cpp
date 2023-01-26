89d97cbd763bda10b2ea6b6eea5c722d8f178aed
Haleshot <hari.leo03@gmail.com>
2023-01-26 19:53:30 +0530

    Updated

--- Codechef/Remove_Cards/Remove_Cards.cpp
+++ Codechef/Remove_Cards/Remove_Cards.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 #define MAX 100
 using namespace std;
 
@@ -10,6 +11,7 @@ int main()
     {
         T -= 1;
         int N, *l;
+        vector<bool> visited(N, false);
         cin >> N;
         l = new int [N];
 
@@ -19,7 +21,23 @@ int main()
             cin >> a;
             l[i] = a;
         }
-        
+        for(int i = 0; i < N; i ++)
+        {
+            if(visited[i] = true)
+            {
+                continue;
+            }
+
+            int count = 1;
+            for (int j = i + 1; j < N; j++)
+            {
+                if(l[i] == l[j])
+                {
+                    visited[i] = true;
+                    count += 1;
+                }
+            }
+        }
 
 
     }
