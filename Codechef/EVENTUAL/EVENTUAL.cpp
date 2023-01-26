283448e02e9ee247dbd415f072c7a7a588197e73
Haleshot <hari.leo03@gmail.com>
2023-01-26 21:34:18 +0530

    Updated

--- Codechef/EVENTUAL/EVENTUAL.cpp
+++ Codechef/EVENTUAL/EVENTUAL.cpp
@@ -1,34 +1,38 @@
 #include <iostream>
-#include <cstring>
 #include <bits/stdc++.h>
 #include <string>
 using namespace std;
 
 int Count_Freq(string word, int limit)
 {
-    unordered_map<int, int> cf;
+    map<int, int> cf;
     int l[limit];
     char* char_array = new char[limit + 1];
     strcpy(char_array, word.c_str());
 
+     
+    for (int i = 0; i < limit; i++)
+    {
+        char_array[i] = word[i];
+    }
 
-    for (int i = 0; i < limit; i ++)
-        {
-            cf[char_array[i]]++;
-        }
 
-        int j = 0;
-        for (auto i : cf) // Traversing the Unordered map
-        {
-            l[j] = i.second;
-            j += 1;
-        }
+    // for (int i = 0; i < limit; i ++)
+    //     {
+    //         cf[char_array[i]]++;
+    //     }
+
+    //     int j = 0;
+    //     for (auto i : cf) // Traversing the Unordered map
+    //     {
+    //         l[j] = i.second;
+    //         j += 1;
+    //     }
         
-        for (int i = 0; i < limit; i ++) // Traversing the Unordered map
-        {
-            cout << l[i] << endl << endl;
-        }
-        
+    //     for (int i = 0; i < limit; i ++) // Traversing the Unordered map
+    //     {
+    //         cout << l[i] << endl << endl;
+    //     }
 
 
         int count = 0;
