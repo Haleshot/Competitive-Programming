6ea1f7a98bd3e145309ebc328af88743f571e65f
Haleshot <hari.leo03@gmail.com>
2023-01-26 20:39:32 +0530

    Updated

--- Codechef/EVENTUAL/EVENTUAL.py
+++ Codechef/EVENTUAL/EVENTUAL.py
@@ -1,6 +1,21 @@
 T = int(input())
 while T:
     T -= 1
+    N = int(input())
     S = input()
 
-    
+    if len(S) == N:
+        d = {}
+        count = 0
+
+        for i in S:
+            if i in d.keys():
+                count += 1
+            
+            else:
+                count = 1
+
+        for i in d.values():
+            if i % 2 == 0:
+                print("YES")
+            print("NO")
