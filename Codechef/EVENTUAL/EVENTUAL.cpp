7f17258ca2776a9712eadf98b82aaa328cfd6720
Haleshot <hari.leo03@gmail.com>
2023-01-26 20:57:00 +0530

    Updated

--- Codechef/EVENTUAL/EVENTUAL.cpp
+++ Codechef/EVENTUAL/EVENTUAL.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 
 int main()
 {
 
-    
+    int T, N;
+    string S = "";
+    cin >> T;
+    cin >> N;
+    cin >> S;
+
+    if(S.length() == N)
+    {
+        
+    }
+
+
+
     return 0;
 }
