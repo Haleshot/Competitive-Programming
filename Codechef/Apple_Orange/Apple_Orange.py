41d8a7184f01cad58e6c5d796736c864774c0fb7
Haleshot <hari.leo03@gmail.com>
2023-01-27 20:41:13 +0530

    Updated

--- Codechef/Apple_Orange/Apple_Orange.py
+++ Codechef/Apple_Orange/Apple_Orange.py
@@ -1,7 +1,12 @@
+import math
 T = int(input())
 while T:
     T -= 1
     N, M = map(int, input().split())
     if (N + M) % 2 == 0:
-        minimum = round((min(N, M)/2))
-        print(minimum)
+        if N % 2 == 0 or M % 2 == 0:
+            print(min(N, M)/2)
+
+        elif N % 2 == 1 or M % 2 == 1:
+            minimum = math.ceil((min(N, M)/2))
+            print(minimum)
