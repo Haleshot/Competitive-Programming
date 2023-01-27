4b08c08a94b32691c4c3ccc11285bc32d41724f0
Haleshot <hari.leo03@gmail.com>
2023-01-27 21:16:02 +0530

    Updated

--- Codechef/Airline/Airline.py
+++ Codechef/Airline/Airline.py
@@ -1,5 +1,15 @@
 T = int(input())
 while T:
     T -= 1
+    l = []
     A, B, C, D, E = map(int, input().split())
-    minimum = min(A, B, C)
+    l = [A, B, C]
+    sorted = l.sort()
+    if sorted[0] <= E:
+        sum = sorted[1] + sorted[2]
+        if sum <= D:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        print("NO")
