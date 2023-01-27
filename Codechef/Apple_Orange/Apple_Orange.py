d784965937c64e585e64c63b5eb146ece5df10fc
Haleshot <hari.leo03@gmail.com>
2023-01-27 20:47:40 +0530

    Updated

--- Codechef/Apple_Orange/Apple_Orange.py
+++ Codechef/Apple_Orange/Apple_Orange.py
@@ -1,12 +1,13 @@
-import math
+def hcf(a, b):
+    if (b == 0):
+        return abs(a)
+    else:
+        return hcf(b, a % b)
+
 T = int(input())
 while T:
     T -= 1
     N, M = map(int, input().split())
     if (N + M) % 2 == 0:
-        if N % 2 == 0 or M % 2 == 0:
-            print(min(N, M)/2)
-
-        elif N % 2 == 1 or M % 2 == 1:
-            minimum = math.ceil((min(N, M)/2))
-            print(minimum)
+        result = hcf
+        print(result)
