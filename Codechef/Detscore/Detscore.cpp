8244572477f38dec2fc1a5b2f6da0881ec528a1f
Haleshot <hari.leo03@gmail.com>
2023-01-28 22:41:17 +0530

    Updated

---
+++ Codechef/Detscore/Detscore.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int X, N, score;
+        cin >> X >> N;
+        X /= 10;
+        score = X * N;
+        cout << score << endl;
+
+    }
+    
+    return 0;
+
+}
