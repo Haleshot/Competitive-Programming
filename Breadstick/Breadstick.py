ecf2e0d5f275d38b2a45132d4c7e1ef7a3cf009e
Haleshot <hari.leo03@gmail.com>
2023-01-29 20:16:10 +0530

    Updated Python Version

--- Breadstick/Breadstick.py
+++ Breadstick/Breadstick.py
@@ -2,6 +2,7 @@ T = int(input())
 while T:
     T -= 1
     N, X = map(int, input().split())
+    
     if (N % X == 0):
         print("YES")
     elif ((N % 2 == 0 and X % 2 == 0) or (X % 2 == 1)):
