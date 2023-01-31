62bd5e04d26dd1dce149cafba47bad37955f1ad6
Haleshot <hari.leo03@gmail.com>
2023-01-31 22:14:25 +0530

    Updated

--- Codechef/Dominant/Dominant.cpp
+++ Codechef/Dominant/Dominant.cpp
@@ -11,7 +11,26 @@ int main()
     {
         int Na, Nb, Nc;
         cin >> Na >> Nb >> Nc;
-        
+
+        if (Na > (Nb + Nc))
+        {
+            cout << "YES" << endl;
+        }
+
+        else if (Nb > (Na + Nc))
+        {
+            cout << "YES" << endl;
+        }
+
+        else if (Nc > (Na + Nb))
+        {
+            cout << "YES" << endl;
+        }
+
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
 
     return 0;
