915be8a508834a9ab2852a13e40c0ce504a48464
Haleshot <hari.leo03@gmail.com>
2023-01-31 22:12:59 +0530

    Updated

---
+++ Codechef/Dominant/Dominant.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        int Na, Nb, Nc;
+        cin >> Na >> Nb >> Nc;
+        
+    }
+
+    return 0;
+}
