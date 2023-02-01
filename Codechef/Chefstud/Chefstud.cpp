d18a17008aba51d636b912642b5f28e46401effa
Haleshot <hari.leo03@gmail.com>
2023-02-01 18:48:23 +0530

    Updated C++ Version

--- Codechef/Chefstud/Chefstud.cpp
+++ Codechef/Chefstud/Chefstud.cpp
@@ -4,26 +4,22 @@ using namespace std;
 
 int main()
 {
-
     int T;
     cin >> T;
     while (T--)
     {
         string s;
         cin >> s;
-        int c;
+        int c = 0;
         for (int i = 0; i < s.length(); i++)
         {
-            if (s[i] == '<' && s[i + 1] == '<')
+            if (s[i] == '<' && s[i + 1] == '>')
             {
                 c++;
             }
-
         }
         cout << c << endl;
-
     }
-
     return 0;
 }
 
