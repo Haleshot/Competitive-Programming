f2874c1aee4353be5394624c483653719bec84d1
Haleshot <hari.leo03@gmail.com>
2023-02-01 21:01:09 +0530

    Updated

--- Codechef/Primereverse/Primereverse.cpp
+++ Codechef/Primereverse/Primereverse.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+int Count_Freq()
+{
+
+}
 
 
 int main()
 {
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        string A, B;
+        cin >> N >> A >> B;
+        
+    }
 
-    
     return 0;
 }
