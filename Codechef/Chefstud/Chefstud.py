bf2801b7f98c009e7a31a776f19b725c4f3993a4
Haleshot <hari.leo03@gmail.com>
2023-02-01 18:44:44 +0530

    Updated Python Version

--- Codechef/Chefstud/Chefstud.py
+++ Codechef/Chefstud/Chefstud.py
@@ -2,8 +2,9 @@ T = int(input())
 while T:
     T -= 1
     s = input()
+    c = 0
     for i in range(len(s) - 1):
-        c = 0
+        
         if s[i] == "<" and s[i + 1] == ">":
             c += 1
     print(c)
