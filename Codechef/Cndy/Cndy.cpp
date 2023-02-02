65377da7bfcead4db98f9f05e2f455212919f781
Haleshot <hari.leo03@gmail.com>
2023-02-02 21:22:18 +0530

    Updated C++ Version

--- Codechef/Cndy/Cndy.cpp
+++ Codechef/Cndy/Cndy.cpp
@@ -12,29 +12,6 @@ int Count_Freq(int X[], int limit)
         cf[X[i]]++;
     }
 
-    // int j = cf.size();
-    // int l[j];
-    // int k = 0;    
-    // for (auto i : cf) // Traversing the Unordered map
-    // {
-    //     if(k < j)
-    //     {
-    //         l[k] = i.second;
-    //         k += 1;
-    //     }
-        
-    // }
-
-    // int c = 0;
-    // for (int i = 0; i < limit; i ++)
-    // {
-    //     if (l[i] > 2)
-    //     {
-    //         c += 1;
-    //         break;
-    //     }
-    // }
-
     int c = 0;
     for (auto i : cf) // Traversing the Unordered map
     {
