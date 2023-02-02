b4a174b609b8903e78ac836c52c8c561458ac30a
Haleshot <hari.leo03@gmail.com>
2023-02-02 20:40:34 +0530

    Updated

--- Codechef/Cndy/Cndy.py
+++ Codechef/Cndy/Cndy.py
@@ -1,4 +1,15 @@
 T = int(input())
 while T:
     T -= 1
-    
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    d = {}
+    c = 0
+    for i in range(len(A)):
+        if A[i] in d.keys():
+            d[A[i]] += 1
+        else:
+            d[A[i]] = 1
+
+    print(d.values())
