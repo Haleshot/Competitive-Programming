559302dec89bf219a462f090d9002c2ddd459f56
Haleshot <hari.leo03@gmail.com>
2023-02-02 21:00:44 +0530

    Updated

--- Codechef/Cndy/Cndy.cpp
+++ Codechef/Cndy/Cndy.cpp
@@ -1,10 +1,56 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 
-int Count_Freq(int X[])
+int Count_Freq(int X[], int limit)
 {
-    
+    map<int, int> cf;
+
+    for (int i = 0; i < limit; i ++)
+    {
+        cf[X[i]]++;
+    }
+
+    int j = cf.size();
+    int l[j];
+    int k = 0;    
+    for (auto i : cf) // Traversing the Unordered map
+    {
+        if(k < j)
+        {
+            l[k] = i.second;
+            k += 1;
+        }
+        
+    }
+    for (int i = 0; i < limit; i ++)
+    {
+        cout << l[i] << endl;
+    }
+
+
+    int c = 0;
+    for (int i = 0; i < limit; i ++)
+    {
+        if (l[i] > 2)
+        {
+            c += 1;
+            break;
+        }
+    }
+
+    if (c > 0)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+
+
+
 }
 
 int main()
@@ -21,8 +67,16 @@ int main()
             cin >> A[i];
         }
 
+        int result = Count_Freq(A, 2 * N);
 
-
+        if (result > 0)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
 
     }
 
