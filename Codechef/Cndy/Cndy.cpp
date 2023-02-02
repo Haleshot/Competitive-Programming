dfe3c295dd3793192e7b88b724d8e6efdefa0b82
Haleshot <hari.leo03@gmail.com>
2023-02-02 20:47:27 +0530

    Updated

---
+++ Codechef/Cndy/Cndy.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        int A[2 * N];
+        for (int i = 0; i < N; i ++)
+        {
+            cin >> A[i];
+        }
+        
+        
+
+    }
+
+    return 0;
+}
