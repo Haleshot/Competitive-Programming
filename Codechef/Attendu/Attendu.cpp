7d649344ec638019e7f3af8bb7160fdaa06e2a3d
Haleshot <hari.leo03@gmail.com>
2023-02-03 20:47:24 +0530

    Updated

--- Codechef/Attendu/Attendu.cpp
+++ Codechef/Attendu/Attendu.cpp
@@ -7,13 +7,21 @@ int main()
     cin >> T;
     while (T--)
     {
-        int N, remain;
-        float atten_perc;
+        int N, remain, c = 0;
+        float atten_perc = 0;
         string B;
         cin >> N;
-        cin >> B;
-        remain = 120 - N;
+        cin >> B; 
+        for (int i = 0; i < B.length(); i++)
+        {
+            if (B[i] == '1')
+            {
+                c += 1;
+            }
+        }
+        remain = 120 - N + c;
         atten_perc = (remain/120) * 100;
+        cout << atten_perc << endl;
         if (atten_perc >= 75.0)
         {
             cout << "YES" << endl;
