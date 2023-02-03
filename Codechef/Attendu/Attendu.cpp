cf28741a85c7e4d0ac0131109ec487b7232d5f76
Haleshot <hari.leo03@gmail.com>
2023-02-03 20:36:30 +0530

    Updated

--- Codechef/Attendu/Attendu.cpp
+++ Codechef/Attendu/Attendu.cpp
@@ -3,7 +3,27 @@ using namespace std;
 
 int main()
 {
-    
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, remain;
+        float atten_perc;
+        string B;
+        cin >> N;
+        cin >> B;
+        remain = 120 - N;
+        atten_perc = (remain/120) * 100;
+        if (atten_perc >= 75)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+
+    }
 
 
 
