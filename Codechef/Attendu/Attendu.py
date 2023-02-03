a2e9f44a2f548ec81f6b0ce0bdf5992287df000d
Haleshot <hari.leo03@gmail.com>
2023-02-03 20:34:05 +0530

    Updated

--- Codechef/Attendu/Attendu.py
+++ Codechef/Attendu/Attendu.py
@@ -3,7 +3,7 @@ while T:
     T -= 1
     N = int(input())
     B = input()
-    remain = 120 - B
+    remain = 120 - N
     atten_perc = (remain/120) * 100
     if atten_perc >= 75:
         print("YES")
