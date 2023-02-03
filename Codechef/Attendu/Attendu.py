f19f6ca9945cc1b7c0272095d3ea8bca0e118302
Haleshot <hari.leo03@gmail.com>
2023-02-03 20:28:12 +0530

    Updated

--- Codechef/Attendu/Attendu.py
+++ Codechef/Attendu/Attendu.py
@@ -3,4 +3,11 @@ while T:
     T -= 1
     N = int(input())
     B = input()
+    c1 = 0
+    c2 = 0
+    for i in B:
+        if i == "0":
+            c1 += 1
+        elif i == "1":
+            c2 += 1
     
