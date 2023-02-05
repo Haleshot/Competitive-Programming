166d0a2df6c65a3fcbdbaadfbb38483a676e6009
Haleshot <hari.leo03@gmail.com>
2023-02-05 21:14:55 +0530

    Updated

--- Codechef/Zero_String/Zero_String.py
+++ Codechef/Zero_String/Zero_String.py
@@ -4,12 +4,9 @@ while T:
     N = int(input())
     S = input()
     l = [i for i in S]
-    c1 = l.count("1")
+    c1 = l.count("0")
     c2 = N - c1
-    if c1 == N:
-        print(1)
+    if c1 >= c2:
+        print(c2)
     else:
-        if c1 > c2:
-            print(c1 + 1)
-        else:
-            print(c1)
+        print(c1 + 1)
