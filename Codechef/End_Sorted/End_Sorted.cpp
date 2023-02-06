1b411af099639de1af7f6658a13bdf67db2a258a
Haleshot <hari.leo03@gmail.com>
2023-02-06 22:17:10 +0530

    Updated

--- Codechef/End_Sorted/End_Sorted.cpp
+++ Codechef/End_Sorted/End_Sorted.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 
@@ -8,13 +9,25 @@ int main()
     cin >> T;
     while (T--)
     {
-        int N;
+        int N, index_1, index_N;
         cin >> N;
         int P[N];
         for (int i = 0; i < N; i++)
         {
             cin >> P[i];
         }
+        index_1 = find(P, P + N, 1) - P;
+        index_N = find(P, P + N, N) - P;
+        
+        if (index_1 < index_N)
+        {
+            cout << index_1 + N - 1 - index_N << endl;
+        }
+        else
+        {
+            cout << index_1 + N - 2 - index_N << endl;
+        }
+
         
     }
 
