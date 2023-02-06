de8efb859033258f96ffe328fe9a2b20c21ed077
Haleshot <hari.leo03@gmail.com>
2023-02-06 21:46:59 +0530

    Updated

--- Codechef/End_Sorted/End_Sorted.py
+++ Codechef/End_Sorted/End_Sorted.py
@@ -3,7 +3,13 @@ while T:
     T -= 1
     N = int(input())
     P = list(map(int, input().split()))
+    c = 0
     if (P[0] == 1 and P[N - 1] == N):
-        print(0)
+        print(c)
     else:
-        pass
+        for i in range(N - 1):
+            if P[i] > P[i + 1]:
+                P[i], P[i + 1] = P[i + 1], P[i]
+                c += 1
+        if (P[0] == 1 and P[N - 1] == N):
+            print(c)
