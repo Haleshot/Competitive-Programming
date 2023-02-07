65cf4a6d12cd230da693daa37505ccf9d52b008f
Haleshot <hari.leo03@gmail.com>
2023-02-07 22:12:12 +0530

    Updated

--- Codechef/Bscost/Bscost.py
+++ Codechef/Bscost/Bscost.py
@@ -3,4 +3,17 @@ while T:
     T -= 1
     N, X, Y = map(int, input().split())
     S = input()
+    l = [i for i in S]
+    if X > Y:
+        l.sort()
+    else:
+        l.sort(reverse=True)
+    c1 = 0
+    c2 = 0
+    for i in range(l):
+        if l[i] == 0 and l[i + 1] == 1:
+            c1 += 1
+        elif l[i] == 1 and l[i + 1] == 0:
+            c2 += 1
+    
     
