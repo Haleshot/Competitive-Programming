7b3cd5e2631472125d8249ca9c923b56d2cda1aa
Haleshot <hari.leo03@gmail.com>
2023-02-08 22:20:18 +0530

    Updated

---
+++ Codechef/Age_Limit/Age_Limit.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X, Y, A;
+        cin >> X >> Y >> A;
+        if (A >= X && A < Y)
+        {
+
+        }
+    }
+
+
+
+    return 0;
+}
