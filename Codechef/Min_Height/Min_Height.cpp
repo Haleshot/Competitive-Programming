4e9acda945939a09b144ab8befcc405e2f77f1f6
Haleshot <hari.leo03@gmail.com>
2023-02-09 22:16:04 +0530

    Updated

---
+++ Codechef/Min_Height/Min_Height.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X, H;
+        cin >> X >> H;
+    }
+    
+    
+    
+    return 0;
+}
