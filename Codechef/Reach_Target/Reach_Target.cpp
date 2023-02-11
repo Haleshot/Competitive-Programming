b534bd81fd55d683dcbdcfafca65dbfffed4a074
Haleshot <hari.leo03@gmail.com>
2023-02-11 22:55:51 +0530

    Updated

---
+++ Codechef/Reach_Target/Reach_Target.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        int X, Y;
+        cin >> X >> Y;
+        cout << X - Y << endl;
+    }
+
+    return 0;
+}
