115e644d07ff87e60b2395e2dbc59b9d2d165884
Haleshot <hari.leo03@gmail.com>
2023-02-12 20:58:14 +0530

    Updated

---
+++ Codechef/Cnt_Wrd/Cnt_Wrd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, M;
+        cin >> N >> M;
+        cout << N * M << endl;
+    }
+    return 0;
+}
