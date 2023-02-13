8f328ab2448a18b171302c62f35fdc344c1df0f7
Haleshot <hari.leo03@gmail.com>
2023-02-13 22:20:44 +0530

    Update

---
+++ Btry_Hlth.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    while (T--)
+    {
+        int X;
+        cin >> X;
+        if (X >= 80)
+        {
+            cout << "YES" << endl;
+        }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    }
+    return 0;
+}
