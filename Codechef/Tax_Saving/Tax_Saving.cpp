93324b0f451300df384edfb8bcd31e5ef0ec8ea0
Haleshot <hari.leo03@gmail.com>
2023-02-17 23:08:00 +0530

    Updated C++ Version

--- Codechef/Tax_Saving/Tax_Saving.cpp
+++ Codechef/Tax_Saving/Tax_Saving.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     int T;
     cin >> T;
-    
     while (T--)
     {
         int X, Y;
         cin >> X >> Y;
         cout << X - Y << endl;
     }
+
     return 0;
 }
