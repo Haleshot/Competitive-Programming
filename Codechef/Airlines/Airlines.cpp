f9d2db4b977d138e49f30166e5ce64ccd37588de
Haleshot <hari.leo03@gmail.com>
2023-02-19 20:53:17 +0530

    Updated

---
+++ Codechef/Airlines/Airlines.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X, Y, Z, limit;
+        cin >> X >> Y >> Z;
+    
+    }
+    return 0;
+
+}
