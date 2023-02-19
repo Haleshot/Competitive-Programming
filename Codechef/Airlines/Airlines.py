bf69cf3517386855c42ddbad74d4f503fe5fd907
Haleshot <hari.leo03@gmail.com>
2023-02-19 20:52:14 +0530

    Updated Python Version

--- Codechef/Airlines/Airlines.py
+++ Codechef/Airlines/Airlines.py
@@ -2,8 +2,8 @@ T = int(input())
 while T:
     T -= 1
     X, Y, Z = map(int, input().split())
-    num = 10 * X
-    if (num < Y):
-        print(num * Z)
+    limit = 10 * X
+    if (limit < Y):
+        print(limit * Z)
     else:
         print(Y * Z)
