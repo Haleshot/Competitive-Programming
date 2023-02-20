38f5b5481c7a0b352b325356077cf80a3dc9ea4d
Haleshot <hari.leo03@gmail.com>
2023-02-20 08:04:31 +0530

    Updated

---
+++ Codechef/Monopoly/Monopoly.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int R1, R2, R3;
+        cin >> R1 >> R2 >> R3;
+        if (R1 > (R2 + R3))
+        {
+            cout << "YES" << endl;
+        }
+        if (R2 > (R1 + R3))
+        {
+            cout << "YES" << endl;
+        }
+        if (R3 > (R1 + R2))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
