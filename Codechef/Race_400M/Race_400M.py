7910d42d28a40f0940245f0447237f5e5079e749
Haleshot <hari.leo03@gmail.com>
2023-02-21 07:40:12 +0530

    Updated

--- Codechef/Race_400M/Race_400M.py
+++ Codechef/Race_400M/Race_400M.py
@@ -2,11 +2,14 @@ T = int(input())
 while T:
     T -= 1
     X, Y, Z = map(int, input().split())
-    if (400//X) > ((400//Y) and (400//Z)):
+    X = 400/X
+    Y = 400/Y
+    Z = 400/Z
+    if X > Y and X > Z:
         print("ALICE")
 
-    elif (400//Y) > ((400//X) and (400//Z)):
+    elif Y > X and Y > Z:
         print("BOB")
 
-    elif (400//Z) > ((400//X) and (400//Y)):
+    elif Z > X and Z > Y:
         print("CHARLIE")
