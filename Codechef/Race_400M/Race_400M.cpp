46030151af71f26effa981b3160b9f829f3219ef
Haleshot <hari.leo03@gmail.com>
2023-02-21 07:42:54 +0530

    Updated

---
+++ Codechef/Race_400M/Race_400M.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    
+    while (T--)
+    {
+        int X, Y, Z;
+        cin  >> X >> Y >> Z;
+        X = 400/X;
+        Y = 400/Y;
+        Z = 400/Z;
+        if (X > Y && X > Z)
+        {
+            cout << "ALICE" << endl;
+        }
+        else if (Y > X && Y > Z)
+        {
+            cout << "BOB" << endl;
+        }
+        else if (Z > X && Z > Y)
+        {
+            cout << "CHARLIE" << endl;
+        }
+    }
+
+    return 0;
+}
