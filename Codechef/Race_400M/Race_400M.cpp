54c14f4db8daa1f11474d410f456f8370d5bfc61
Haleshot <hari.leo03@gmail.com>
2023-02-21 07:51:22 +0530

    Updated

--- Codechef/Race_400M/Race_400M.cpp
+++ Codechef/Race_400M/Race_400M.cpp
@@ -8,18 +8,15 @@ int main()
     {
         int X, Y, Z;
         cin >> X >> Y >> Z;
-        X = 400/X;
-        Y = 400/Y;
-        Z = 400/Z;
-        if (X > Y && X > Z)
+        if (X < Y && X < Z)
         {
             cout << "ALICE" << endl;
         }
-        else if (Y > X && Y > Z)
+        else if (Y < X && Y < Z)
         {
             cout << "BOB" << endl;
         }
-        else if (Z > X && Z > Y)
+        else if (Z < X && Z < Y)
         {
             cout << "CHARLIE" << endl;
         }
