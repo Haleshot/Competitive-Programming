0624a58d72f38b016c0b11a8485ef61d90ddd063
Haleshot <hari.leo03@gmail.com>
2023-02-22 08:24:42 +0530

    Updated

--- Codechef/Min_Pizza/Min_Pizza.cpp
+++ Codechef/Min_Pizza/Min_Pizza.cpp
@@ -11,7 +11,7 @@ int main()
         int N, X, slices;
         cin >> N >> X;
         slices = N * X;
-        cout << ceil(slices/4) << endl;
+        cout << round(slices/4) << endl;
     }
     return 0;
 }
