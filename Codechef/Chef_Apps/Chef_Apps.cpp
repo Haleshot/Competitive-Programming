e88b70dc9e73f4761e15cdc61ca5c8f1637bd221
Haleshot <hari.leo03@gmail.com>
2023-02-26 21:52:54 +0530

    Updated

--- Codechef/Chef_Apps/Chef_Apps.cpp
+++ Codechef/Chef_Apps/Chef_Apps.cpp
@@ -6,16 +6,17 @@ int main()
 {
     int T;
     cin >> T;
-    
+
     while (T--)
     {
         int S, X, Y, Z, remain;
         cin >> S >> X >> Y >> Z;
+        remain = Z - (X + Y);
         if (Z <= remain)
         {
             cout << "0" << endl;
         }
-        else if (S >= (X + Z) || Z >= (Y + Z))
+        else if (S >= (X + Z) || S >= (Y + Z))
         {
             cout << "1" << endl;
         }
