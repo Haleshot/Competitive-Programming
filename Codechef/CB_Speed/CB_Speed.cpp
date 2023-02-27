06f1fb5360f79e94808b14cc3bd48ca5fbdf46ca
Haleshot <hari.leo03@gmail.com>
2023-02-27 23:03:44 +0530

    Updated

---
+++ Codechef/CB_Speed/CB_Speed.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int X, Y;
+    cin >> X >> Y;
+
+    if (Y <= X)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
+    return 0;
+}
