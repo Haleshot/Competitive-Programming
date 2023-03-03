a83454f422613aed6527bc0abfd54c2c44f1864a
Haleshot <hari.leo03@gmail.com>
2023-03-03 23:30:58 +0530

    Updated

---
+++ Codechef/Kngtor/Kngtor.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, M;
+        cin >> N >> M;
+
+        cout << (N * 5) + (M * 7) << endl;
+    }
+    return 0;
+}
