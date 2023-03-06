9ca8eceaa76433d6fdaf62b151015938486d759e
Haleshot <hari.leo03@gmail.com>
2023-03-06 22:24:37 +0530

    Update

---
+++ Codechef/Budget_/Budget_.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    while (T--)
+    {
+        int X, Y, expense;
+        cin >> X >> Y;
+        expense = 30 * Y;
+        if (expense <= X)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
