562f4f9e627e8058bc05aa6b8cd84c9b42b8fde1
Haleshot <hari.leo03@gmail.com>
2023-03-07 19:48:57 +0530

    Updated

---
+++ Codechef/Avg_Problem/Avg_Problem.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int A, B, C;
+        if ((A + B)/2 > C)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+
+    }
+    return 0;
+}
