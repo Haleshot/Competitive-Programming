cfd1a45ad37107a6c0bec9bac4030d0d2de3c7fd
Haleshot <hari.leo03@gmail.com>
2023-03-08 21:57:36 +0530

    Updated

---
+++ Codechef/Three_Topics/Three_Topics.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B, C, X;
+    cin >> A >> B >> C >> X;
+    if (X == A || X == B || X == C)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
