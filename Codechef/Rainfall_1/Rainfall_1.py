80886241d5aae6794e8602d06ccaf34650dfe0c6
Haleshot <hari.leo03@gmail.com>
2023-03-09 23:03:14 +0530

    Updated

--- Codechef/Rainfall_1/Rainfall_1.py
+++ Codechef/Rainfall_1/Rainfall_1.py
@@ -1,4 +1,10 @@
 T = int(input())
 while T:
     T -= 1
-    
+    X = int(input())
+    if (X < 3):
+        print("LIGHT")
+    elif (X >= 3 and X < 7):
+        print("MODERATE")
+    elif (X >= 7):
+        print("HEAVY")
