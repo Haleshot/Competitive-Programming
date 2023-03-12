cb33701d9e048ea47be9522fb65a41f75f09807b
Haleshot <hari.leo03@gmail.com>
2023-03-12 15:23:36 +0530

    Updted

---
+++ Codechef/F1_Rule/F1_Rule.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X, Y, time;
+        cin >> X >> Y;
+        time = 1.07 * X;
+        if (Y > time)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
