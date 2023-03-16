fe2d608bbb35388ef8924ddbee3156587e202c6c
Haleshot <hari.leo03@gmail.com>
2023-03-16 19:31:11 +0530

    Updated

--- Codechef/Netflix/Netflix.cpp
+++ Codechef/Netflix/Netflix.cpp
@@ -8,7 +8,15 @@ int main()
     while (T--)
     {
         int A, B, C, X;
-        
+        cin >> A >> B >> C >> X;
+        if ((A + B) >= X || (A + C) >= X || (B + C) >= X )
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
     return 0;
 }
