29b66b82f41f657e25ac171197536f71d83ac44e
Haleshot <hari.leo03@gmail.com>
2023-03-16 19:29:29 +0530

    Update

---
+++ Codechef/Netflix/Netflix.py
@@ -0,0 +1,8 @@
+T = int(input())
+while T:
+    T -= 1
+    A, B, C, X = map(int, input().split())
+    if ((A + B) >= X or(A + C) >= X or(B + C) >= X):
+        print("YES")
+    else:
+        print("NO")
