93e1680cfb0c7cfc258113f41a9ca00e16ec328b
Haleshot <hari.leo03@gmail.com>
2023-03-18 12:36:52 +0530

    Updated

---
+++ Codechef/Tea/Tea.py
@@ -0,0 +1,13 @@
+T = int(input())
+while T:
+    T -= 1
+    X, Y, Z = map(int, input().split())
+    if X <= Y:
+        print(Z)
+    else:
+        while True:
+            Y *= 2
+            Z *= 2
+            if (X <= Y):
+                print(Z)
+                break
