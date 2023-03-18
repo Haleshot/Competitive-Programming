c804d206a216b85e41bec58d9f776a9641ca3431
Haleshot <hari.leo03@gmail.com>
2023-03-18 12:38:35 +0530

    Updated

--- Codechef/Tea/Tea.py
+++ Codechef/Tea/Tea.py
@@ -5,9 +5,10 @@ while T:
     if X <= Y:
         print(Z)
     else:
+        i = 2
         while True:
-            Y *= 2
-            Z *= 2
+            temp_Y = Y * i
+            temp_Z = Z * 2
             if X <= Y:
                 print(Z)
                 break
