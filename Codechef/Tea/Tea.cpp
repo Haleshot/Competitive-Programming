7fd5647292584fc3c2dc643edbf4e6742f1ef10c
Haleshot <hari.leo03@gmail.com>
2023-03-18 12:51:00 +0530

    Updated

--- Codechef/Tea/Tea.cpp
+++ Codechef/Tea/Tea.cpp
@@ -13,6 +13,21 @@ int main()
         {
             cout << Z << endl;
         }
+        else
+        {
+            while (true)
+            {
+                int i = 2, temp_Y, temp_Z;
+                temp_Y = Y * i;
+                temp_Z = Z * i;
+                if (X <= temp_Y)
+                {
+                    cout << temp_Z << endl;
+                    break;
+                }
+
+            }
+        }
     }
     return 0;
 }
