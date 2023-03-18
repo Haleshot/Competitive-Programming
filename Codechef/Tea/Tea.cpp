45eedf1a7752ac40521eeacf127be96da73a031b
Haleshot <hari.leo03@gmail.com>
2023-03-18 12:47:28 +0530

    Updated

---
+++ Codechef/Tea/Tea.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    [
+        int X, Y, Z;
+        cin >> X >> Y >> Z;
+        
+    ]
+    erturn 0;
+}
