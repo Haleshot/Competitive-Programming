043a36708f2630ac7e9d72d1d973a7a98f775201
Haleshot <hari.leo03@gmail.com>
2023-03-20 17:11:27 +0530

    Updated

---
+++ Codechef/Qualify/Qualify.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X, A, B;
+        cin >> X >> A >> B;
+        if (A + (B * 2) >= X)
+        {
+            cout << "QUALIFY" << endl;
+        }
+        else
+        {
+            
+        }
+    }
+    return 0;
+
+}
