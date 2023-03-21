15476dd9bf9adf6079af9303bb75f52f56bd8d97
Haleshot <hari.leo03@gmail.com>
2023-03-21 23:02:09 +0530

    Updated

--- Codechef/Test_Avg/Test_Avg.py
+++ Codechef/Test_Avg/Test_Avg.py
@@ -3,5 +3,7 @@ T = int(input())
 while T:
     T -= 1
     A, B, C = map(int, input().split())
-    if (((A + B)/2 >= 35)):
-        pass
+    if (((A + B)/2 >= 35) or ((A + C)/2 >= 35) or ((B + C)/2 >= 35)):
+        print("PASS")
+    else:
+        print("FAIL")
