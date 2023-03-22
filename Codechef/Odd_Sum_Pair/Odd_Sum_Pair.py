b8ba1d773d2280a2ae409f1d861c849f6f4a9e75
Haleshot <hari.leo03@gmail.com>
2023-03-22 22:38:38 +0530

    Updated

--- Codechef/Odd_Sum_Pair/Odd_Sum_Pair.py
+++ Codechef/Odd_Sum_Pair/Odd_Sum_Pair.py
@@ -2,7 +2,7 @@ T = int(input())
 while T:
     T -= 1
     A, B, C = map(int, input().split())
-    if (((A + B) % 2 == 1) and ((A + C) % 2 == 1) and ((B + C) % 2 == 1)):
+    if (((A + B) % 2 == 1) or ((A + C) % 2 == 1) or ((B + C) % 2 == 1)):
         print("YES")
     else:
         print("NO")
