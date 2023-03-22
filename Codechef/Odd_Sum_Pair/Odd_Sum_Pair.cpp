c68d682fbaa2bc822ce885e6951400585d632543
Haleshot <hari.leo03@gmail.com>
2023-03-22 22:40:43 +0530

    Updated

--- Codechef/Odd_Sum_Pair/Odd_Sum_Pair.cpp
+++ Codechef/Odd_Sum_Pair/Odd_Sum_Pair.cpp
@@ -11,11 +11,11 @@ int main()
         cin >> A >> B >> C;
         if (((A + B) %2 == 1) || ((A + C) % 2 == 1) || ((B + C) % 2 == 1))
         {
-            cout << "PASS" << endl;
+            cout << "YES" << endl;
         }
         else
         {
-            cout << "FAIL" << endl;
+            cout << "NO" << endl;
         }
     }
     return 0;
