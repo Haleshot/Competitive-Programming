00ffbe96e560608239ad5b8cd092df3c0574dd1b
Haleshot <hari.leo03@gmail.com>
2023-03-23 18:18:11 +0530

    Updated

---
+++ Codechef/Accuracy/Accuracy.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        float X;
+        int temp;
+        cin >> X;
+        temp = ceil(X/3)
+        temp *= 3;
+        cout << temp - X << endl;
+
+
+    }
+    return 0;
+}
