d7c16d12d3fa559bde05a21161474df53c1a8ef4
Haleshot <hari.leo03@gmail.com>
2023-03-24 22:02:22 +0530

    Updated

---
+++ Codechef/Cabs/Cabs.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X, Y;
+        cin >> X >> y;
+        if (X < Y)
+        {
+            cout << "FIRST" << endl;
+        }
+        else if (X > Y)
+        {
+            cout << "SECOND" << endl;
+        }
+        else
+        {
+            cout << "ANY" << endl;
+        }
+    }
+    return 0;
+}
