18d93dabe9e1e2142583d5c5379cd8778623942e
Haleshot <hari.leo03@gmail.com>
2023-03-29 23:24:42 +0530

    Updated

--- Codechef/Nibble/Nibble.cpp
+++ Codechef/Nibble/Nibble.cpp
@@ -7,7 +7,16 @@ int main()
     cin >> T;
     while (T--)
     {
-        
+        int X;
+        cin >> X;
+        if (X % 4 == 0)
+        {
+            cout << "Good" << endl;
+        }
+        else
+        {
+            cout << "Not Good" << endl;
+        }
     }
     return 0;
 }
