4e615d05db3a2c88870d104abe73e61dca518fb3
Haleshot <hari.leo03@gmail.com>
2023-03-31 08:13:20 +0530

    Updated

--- Codechef/Auction/Auction.cpp
+++ Codechef/Auction/Auction.cpp
@@ -7,7 +7,16 @@ int main()
     cin >> T;
     while (T--)
     {
-        
+        int A, B, C;
+        cin >> A >> B >> C;
+        if (A > B && A > C)
+        {
+            cout << "Alice" << endl;
+        }
+        else if (B > A && B > C)
+        {
+            cout << "Bob" << endl;
+        }
     }
     return 0;
 }
