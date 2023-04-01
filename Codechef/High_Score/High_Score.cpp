bacb6484a838f3480b43fb3efb0d94a37e499876
Haleshot <hari.leo03@gmail.com>
2023-04-01 14:20:16 +0530

    Updated

---
+++ Codechef/High_Score/High_Score.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, Na, Nb, Nc, Nd;
+        cin >> N >> Na >> Nb >> Nc >> Nd;
+        
+        
+    }
+    return 0;
+}
