4250f5c77feff160cc1fa08199ea2c3f4f550abe
Haleshot <hari.leo03@gmail.com>
2023-04-02 16:32:06 +0530

    Updated

--- Codechef/Speed_Test/Speed_Test.cpp
+++ Codechef/Speed_Test/Speed_Test.cpp
@@ -7,8 +7,21 @@ int main()
     cin >> T;
     while (T--)
     {
-
+        int A, X, B, Y;
+        cin >> A >> X >> B >> Y;
+        if ((A/X) > (B/Y))
+        {
+            cout << "ALICE" << endl;
+        }
+        else if (((A/X) < (B/Y)))
+        {
+            cout << "BOB" << endl;
+        }
+        else
+        {
+            cout << "EQUAL" << endl;
+        }
     }
-    
+
     return 0;
 }
