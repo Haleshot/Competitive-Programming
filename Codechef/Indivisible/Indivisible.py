1e84ce0d290953dbffc0ea332947f74fd5d53fc8
Haleshot <hari.leo03@gmail.com>
2023-04-03 22:24:18 +0530

    Updated

--- Codechef/Indivisible/Indivisible.py
+++ Codechef/Indivisible/Indivisible.py
@@ -2,6 +2,7 @@ T = int(input())
 while T:
     T -= 1
     A, B, C = map(int, input().split())
-    for i in range(1, 100):
-        if (i % A == 1 and i % B == 1 and i % C == 1):
+    min_num = min(A, B, C)
+    for i in range(min_num, 100):
+        if (A % i != 0 and B % i != 0 and C % i != 0):
             print(i)
