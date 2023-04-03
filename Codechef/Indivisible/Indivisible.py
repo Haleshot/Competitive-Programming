0a757f1808762952e4c1194d92d20bd99eeba79e
Haleshot <hari.leo03@gmail.com>
2023-04-03 22:09:58 +0530

    Updated

--- Codechef/Indivisible/Indivisible.py
+++ Codechef/Indivisible/Indivisible.py
@@ -2,4 +2,7 @@ T = int(input())
 while T:
     T -= 1
     A, B, C = map(int, input().split())
-    
+    max_D = max(A, B, C)
+    for i in range(max_D):
+        if (i % A == 0 and i % B == 0 and i % C == 0):
+            print(i)
