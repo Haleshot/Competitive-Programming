f47b04ea0724709ac633fe347a69060080012dd5
Haleshot <hari.leo03@gmail.com>
2023-04-03 22:31:25 +0530

    Updated

---
+++ Codechef/Indivisible/Indivisible.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int A, B, C;
+        cin >> A >> B >> C;
+        
+    }
+    return 0;
+}
