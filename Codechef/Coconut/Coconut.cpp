6550a3a426a7c3d68cafaeda87b4cafcd4f15ebd
Haleshot <hari.leo03@gmail.com>
2023-04-11 22:30:43 +0530

    U0pdated

---
+++ Codechef/Coconut/Coconut.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int xa, xb, Xa, Xb;
+        cin >> xa >> xb >> Xa >> Xb;
+        
+    }
+    return 0;
+}
