3dd3d587b7503ee67392421196b40700be4317ea
Haleshot <hari.leo03@gmail.com>
2023-04-11 22:29:50 +0530

    Updated

--- Codechef/Coconut/Coconut,.py
+++ Codechef/Coconut/Coconut,.py
@@ -2,6 +2,6 @@ T = int(input())
 while T:
     T -= 1
     xa, xb, Xa, Xb = map(int, input().split())
-    req_1 = Xa/xa
-    req_2 = Xb/xb
+    req_1 = Xa//xa
+    req_2 = Xb//xb
     print(req_1 + req_2)
