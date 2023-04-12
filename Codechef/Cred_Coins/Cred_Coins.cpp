d35263d258223731eaf421cc52daf00cf90fb2ce
Haleshot <hari.leo03@gmail.com>
2023-04-12 22:52:39 +0530

    Updated

---
+++ Codechef/Cred_Coins/Cred_Coins.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X, Y, prod;
+        cin >> X >> Y;
+        prod = X * Y;
+        cout << prod/100 << endl;
+
+    }
+    return -;
+}
