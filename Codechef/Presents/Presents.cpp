fd8f26732c1df40cf3c25eb387d18345aa968c6b
Haleshot <hari.leo03@gmail.com>
2023-04-13 22:52:37 +0530

    Updated

--- Codechef/Presents/Presents.cpp
+++ Codechef/Presents/Presents.cpp
@@ -11,7 +11,13 @@ int main()
         cin >> N;
         if (N % 5 == 0)
         {
-
+            temp = int(N / 5);
+            cout << 4 * temp << endl;
+        }
+        else
+        {
+            temp = N / 5;
+            cout << N - temp << endl;
         }
     }
     return 0;
