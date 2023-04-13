1b6f3b56ad183b22d278f6e685b86bc2b5eeb11a
Haleshot <hari.leo03@gmail.com>
2023-04-13 22:50:57 +0530

    Updated

--- Codechef/Presents/Presents.py
+++ Codechef/Presents/Presents.py
@@ -3,10 +3,8 @@ while T:
     T-= 1
     N = int(input())
     if N % 5 == 0:
-        temp = N / 5
+        temp = int(N / 5)
         print(4 * temp)
-    elif (N < 5):
-        print(N)
     else:
         temp = N // 5
         print(N - temp)
