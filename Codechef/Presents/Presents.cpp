f0230364e6d0d72dc650bb495dab5a74f5b8f9d7
Haleshot <hari.leo03@gmail.com>
2023-04-13 22:51:52 +0530

    Updated

---
+++ Codechef/Presents/Presents.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, temp;
+        cin >> N;
+        if (N % 5 == 0)
+        {
+
+        }
+    }
+    return 0;
+}
