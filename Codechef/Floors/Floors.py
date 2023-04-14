e0e4c3cd55e57497672d8302316055387fa79ac6
Haleshot <hari.leo03@gmail.com>
2023-04-14 23:06:03 +0530

    Updated

--- Codechef/Floors/Floors.py
+++ Codechef/Floors/Floors.py
@@ -1,8 +1,8 @@
+import math
 T = int(input())
 while T:
     T -= 1
     X, Y = map(int, input().split())
-    if X % 10 == 0 and Y % 10 == 0:
-        print(X - Y)
-    else:
-        pass
+    x = math.ceil(X / 10)
+    y = math.ceil(Y / 10)
+    print(abs(x - y))
