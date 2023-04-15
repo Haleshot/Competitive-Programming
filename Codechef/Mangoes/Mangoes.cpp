42a7f555424306471933f0200dd48a930b4a42b0
Haleshot <hari.leo03@gmail.com>
2023-04-15 23:11:32 +0530

    Updated

---
+++ Codechef/Mangoes/Mangoes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X, Y, Z;
+        cin >> X >> Y >> Z;
+        cout << (Z - Y)/X << endl;
+    }
+    return 0;
+}
