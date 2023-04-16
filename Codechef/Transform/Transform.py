13f0f74cdf833c06f71aa4a6b2cb1e21742519dd
Haleshot <hari.leo03@gmail.com>
2023-04-16 23:12:00 +0530

    Updated

--- Codechef/Transform/Transform.py
+++ Codechef/Transform/Transform.py
@@ -1,4 +1,10 @@
 T = int(input())
 while T:
     T-= 1
-    
+    X = int(input())
+    if X % 3 == 0:
+        print("NORMAL")
+    elif X % 3 == 1:
+        print("HIGH")
+    else:
+        print("SMALL")
