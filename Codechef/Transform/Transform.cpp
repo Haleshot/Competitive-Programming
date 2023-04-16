92b7209ce99c790b259100d75a496ed400ab5401
Haleshot <hari.leo03@gmail.com>
2023-04-16 23:13:54 +0530

    Updated

--- Codechef/Transform/Transform.cpp
+++ Codechef/Transform/Transform.cpp
@@ -9,7 +9,18 @@ int main()
     {
         int X;
         cin >> X;
-        
+        if (X % 3 == 0)
+        {
+            cout << "NORMAL" << endl;
+        }
+        else if (X % 3 == 1)
+        {
+            cout << "HUGE" << endl;
+        }
+        else
+        {
+            cout << "SMALL" << endl;
+        }
     }
     return 0;
 }
