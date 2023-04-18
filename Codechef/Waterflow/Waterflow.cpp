ad6f7bbe744fbe75c0244d3621ca26ac06edcdd4
Haleshot <hari.leo03@gmail.com>
2023-04-18 22:26:34 +0530

    Updated

--- Codechef/Waterflow/Waterflow.cpp
+++ Codechef/Waterflow/Waterflow.cpp
@@ -7,22 +7,22 @@ int main()
     cin >> T;
     while (T--)
     {
-            int W, X, Y, Z, c, total;
-            cin >> W >> X >> Y >> Z;
-            c = Y * Z;
-            total = W + c;
-            if (total > X)
-            {
-                cout << "Overflow" << endl;
-            } 
-            if (total < X)
-            {                
-                cout << "Unfilled" << endl;
-            }
-            if (total == X)
-            {
-                cout << "Filled" << endl;
-            }
+        int W, X, Y, Z, c, total;
+        cin >> W >> X >> Y >> Z;
+        c = Y * Z;
+        total = W + c;
+        if (total > X)
+        {
+            cout << "Overflow" << endl;
+        } 
+        if (total < X)
+        {                
+            cout << "Unfilled" << endl;
+        }
+        if (total == X)
+        {
+            cout << "Filled" << endl;
+        }
     }
     return 0;
 }
