1994395396af8606d12a3c2bc25d269dbbe9b1c1
Haleshot <hari.leo03@gmail.com>
2023-04-18 22:20:05 +0530

    Updated

--- Codechef/Waterflow/Waterflow.py
+++ Codechef/Waterflow/Waterflow.py
@@ -2,4 +2,12 @@ T = int(input())
 while T:
     T -= 1
     W, X, Y, Z = map(int, input().split())
-    
+    initial = W + X
+    total = Y * Z
+    if total > initial:
+        print("Overflow")
+    if total < initial:
+        print("Unfilled")
+    if total = initial:
+        print("Filled")
+
