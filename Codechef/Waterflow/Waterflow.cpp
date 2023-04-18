f36a5e2cf9e3eb4bd2a216bbd663050f4060fb15
Haleshot <hari.leo03@gmail.com>
2023-04-18 22:23:37 +0530

    Updated

---
+++ Codechef/Waterflow/Waterflow.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int T;
+        cin >> T;
+        while (T--)
+        {
+            int W, X, Y, Z;
+            cin >> W >> X >> Y >> Z;
+        }
+    }
+    return 0;
+}
