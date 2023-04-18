e40b4e10af9d788b927998c141a1693359f56a62
Haleshot <hari.leo03@gmail.com>
2023-04-18 22:25:25 +0530

    Updated

--- Codechef/Waterflow/Waterflow.cpp
+++ Codechef/Waterflow/Waterflow.cpp
@@ -14,6 +14,20 @@ int main()
             int W, X, Y, Z, c, total;
             cin >> W >> X >> Y >> Z;
             c = Y * Z;
+            total = W + c;
+            if (total > X)
+            {
+                cout << "Overflow" << endl;
+            }
+                
+            if (total < X)
+            {                
+                cout << "Unfilled" << endl;
+            }
+            if (total == X)
+            {
+                cout << "Filled" << endl;
+            }
             
         }
     }
