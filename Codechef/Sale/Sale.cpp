af5f7da2d11d67c58b1ab4bdfa95f825bdf74adc
Haleshot <hari.leo03@gmail.com>
2023-04-19 22:26:19 +0530

    Updated

---
+++ Codechef/Sale/Sale.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+bool compare(int a, int b)
+{
+    return(a < b);
+}
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int A, B, C, minimum;
+        cin >> A >> B >> C;
+        minimum = min({A, B, C}, compare);
+        cout << ((A + B + C) - minimum) << endl;
+
+    }
+    return 0;
+}
