6654374c8b71745b5225ae6bc2d22f9284af3beb
Haleshot <hari.leo03@gmail.com>
2023-04-20 22:07:47 +0530

    Updasted

---
+++ Codechef/Ludo/Ludo.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X;
+        cin >> X;
+        if (X != 6)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+    }
+    return 0;
+}
