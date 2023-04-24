14f709e158461df56fcba60f050da5fbfe2c0a45
Haleshot <hari.leo03@gmail.com>
2023-04-24 23:10:30 +0530

    Updated

---
+++ Codechef/Timely/Timely.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X;
+        cin >> X;
+        if (X >= 30)
+        {
+            cout << "YES";
+        }
+    }
+    return 0;
+}
