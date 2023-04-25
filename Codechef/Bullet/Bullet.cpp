507c0ab8dc739a953b61eac1374f7ff30eff739a
Haleshot <hari.leo03@gmail.com>
2023-04-25 22:18:50 +0530

    Update

---
+++ Codechef/Bullet/Bullet.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X, Y, Z, t;
+        cin >> X >> Y >> Z;
+        t = Z/X;
+    }
+    return 0;
+}
