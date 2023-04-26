a926ab44398cd8e617d12c28ec3ce890d0b813fe
Haleshot <hari.leo03@gmail.com>
2023-04-26 22:11:16 +0530

    Updated

--- Codechef/Creds/Creds.cpp
+++ Codechef/Creds/Creds.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main()
-[
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X, Y, Z;
+        cin >> X >> Y >> Z;
+        cout << (4 * X) + (2 * Y) << endl;
+    }
     return 0;
-]
+}
