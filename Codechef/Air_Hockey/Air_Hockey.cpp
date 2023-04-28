2476868004f933921f7c5addad5a392a07aa094e
Haleshot <hari.leo03@gmail.com>
2023-04-28 22:44:00 +0530

    Updated

---
+++ Codechef/Air_Hockey/Air_Hockey.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int A, B;
+        cin >> A >> B;
+        A = 7 - A;
+        B = 7 - B;
+        cout << 
+    }
+    return 0;
+}
--- Codechef/Air_Hockey/Air_Hockey.py
+++ Codechef/Air_Hockey/Air_Hockey.py
@@ -2,4 +2,6 @@ T = int(input())
 while T:
     T -= 1
     A, B = map(int, input().split())
-    print(max(A, B))
+    A = 7 - A
+    B = 7 - B
+    print(min(A, B))
