490a78ff8a0ec0fa5f311ec102de5e2765fadb01
Haleshot <hari.leo03@gmail.com>
2023-04-29 22:11:13 +0530

    Updated

---
+++ Codechef/NEARESTEXIT/NEARESTEXIT.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X;
+        cin >> X;
+        if (X <= 50)
+        {
+            cout << "LEFT" << endl;
+        }
+    }
+    return 0;
+}
