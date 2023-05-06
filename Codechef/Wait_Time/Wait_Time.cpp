561fab34ed64bef9007e077225dcce49b3903a11
Haleshot <hari.leo03@gmail.com>
2023-05-06 19:15:42 +0530

    Updated

---
+++ Codechef/Wait_Time/Wait_Time.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int K, X;
+        cin >> K >> X;
+        cout << (7 * K) - X << endl;
+    }
+}
