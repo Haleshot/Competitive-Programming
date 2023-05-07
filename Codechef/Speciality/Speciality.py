77a866e7fbaa58977084f32d99f86f21ccb48f77
Haleshot <hari.leo03@gmail.com>
2023-05-07 20:15:36 +0530

    Updated

--- Codechef/Speciality/Speciality.py
+++ Codechef/Speciality/Speciality.py
@@ -2,4 +2,13 @@ T = int(input())
 while T:
     T -= 1
     X, Y, Z = map(int, input().split())
-    
+    if X > Y:
+        if X > Z:
+            print("Setter")
+        else:
+            print("Editorialist")
+    else:
+        if Y > Z:
+            print("Tester")
+        else:
+            print("Editorialist")
