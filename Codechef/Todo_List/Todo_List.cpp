282340f34bd57903333a82e289bb7b2d823008ec
Haleshot <hari.leo03@gmail.com>
2023-05-08 20:39:38 +0530

    Updated

---
+++ Codechef/Todo_List/Todo_List.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        int l[N];
+        for(int i = 0; i <= N;i ++)
+        {
+            cin >> l[i];
+        }
+        
+    }
+    return 0;
+}
