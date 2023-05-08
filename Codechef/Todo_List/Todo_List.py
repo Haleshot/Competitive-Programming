8f4db95e4c1f6b95398c7712c347c9ded0f5b30b
Haleshot <hari.leo03@gmail.com>
2023-05-08 20:36:53 +0530

    Updated

--- Codechef/Todo_List/Todo_List.py
+++ Codechef/Todo_List/Todo_List.py
@@ -3,5 +3,5 @@ while T:
     T -= 1
     N = int(input())
     l = list(map(int, input().split()))
-    num = [i for i in l if i < 1000]
-    print(num.count)
+    num = [i for i in l if i >= 1000]
+    print(len(num))
