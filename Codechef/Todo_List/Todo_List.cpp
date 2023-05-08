8aa47a6991bfebb6a06a31548b2ed04ac1f712c9
Haleshot <hari.leo03@gmail.com>
2023-05-08 20:45:00 +0530

    Updated

--- Codechef/Todo_List/Todo_List.cpp
+++ Codechef/Todo_List/Todo_List.cpp
@@ -10,11 +10,11 @@ int main()
         int N, c = 0;
         cin >> N;
         int l[N];
-        for(int i = 0; i <= N;i ++)
+        for(int i = 0; i < N;i ++)
         {
             cin >> l[i];
         }
-        for (int i = 0;i < N; i++)
+        for (int i = 0; i < N; i++)
         {
             if (l[i] >= 1000)
             {
