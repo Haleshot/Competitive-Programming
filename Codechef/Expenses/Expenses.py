3e4ee8ce57bc64662e100eebb22bab9f44e2eaca
Haleshot <hari.leo03@gmail.com>
2023-05-10 21:59:37 +0530

    Updated

--- Codechef/Expenses/Expenses.py
+++ Codechef/Expenses/Expenses.py
@@ -2,4 +2,11 @@ T = int(input())
 while T:
     T -= 1
     N, X = map(int, input().split())
-    
+    salary = 2**X
+    if N == 1:
+        print(int(salary - 0.5*salary))
+    else:
+        salary -= 0.5*salary
+        for i in range(N - 1):
+            salary -= 0.5*salary
+        print(int(salary))
