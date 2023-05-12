3c67ab20f1e303e568300143bdd32073185a588a
Haleshot <hari.leo03@gmail.com>
2023-05-12 21:56:30 +0530

    Updated

---
+++ Codechef/Equal_Dist/Equal_Dist.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int A, B;
+        cin >> A >> B;
+        if ((A + B) % 2 == 0)
+        {
+            cout << "YES" << endl;
+        }
+
+    }
+    return 0;
+}
