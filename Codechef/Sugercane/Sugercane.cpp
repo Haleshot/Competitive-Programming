44d174c873305cb04fe9c0d88589b6b61b2418c1
Haleshot <hari.leo03@gmail.com>
2023-05-17 21:49:12 +0530

    Updated

--- Codechef/Sugercane/Sugercane.cpp
+++ Codechef/Sugercane/Sugercane.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main()
 {
-    
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, tot;
+        cin >> N;
+        tot = 100 * N;
+        cout << int(0.3 * N) << endl;
+    }
     return 0;
 }
