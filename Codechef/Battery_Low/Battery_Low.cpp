5661e336c122a039821d20207f3ff19dc5034c80
Haleshot <hari.leo03@gmail.com>
2023-05-18 21:46:05 +0530

    Updated

--- Codechef/Battery_Low/Battery_Low.cpp
+++ Codechef/Battery_Low/Battery_Low.cpp
@@ -5,5 +5,18 @@ int main()
 {
     int T;
     cin >> T;
+    while (T--)
+    {
+        int X;
+        cin >> X;
+        if (X <= 15)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
     return 0;
 }
