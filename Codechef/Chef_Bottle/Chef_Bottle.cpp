22cc0db1db023bb4cdc2353936639e3129c080a4
Haleshot <hari.leo03@gmail.com>
2023-05-20 22:21:28 +0530

    Initial Commity

---
+++ Codechef/Chef_Bottle/Chef_Bottle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, X, K, num;
+        cin >> N >> X >> K;
+    }
+    return 0;
+}
