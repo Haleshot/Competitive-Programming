7a1690ea545ef5453e6fa3f9c56fbd10e9b3c269
Haleshot <hari.leo03@gmail.com>
2023-05-20 22:20:16 +0530

    Updated

--- Codechef/Chef_Bottle/Chef_Bottle.py
+++ Codechef/Chef_Bottle/Chef_Bottle.py
@@ -2,3 +2,11 @@ T = int(input())
 while T:
     T -= 1
     N, X, K = map(int, input().split())
+    if X <= K:
+        num = K//X
+        if (num >= N):
+            print(N)
+        else:
+            print(num)
+    else:
+        print(K//X)
