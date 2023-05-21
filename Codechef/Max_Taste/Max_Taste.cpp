0832cb6ee4774b9403a5f195ed7a12e44607ddc9
Haleshot <hari.leo03@gmail.com>
2023-05-21 21:33:54 +0530

    Initial Commit

---
+++ Codechef/Max_Taste/Max_Taste.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+        
+    }
+    return 0;
+}
