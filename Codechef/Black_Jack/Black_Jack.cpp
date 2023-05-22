6bd908f535817c48b7b52c2a535319c19be9676b
Haleshot <hari.leo03@gmail.com>
2023-05-22 21:34:18 +0530

    Updated

--- Codechef/Black_Jack/Black_Jack.cpp
+++ Codechef/Black_Jack/Black_Jack.cpp
@@ -4,5 +4,22 @@ using namespace std;
 int main()
 {
     int T;
+    cin >> T;
+    while (T--)
+    {
+        int A, B, C;
+        cin >> A >> B;
+        C = 21 - (A + B);
+        if (C <= 10)
+        {
+            cout << C << endl;
+        }
+        else
+        {
+            cout << -1 << endl;
+        }
+
+
+    }
     return 0;
 }
