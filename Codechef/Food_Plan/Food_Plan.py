6e1516bca379b68708b842e85e78e15f8dcdfd16
Haleshot <hari.leo03@gmail.com>
2023-05-23 22:30:52 +0530

    Updated

--- Codechef/Food_Plan/Food_Plan.py
+++ Codechef/Food_Plan/Food_Plan.py
@@ -1,4 +1,12 @@
 T = int(input())
 while T:
     T -= 1
-    
+    N, M = map(int, input().split())
+    N -= 0.1*N
+    least = min(M, N)
+    if least == N:
+        print("ONLINE")
+    elif least == M:
+        print("DINING")
+    else:
+        print("EITHER")
