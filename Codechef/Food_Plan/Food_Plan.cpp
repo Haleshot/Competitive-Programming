123c2ed02f0e812f97b7d9f9fe522088e66d247c
Haleshot <hari.leo03@gmail.com>
2023-05-23 22:36:59 +0530

    Updated

---
+++ Codechef/Food_Plan/Food_Plan.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, M, least;
+        cin >> N >> M;
+        N -= 0.1*N;
+        least = min(N, M);
+
+        if (M == N)
+        {
+            cout << "EITHER" << endl;
+        }
+        else if (least == N)
+        {
+            cout << "ONLINE" << endl;
+        }
+        else if (least == M)
+        {
+            cout << "DINING" << endl;
+        }
+
+    }
+    return 0;
+}
