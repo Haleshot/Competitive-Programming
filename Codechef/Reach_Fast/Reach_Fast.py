727f1a4f84b0240d3ef539b714a05b2a83510454
Haleshot <hari.leo03@gmail.com>
2023-05-24 21:25:31 +0530

    Updated

--- Codechef/Reach_Fast/Reach_Fast.py
+++ Codechef/Reach_Fast/Reach_Fast.py
@@ -1,11 +1,12 @@
+import math
 T = int(input())
 while T:
     T -= 1
     X, Y, K = map(int, input().split())
     if X <= Y:
-        least = round((Y - X)/K)
+        least = math.ceil((Y - X)/K)
     elif X >= Y:
-        least = round((X - Y)/K)
+        least = math.ceil((X - Y)/K)
     else:
         print(0)
     print(least)
