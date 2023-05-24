f6463a3c98956a5da2794f1bd4a056bd55110138
Haleshot <hari.leo03@gmail.com>
2023-05-24 21:24:50 +0530

    Updated

--- Codechef/Reach_Fast/Reach_Fast.py
+++ Codechef/Reach_Fast/Reach_Fast.py
@@ -3,9 +3,9 @@ while T:
     T -= 1
     X, Y, K = map(int, input().split())
     if X <= Y:
-        least = (Y - X)//K
+        least = round((Y - X)/K)
     elif X >= Y:
-        least = (X - Y)//K
+        least = round((X - Y)/K)
     else:
         print(0)
     print(least)
