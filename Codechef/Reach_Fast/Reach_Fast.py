7d5f2c3c4cae754acc4ec20b94c23bc1addd7af1
Haleshot <hari.leo03@gmail.com>
2023-05-24 21:23:58 +0530

    Updated

--- Codechef/Reach_Fast/Reach_Fast.py
+++ Codechef/Reach_Fast/Reach_Fast.py
@@ -2,4 +2,10 @@ T = int(input())
 while T:
     T -= 1
     X, Y, K = map(int, input().split())
-    
+    if X <= Y:
+        least = (Y - X)//K
+    elif X >= Y:
+        least = (X - Y)//K
+    else:
+        print(0)
+    print(least)
