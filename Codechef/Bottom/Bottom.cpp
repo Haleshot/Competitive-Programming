30c6e151c9c2942bc28c4a02c0892ff06c39d21d
Haleshot <hari.leo03@gmail.com>
2023-05-25 21:55:33 +0530

    Updated

---
+++ Codechef/Bottom/Bottom.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X;
+        cin >> X;
+        cout << (7 - X) << endl;
+    }
+    return 0;
+}
