dd7caad4d490ad6b7a69e683bbe5dba0178f0800
Haleshot <hari.leo03@gmail.com>
2023-05-26 22:10:59 +0530

    Updated

--- Codechef/Credits/Credits.py
+++ Codechef/Credits/Credits.py
@@ -1,4 +1,10 @@
 T = int(input())
 while T:
     T -= 1
-    
+    X = int(input())
+    if X > 65:
+        print("OVERLOAD")
+    elif X < 35:
+        print("UNDERLOAD")
+    else:
+        print("NORMAL")
