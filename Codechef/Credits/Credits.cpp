f1d01836ea8f3b7de4a4dee1b8b6e1e52f952a1c
Haleshot <hari.leo03@gmail.com>
2023-05-26 22:12:59 +0530

    Updated

--- Codechef/Credits/Credits.cpp
+++ Codechef/Credits/Credits.cpp
@@ -7,7 +7,20 @@ int main()
     cin >> T;
     while (T--)
     {
-        
+        int X;
+        cin >> X;
+        if (X > 65)
+        {
+            cout << "OVERLOAD" << endl;
+        }
+        if (X < 35)
+        {
+            cout << "UNDERLOAD" << endl;
+        }
+        else
+        {
+            cout << "NORMAL" << endl;
+        }
     }
     return 0;
 }
