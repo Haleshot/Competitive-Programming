9db5e02ab815d5169d2f8bcf3a5e26a9a70029cb
Haleshot <hari.leo03@gmail.com>
2023-05-27 22:33:58 +0530

    Updated

--- Codechef/Mileage/Mileage.py
+++ Codechef/Mileage/Mileage.py
@@ -2,4 +2,11 @@ T = int(input())
 while T:
     T -= 1
     N, X, Y, A, B = map(int, input().split())
-    
+    petrol = (X * A) * N
+    diesel = (Y * B) * N
+    if petrol > diesel:
+        print("DIESEL")
+    elif petrol < diesel:
+        print("PETROL")
+    else:
+        print("ANY")
