86d841d9ed279d69676a29e64369dbd17e76ac45
Haleshot <hari.leo03@gmail.com>
2023-05-27 22:38:46 +0530

    Updated

---
+++ Codechef/Mileage/Mileage.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, X, Y, A, B, petrol, diesel;
+        cin >> N >> X >> Y >> A >> B;
+        petrol = (N/A)* X;
+        diesel = (N/B)* Y;
+        if (petrol > diesel)
+        {
+            cout << "DIESEL" << endl;
+        }
+        else if (petrol < diesel)
+        {
+            cout << "PETROL" << endl;
+        }
+        else
+        {
+            
+        }
+    }
+    return 0;
+}
