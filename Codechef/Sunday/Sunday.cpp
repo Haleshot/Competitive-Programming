e4e51b828310969eaafe54ae2d52753860669659
Haleshot <hari.leo03@gmail.com>
2023-05-29 22:20:54 +0530

    Update

--- Codechef/Sunday/Sunday.cpp
+++ Codechef/Sunday/Sunday.cpp
@@ -9,11 +9,20 @@ int main()
     {
         int N;
         cin >> N;
-        char A[N];
+        int A[N];
         for (int i = 0;i < N; i++)
         {
             cin >> A[i];
         }
+        int c = 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (A[i] == 6 || A[i] == 7 || A[i] == 13 || A[i] == 14 || A[i] == 20 || A[i] == 21 || A[i] == 27 || A[i] == 28)
+            {
+                c += 1;
+            }
+        }
+        cout << (8 + (N - c)) << endl;
         
     }
     return 0;
