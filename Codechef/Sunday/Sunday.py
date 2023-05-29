bd8b34f538a57e2fc3ecde92451681d051b43feb
Haleshot <hari.leo03@gmail.com>
2023-05-29 21:38:08 +0530

    Updated

--- Codechef/Sunday/Sunday.py
+++ Codechef/Sunday/Sunday.py
@@ -2,6 +2,8 @@ T = int(input())
 while T:
     T -= 1
     N = int(input())
-    A = list(map(int(input())))
-    weekend = [6, 7, 13, 14, 20, 21, 27, 28]
+    A = input()
     
+    weekend = [6, 7, 13, 14, 20, 21, 27, 28]
+    matching = [i for i in A if i in weekend]
+    print(30 - (A - matching) - 8)
