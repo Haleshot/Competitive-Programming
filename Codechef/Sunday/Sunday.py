3c27ef7f554d979db5408e5f0da8dd804ebec054
Haleshot <hari.leo03@gmail.com>
2023-05-29 22:04:35 +0530

    Update

--- Codechef/Sunday/Sunday.py
+++ Codechef/Sunday/Sunday.py
@@ -3,7 +3,11 @@ while T:
     T -= 1
     N = int(input())
     A = input()
-    
+    B = A.split()
+    for i in range(len(B)):
+        B[i] = int(B[i])
     weekend = [6, 7, 13, 14, 20, 21, 27, 28]
-    matching = [i for i in A if i in weekend]
-    print(30 - (A - matching) - 8)
+    matching = [i for i in B if i in weekend]
+    print()
+    print((len(B) - len(matching)) + 8)
+    print()
