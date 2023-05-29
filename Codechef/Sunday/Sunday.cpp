21edbb0ef9053487bc56e9148fbfb553516a864c
Haleshot <hari.leo03@gmail.com>
2023-05-29 22:17:20 +0530

    Updated

--- Codechef/Sunday/Sunday.cpp
+++ Codechef/Sunday/Sunday.cpp
@@ -7,6 +7,13 @@ int main()
     cin >> T;
     while (T--)
     {
+        int N;
+        cin >> N;
+        char A[N];
+        for (int i = 0;i < N; i++)
+        {
+            cin >> A[i];
+        }
         
     }
     return 0;
