50a930501de67e6a5413bafc225aaf8ffba1829a
Haleshot <hari.leo03@gmail.com>
2023-05-30 22:19:08 +0530

    Updated

--- Codechef/Penalty/Penalty.py
+++ Codechef/Penalty/Penalty.py
@@ -1,4 +1,14 @@
 T = int(input())
 while T:
     T -= 1
-    
+    A = list(map(int, input().split()))
+    odd = [A[i] for i in range(len(A)) if (i % 2 == 1)]
+    even = [A[i] for i in range(len(A)) if (i % 2 == 0)]
+    odd_one_count = [i for i in odd if i == 1]
+    even_one_count = [i for i in even if i == 1]
+    if (odd_one_count > even_one_count):
+        print(2)
+    elif (odd_one_count < even_one_count):
+        print(1)
+    else:
+        print(0)
