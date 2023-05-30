a6aeebf2a28fd059c057581216516c4ff7fdb357
Haleshot <hari.leo03@gmail.com>
2023-05-30 22:37:26 +0530

    Updated

--- Codechef/Penalty/Penalty.cpp
+++ Codechef/Penalty/Penalty.cpp
@@ -24,35 +24,35 @@ int main()
                 even[i] = A[i];
             }
         }
-        int odd_one_count[5], even_one_count[5];
-        for (int i = 0; i < 5; i++)
-        {
-            if (odd[i] == 1)
-            {
-                odd_one_count[i] = A[i];
-            }
-        }
-        for (int i = 0; i < 5; i++)
-        {
-            if (even[i] == 1)
-            {
-                even_one_count[i] = A[i];
-            }
-        }
+        // int odd_one_count[5], even_one_count[5];
+        // for (int i = 0; i < 5; i++)
+        // {
+        //     if (odd[i] == 1)
+        //     {
+        //         odd_one_count[i] = A[i];
+        //     }
+        // }
+        // for (int i = 0; i < 5; i++)
+        // {
+        //     if (even[i] == 1)
+        //     {
+        //         even_one_count[i] = A[i];
+        //     }
+        // }
 
 
-        if (odd_one_count > even_one_count)
-        {
-            cout << 2 << endl;;
-        }
-        else if (odd_one_count < even_one_count)
-        {
-            cout << 1 << endl;;
-        }
-        else
-        {
-            cout << 0 << endl;
-        }
+        // if (odd_one_count > even_one_count)
+        // {
+        //     cout << 2 << endl;;
+        // }
+        // else if (odd_one_count < even_one_count)
+        // {
+        //     cout << 1 << endl;;
+        // }
+        // else
+        // {
+        //     cout << 0 << endl;
+        // }
 
 
 
