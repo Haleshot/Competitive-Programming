f807cc13ae1b987798fbdec844da835ceae171fb
Haleshot <hari.leo03@gmail.com>
2023-05-30 22:20:55 +0530

    Updated

--- Codechef/Penalty/Penalty.cpp
+++ Codechef/Penalty/Penalty.cpp
@@ -7,6 +7,11 @@ int main()
     cin >> T;
     while (T--)
     {
+        int A[10];
+        for (int i = 0; i < 10; i++)
+        {
+            cin >> A[i];
+        }
         
     }
     return 0;
