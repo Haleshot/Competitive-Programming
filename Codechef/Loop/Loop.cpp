4ac60a70881eb160e09ddc0d7f097cb0a926415c
Haleshot <hari.leo03@gmail.com>
2023-05-31 22:23:06 +0530

    Updated

--- Codechef/Loop/Loop.cpp
+++ Codechef/Loop/Loop.cpp
@@ -7,7 +7,12 @@ int main()
     cin >> T;
     while (T--)
     {
-        
+        int A, B, M, t1, t2;
+        cin >> A >> B >> M;
+        t1 = abs(A - B);
+        t2 = abs(M - t1);
+        cout << min(t1, t2) << endl;
+
     }
     return 0;
 }
