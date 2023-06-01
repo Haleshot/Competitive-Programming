5a7db08e329aea4c1de9fb6d367dd464c53224ab
Haleshot <hari.leo03@gmail.com>
2023-06-01 22:10:01 +0530

    Updated

--- Codechef/Visa/Visa.py
+++ Codechef/Visa/Visa.py
@@ -2,4 +2,7 @@ T = int(input())
 while T:
     T -= 1
     x1, x2, y1, y2, z1, z2 = map(int, input().split())
-    
+    if x2 >= x1 and y2 >= y1 and z2 <= z1:
+        print("YES")
+    else:
+        print("NO")
