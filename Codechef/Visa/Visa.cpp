972a858b844412e0c5cd73b7f88844a6a4c0f689
Haleshot <hari.leo03@gmail.com>
2023-06-01 22:13:09 +0530

    Updated

--- Codechef/Visa/Visa.cpp
+++ Codechef/Visa/Visa.cpp
@@ -9,7 +9,14 @@ int main()
     {
         int x1, x2, y1, y2, z1, z2;
         cin >> x1 >> x2 >> y1 >> y2 >> z1 >> z2;
-        
+        if ((x2 >= x1) && (y2 >= y1) && (z2 <= z1))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
     return 0;
 }
