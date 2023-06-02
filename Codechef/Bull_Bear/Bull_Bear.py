119db0c67b8196cd244e8f83f5ccb2ba9078e26e
Haleshot <hari.leo03@gmail.com>
2023-06-02 23:04:03 +0530

    Updated

--- Codechef/Bull_Bear/Bull_Bear.py
+++ Codechef/Bull_Bear/Bull_Bear.py
@@ -1,4 +1,10 @@
 T = int(input())
 while T:
     T -= 1
-    
+    X, Y = map(int, input().split())
+    if Y > X:
+        print("PROFIT")
+    elif Y < X:
+        print("LOSS")
+    else:
+        print("NEUTRAL")
