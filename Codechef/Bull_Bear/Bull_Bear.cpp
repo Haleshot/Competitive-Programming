94468658d55e50ab52803bcb108397dfd2b5541e
Haleshot <hari.leo03@gmail.com>
2023-06-02 23:05:22 +0530

    Updated

--- Codechef/Bull_Bear/Bull_Bear.cpp
+++ Codechef/Bull_Bear/Bull_Bear.cpp
@@ -9,6 +9,18 @@ int main()
     {
         int X, Y;
         cin >> X >> Y;
+        if (Y > X)
+        {
+            cout << "PROFIT" << endl;
+        }
+        else if (Y > X)
+        {
+            cout << "LOSS" << endl;
+        }
+        else
+        {
+            cout << "NEUTRAL" << endl;
+        }
     }
     return 0;
 }
