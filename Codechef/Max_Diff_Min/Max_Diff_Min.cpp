f5b2944a2aaff2156b768c802bfffe60e0ff57d0
Haleshot <hari.leo03@gmail.com>
2023-06-03 21:48:25 +0530

    Initial Commit

---
+++ Codechef/Max_Diff_Min/Max_Diff_Min.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int A, B, C;
+        cin >> A >> B >> C;
+        cout << (max(A, B, C) - min(A, B, C)) << endl;
+    }
+    return 0;
+}
