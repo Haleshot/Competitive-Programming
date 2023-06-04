50fb2a49d22a1602109a3d95e4798dc2151e1a2f
Haleshot <hari.leo03@gmail.com>
2023-06-04 22:05:29 +0530

    Updated

--- Codechef/Existence/Existence.py
+++ Codechef/Existence/Existence.py
@@ -1,4 +1,10 @@
 T = int(input())
 while T:
     T -= 1
-    
+    X, Y = map(int, input().split())
+    LHS = X**4 + (4 * Y**2)
+    RHS = 4 * (X**2 * Y)
+    if (LHS == RHS):
+        print("YES")
+    else:
+        print("NO")
