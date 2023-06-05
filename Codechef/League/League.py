6fd320a865212b1ef4dfa94e0b70ba204a2dccf7
Haleshot <hari.leo03@gmail.com>
2023-06-05 21:57:37 +0530

    Updated

--- Codechef/League/League.py
+++ Codechef/League/League.py
@@ -3,6 +3,6 @@ while T:
     T -= 1
     N = int(input())
     if N % 2 == 0:
-        print((N/2) * 3)
+        print(int((N/2) * 3))
     else:
-        print((N - 1/2) * 3)
+        print(int(((N - 1)/2) * 3))
