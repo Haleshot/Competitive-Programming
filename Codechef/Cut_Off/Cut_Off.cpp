23af245d5a2998f6417b588377ea5c5c1b9fd384
Haleshot <hari.leo03@gmail.com>
2023-06-06 21:26:05 +0530

    Updated

--- Codechef/Cut_Off/Cut_Off.cpp
+++ Codechef/Cut_Off/Cut_Off.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -8,6 +9,16 @@ int main()
     while (T--)
     {
         int N, X;
+        cin >> N >> X;
+        int A[N];
+        for (int i = 0; i < N; i++)
+        {
+            cin >> A[i];
+        }
+        sort(A, A + N);
+        cout << A[N - X] - 1 << endl;
+
+
     }
     return 0;
 }
