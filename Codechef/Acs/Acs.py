55d712769888a3f31437415ec99194bc0587b29d
Haleshot <hari.leo03@gmail.com>
2023-06-07 20:47:55 +0530

    Updated

--- Codechef/Acs/Acs.py
+++ Codechef/Acs/Acs.py
@@ -2,4 +2,13 @@ T = int(input())
 while T:
     T -= 1
     P = int(input())
-    
+    if P % 100 == 0:
+        print(P/100)
+    elif P % 100 != 0:
+        c = P % 100
+        if len(str(c)) == 2:
+            print(-1)
+        else:
+            print(int(c + 1))
+    else:
+        print(-1)
