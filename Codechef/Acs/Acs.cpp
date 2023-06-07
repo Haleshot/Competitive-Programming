b1415db993a08ea09f3b107ea44c3504a09ef9a2
Haleshot <hari.leo03@gmail.com>
2023-06-07 21:02:46 +0530

    Updated

--- Codechef/Acs/Acs.cpp
+++ Codechef/Acs/Acs.cpp
@@ -7,8 +7,17 @@ int main()
     cin >> T;
     while (T--)
     {
-        int P;
+        int P, R;
         cin >> P;
+        R = (P % 100) + (P / 100);
+        if (R <= 10)
+        {
+            cout << R << endl;
+        }
+        else
+        {
+            cout << -1  << endl;
+        }
     }
     return 0;
 }
