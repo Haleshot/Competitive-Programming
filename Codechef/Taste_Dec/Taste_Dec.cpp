6cb5260d1e542121a01d0677f73aaed76006180c
Haleshot <hari.leo03@gmail.com>
2023-06-09 21:57:22 +0530

    Updated

--- Codechef/Taste_Dec/Taste_Dec.cpp
+++ Codechef/Taste_Dec/Taste_Dec.cpp
@@ -8,6 +8,19 @@ int main()
     while (T--)
     {
         int X, Y;
+        cin >> X >> Y;
+        if ((2 * X) > (5 * Y))
+        {
+            cout << "Chocolate" << endl;
+        }
+        else if ((2 * X) < (5 * Y))
+        {
+            cout << "Candy" << endl;
+        }
+        else
+        {
+            cout << "Either" << endl;
+        }
     }
     return 0;
 }
