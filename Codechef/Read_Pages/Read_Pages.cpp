ec9a8ab73a5de20240d3d34a425f559e1fa5b73d
Haleshot <hari.leo03@gmail.com>
2023-06-10 22:19:44 +0530

    Updated

--- Codechef/Read_Pages/Read_Pages.cpp
+++ Codechef/Read_Pages/Read_Pages.cpp
@@ -7,7 +7,16 @@ int main()
     cin >> T;
     while (T--)
     {
-        
+        int N, X, Y;
+        cin >> N >> X >> Y;
+        if ((X * Y) >= N)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
     }
     return 0;
 }
