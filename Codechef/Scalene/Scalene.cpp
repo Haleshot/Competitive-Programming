a14ca20c035fccd25633bc2f7d8b406a9e1a846e
Haleshot <hari.leo03@gmail.com>
2023-06-11 22:42:59 +0530

    Updated

--- Codechef/Scalene/Scalene.cpp
+++ Codechef/Scalene/Scalene.cpp
@@ -11,19 +11,8 @@ int main()
     {
         int A, B, C, scalene[3];
         cin >> A >> B >> C;
+        
     }
     return 0;
 }
  
-// int main()
-// {
-//     int arr[] = {1, 3, 5, 7, 3, 9};
-//     int n = sizeof(arr) / sizeof(*arr);
- 
-//     std::unordered_set<int> distinct(std::begin(arr), std::end(arr));
-//     bool hasDuplicates = n != distinct.size();
- 
-//     if (hasDuplicates) {
-//     }
-//     return 0;
-// }
