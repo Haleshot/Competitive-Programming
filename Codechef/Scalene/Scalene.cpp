e856aaaec146d12536b30e570fb8c8a0726b98c1
Haleshot <hari.leo03@gmail.com>
2023-06-11 22:41:53 +0530

    Initial Commit

--- Codechef/Scalene/Scalene.cpp
+++ Codechef/Scalene/Scalene.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unordered_set>
+#include <iterator>
 using namespace std;
 
 int main()
@@ -7,9 +9,21 @@ int main()
     cin >> T;
     while (T--)
     {
-        int A, B, C;
+        int A, B, C, scalene[3];
         cin >> A >> B >> C;
-        
     }
     return 0;
 }
+ 
+// int main()
+// {
+//     int arr[] = {1, 3, 5, 7, 3, 9};
+//     int n = sizeof(arr) / sizeof(*arr);
+ 
+//     std::unordered_set<int> distinct(std::begin(arr), std::end(arr));
+//     bool hasDuplicates = n != distinct.size();
+ 
+//     if (hasDuplicates) {
+//     }
+//     return 0;
+// }
