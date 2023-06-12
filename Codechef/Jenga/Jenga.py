7978d9b63be74eedc010cdd4fa68389cf1c7d755
Haleshot <hari.leo03@gmail.com>
2023-06-12 22:18:15 +0530

    Updated

--- Codechef/Jenga/Jenga.py
+++ Codechef/Jenga/Jenga.py
@@ -2,7 +2,7 @@ T = int(input())
 while T:
     T -= 1
     N, X = map(int, input().split())
-    if X < N:
-        print("NO")
-    elif (X % N == 0):
+    if (X % N == 0):
         print("YES")
+    else:
+        print("NO")
