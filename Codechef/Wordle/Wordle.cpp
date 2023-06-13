57e1a41a10b09d412e8fab43111c91abb3636068
Haleshot <hari.leo03@gmail.com>
2023-06-13 22:15:23 +0530

    Initial Commit

---
+++ Codechef/Wordle/Wordle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Test;
+    cin >> Test;
+    while (Test--)
+    {
+        char S, T;
+        cin >> S >> T;
+        for (int i = 0; i = 5; i++)
+        {
+            
+        }
+    }
+    return 0;
+}
