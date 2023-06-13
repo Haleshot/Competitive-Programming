2b036a82bc5eb45f7775b884af873d7a59abe222
Haleshot <hari.leo03@gmail.com>
2023-06-13 22:03:18 +0530

    Updated

--- Codechef/Wordle/Wordle.py
+++ Codechef/Wordle/Wordle.py
@@ -3,4 +3,9 @@ while T:
     T -= 1
     S = input()
     T = input()
-    
+    for i in S:
+        for j in T:
+            if i == j:
+                print("G", end = "")
+            else:
+                print("B", end = "")
