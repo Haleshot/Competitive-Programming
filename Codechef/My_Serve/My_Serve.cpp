f70783069d659b688948c6ad20edd35a99c3e58a
Haleshot <hari.leo03@gmail.com>
2023-06-14 23:36:45 +0530

    Updated

--- Codechef/My_Serve/My_Serve.cpp
+++ Codechef/My_Serve/My_Serve.cpp
@@ -7,7 +7,15 @@ int main()
     cin >> T;
     while (T--)
     {
+        int P, Q, points;
+        cin >> P >> Q;
+        points = P + Q;
+        if (((P + Q) % 4 == 0) || ((P + Q) % 4 == 1))
+        {
+            cout << "ALICE" << endl;
+        }
         
+
     }
     return 0;
 }
