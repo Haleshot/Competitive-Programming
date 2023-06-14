e7dbdb53d8db371e88c23d94c4cb90a2992ad460
Haleshot <hari.leo03@gmail.com>
2023-06-14 23:31:27 +0530

    Updated

--- Codechef/My_Serve/My_Serve.py
+++ Codechef/My_Serve/My_Serve.py
@@ -2,4 +2,8 @@ T = int(input())
 while T:
     T -= 1
     P, Q = map(int, input().split())
-    
+    points = P + Q - 1
+    if (points % 2 == 0):
+        print("BOB")
+    else:
+        print("ALICE")
