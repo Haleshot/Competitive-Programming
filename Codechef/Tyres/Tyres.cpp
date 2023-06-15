09a0eb74166952a1853480d3eb3a815430de779f
Haleshot <hari.leo03@gmail.com>
2023-06-15 20:56:26 +0530

    Updated

--- Codechef/Tyres/Tyres.cpp
+++ Codechef/Tyres/Tyres.cpp
@@ -9,6 +9,14 @@ int main()
     {
         int N;
         cin >> N;
+        if (N % 4 == 0)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
     }
     return 0;
 }
