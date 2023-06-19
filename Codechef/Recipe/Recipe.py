966f769a3df19d89e7633888f4881991940fd765
Haleshot <hari.leo03@gmail.com>
2023-06-19 23:13:50 +0530

    Updated

--- Codechef/Recipe/Recipe.py
+++ Codechef/Recipe/Recipe.py
@@ -2,12 +2,14 @@ import numpy as np
 T = int(input())
 while T:
     T -= 1
-    N = list(map(int(input())))
+    N = list(map(int, input().split()))
     B = []
     C = []
-    for i in range(N[0]):
+    for i in range(1, len(N)):
         B.append(N[i])
     gcd = B[0]
+    for i in range(1, N[0] - 1):
+        gcd = np.gcd(gcd, B[i])
     for i in range(N[0] - 1):
         C.append(B[i]//gcd)
     for i in range(N[0] - 1):
