ac75afdc4749fa9d1daf4dad2eaaae2155a57dfc
Haleshot <hari.leo03@gmail.com>
2023-06-19 23:08:04 +0530

    Updated

--- Codechef/Recipe/Recipe.py
+++ Codechef/Recipe/Recipe.py
@@ -1,10 +1,9 @@
-def gcd_calc(a, b):
-    while b:
-        a, b = b, a % b
-    return a
-
+import numpy as np
 T = int(input())
 while T:
     T -= 1
     N = list(map(int(input())))
-    
+
+    gcd = gcd_calc(N[1], N[2])
+    for i in range(2, N):
+        gcd=gcd_calc(gcd, N[i])
