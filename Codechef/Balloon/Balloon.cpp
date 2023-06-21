ac08a3e2fe255dbc2cf590498a3747b082673d90
Haleshot <hari.leo03@gmail.com>
2023-06-21 19:20:14 +0530

    Updated

--- Codechef/Balloon/Balloon.cpp
+++ Codechef/Balloon/Balloon.cpp
@@ -10,7 +10,12 @@ int main()
         int N;
         cin >> N;
         int A[N];
+        for (int i = 0; i < N; i++)
+        {
+            cin >> A[i];
+        }
         
+
     }
     return 0;
 }
