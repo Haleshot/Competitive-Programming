d0a4a762607914216fc194daab90f918599298d9
Haleshot <hari.leo03@gmail.com>
2023-06-21 19:18:57 +0530

    Updated

--- Codechef/Balloon/Balloon.py
+++ Codechef/Balloon/Balloon.py
@@ -3,3 +3,10 @@ while T:
     T -= 1
     N = int(input())
     A = list(map(int, input().split()))
+    c = 0
+    for i in range(N):
+        if A[i] <= 7:
+            c += 1
+        if c == 7:
+            print(i + 1)
+            break
