d1722a738870046d8ac1522ff346ac51c53b06f5
Haleshot <hari.leo03@gmail.com>
2023-06-21 19:21:28 +0530

    Updated

--- Codechef/Balloon/Balloon.cpp
+++ Codechef/Balloon/Balloon.cpp
@@ -7,14 +7,25 @@ int main()
     cin >> T;
     while (T--)
     {
-        int N;
+        int N, c = 0;
         cin >> N;
         int A[N];
         for (int i = 0; i < N; i++)
         {
             cin >> A[i];
         }
-        
+        for (int i = 0; i < N; i++)
+        {
+            if (A[i] <= 7)
+            {
+                c += 1;
+            }
+            if (c == 7)
+            {
+                cout << c + 1 << endl;
+                break;
+            }
+        }
 
     }
     return 0;
