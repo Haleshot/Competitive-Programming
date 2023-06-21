39b3d4db1741901bf32782af8d8aaf676a513aea
Haleshot <hari.leo03@gmail.com>
2023-06-21 19:19:51 +0530

    Initial Commit

---
+++ Codechef/Balloon/Balloon.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        int A[N];
+        
+    }
+    return 0;
+}
