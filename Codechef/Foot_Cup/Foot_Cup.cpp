54f47826f0b600e18bca2332912472f4dac8e071
Haleshot <hari.leo03@gmail.com>
2023-06-23 22:31:46 +0530

    Initial Commit

---
+++ Codechef/Foot_Cup/Foot_Cup.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X, Y;
+        cin >> X >> Y;
+        
+    }
+    return 0;
+}
