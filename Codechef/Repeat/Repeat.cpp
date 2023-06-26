4f72d1e8f981e360c675b0c60d101262906b9626
Haleshot <hari.leo03@gmail.com>
2023-06-26 13:16:22 +0530

    Initial Commit

---
+++ Codechef/Repeat/Repeat.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, K, S;
+        cin >> N >> K >> S;
+        
+    }
+    return 0;
+}
