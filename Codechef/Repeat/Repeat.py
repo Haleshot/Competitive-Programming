eafd8f04b8a2d76a30f143773fb3514b5f69541d
Haleshot <hari.leo03@gmail.com>
2023-06-26 13:15:31 +0530

    Updated

--- Codechef/Repeat/Repeat.py
+++ Codechef/Repeat/Repeat.py
@@ -1,16 +1,23 @@
+# T = int(input())
+# while T:
+#     T -= 1
+#     N, K, S = map(int, input().split())
+#     l = [i for i in range(2 * N + 1) if (i % 2 == 1)]
+#     l2 = l.copy()
+#     for i in range(N + K - 1):
+#         temp = l2[i]
+#         l2.extend([temp] * (K- 1))
+#         if sum(l2) == S:
+#             print(temp)
+#             break
+#         l2 = l.copy()
+
 T = int(input())
 while T:
     T -= 1
     N, K, S = map(int, input().split())
-    l = []
+    c = 0
     for i in range(2 * N + 1):
         if (i % 2 == 1):
-            l.append(i)
-    l2 = l.copy()
-    for i in range(N + K - 1):
-        temp = l2[i]
-        l2.extend([temp] * (K- 1))
-        if sum(l2) == S:
-            print(temp)
-            break
-        l2 = l.copy()
+            c += i
+    print(int((S - c)/(K - 1)))
