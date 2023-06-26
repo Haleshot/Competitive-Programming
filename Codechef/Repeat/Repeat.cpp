6685cdf236c85ec77d5127d3a6dea8d80e3d4fc3
Haleshot <hari.leo03@gmail.com>
2023-06-26 13:17:37 +0530

    Updated

--- Codechef/Repeat/Repeat.cpp
+++ Codechef/Repeat/Repeat.cpp
@@ -7,9 +7,16 @@ int main()
     cin >> T;
     while (T--)
     {
-        int N, K, S;
+        int N, K, S, c = 0;
         cin >> N >> K >> S;
-        
+        for (int i = 0; i < 2 * N + 1; i++)
+        {
+            if (i % 2 == 1)
+            {
+                c += i;
+            }
+        }    
+        cout << (int((S - c)/(K - 1))) << endl;
     }
     return 0;
 }
