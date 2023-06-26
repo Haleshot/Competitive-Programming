8fb5f04cc0bc6909762753cf7a06a386ceda9ccb
Haleshot <hari.leo03@gmail.com>
2023-06-26 11:59:22 +0530

    Updated

--- Codechef/Repeat/Repeat.py
+++ Codechef/Repeat/Repeat.py
@@ -8,10 +8,12 @@ while T:
         if (i % 2 == 1):
             l.append(i)
     l2 = l.copy()
+    print(l2)
     for i in range(length):
         temp = l2[i]
-        for j in range(K - 1):
-            l2.append(temp)
+        l2.extend([temp] * K)
+        # for j in range(K - 1):
+        #     l2.append(temp)
         if sum(l2) == S:
             print(temp)
             break
