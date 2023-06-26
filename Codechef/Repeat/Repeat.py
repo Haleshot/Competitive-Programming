33155be93dde9b193380fbf4715e79d2480efc82
Haleshot <hari.leo03@gmail.com>
2023-06-26 10:55:34 +0530

    Updated

--- Codechef/Repeat/Repeat.py
+++ Codechef/Repeat/Repeat.py
@@ -2,4 +2,17 @@ T = int(input())
 while T:
     T -= 1
     N, K, S = map(int, input().split())
-    
+    length = N + K - 1
+    l = []
+    for i in range(2 * N + 1):
+        if (i % 2 == 1):
+            l.append(i)
+
+    for i in range(length - N):
+        temp = l[i]
+        for j in range(K - l.count(temp)):
+            l.append(temp)
+        print(l)
+        # if sum(l) == S:
+        #     print(temp)
+        #     break
