009b0b1ec2f6ea2f937f5a355575cd01954a8add
Haleshot <hari.leo03@gmail.com>
2023-06-28 16:44:31 +0530

    Updated

--- Codechef/Hills/Hills.py
+++ Codechef/Hills/Hills.py
@@ -3,4 +3,12 @@ while T:
     T -= 1
     N, U, D = map(int, input().split())
     H = list(map(int, input().split()))
-    
+    c, c1 = 0, 0
+    for i in range(N + 1):
+        l1, l2 = H[i], H[i + 1]
+        if ((abs(l1 - l2) <= U) or (abs(l1 - l2) <= D and c1 <= 1)):
+            c += 1
+        if (abs(l1 - l2) <= U):
+            c1 += 1
+            c += 1
+    print(c)
