588008d65eec34c98b78ddfbaa1a2cc50f1fd33f
Haleshot <hari.leo03@gmail.com>
2023-06-28 17:47:04 +0530

    Updated

--- Codechef/Hills/Hills.cpp
+++ Codechef/Hills/Hills.cpp
@@ -7,7 +7,14 @@ int main()
     cin >> T;
     while (T--)
     {
-
+        int N, U, D;
+        cin >> N >> U >> D;
+        int H[N];
+        for (int i = 0; i < N; i++)
+        {
+            cin >> H[i];
+        }
+        
     }
     return 0;
 }
