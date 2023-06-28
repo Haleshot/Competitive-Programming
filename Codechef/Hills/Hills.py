bb7329dffcd35d08fc465d64709818a9c848e074
Haleshot <hari.leo03@gmail.com>
2023-06-28 17:00:32 +0530

    Updated

--- Codechef/Hills/Hills.py
+++ Codechef/Hills/Hills.py
@@ -6,13 +6,10 @@ while T:
     c, c1 = 0, 0
     for i in range(N - 1):
         l1, l2 = H[i], H[i + 1]
-        print(l1, " ", l2)
         if ((l1 - l2 <= abs(U)) or (((l1 - l2) <= D) and c1 <= 1)):
             c += 1
-        elif (abs(l1 - l2) > D):
+        if (l1 - l2) > D and c1 != 1:
             c1 += 1
             c += 1
-        else:
-            break
         
     print(c)
