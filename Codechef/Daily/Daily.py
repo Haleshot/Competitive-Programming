263865f898222e28593f5d55b602284267e76d80
Haleshot <hari.leo03@gmail.com>
2023-06-29 20:03:05 +0530

    Updated

--- Codechef/Daily/Daily.py
+++ Codechef/Daily/Daily.py
@@ -1,6 +1,14 @@
+import math
+
 X, N = map(int, input().split())
 c = 0
 while N:
     N -= 1
     H = input()
-    
+    i, j = 0, 54
+    for _ in range(9):
+        l = H[i:i+4] + H[j - 2:j]
+        c += math.comb(l.count('0'), X)
+        i += 4
+        j -= 2
+print(c)
