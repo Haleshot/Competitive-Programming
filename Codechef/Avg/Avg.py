354146e581055490faeed5a3e2a30c158e73b575
Haleshot <hari.leo03@gmail.com>
2023-07-01 16:37:52 +0530

    Updated

--- Codechef/Avg/Avg.py
+++ Codechef/Avg/Avg.py
@@ -3,3 +3,8 @@ while T:
     T -= 1
     N, K, V = map(int, input().split())
     A = list(map(int, input().split()))
+    result = ((V * (N + K)) - sum(A))/K
+    if result > 0:
+        print(result)
+    else:
+        print(-1)
