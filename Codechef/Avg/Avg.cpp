e234bfc4970d0bddc8c437cce4077c83252397bf
Haleshot <hari.leo03@gmail.com>
2023-07-01 17:00:08 +0530

    Initial Commit

---
+++ Codechef/Avg/Avg.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int N, K, V;
+        cin >> N >> K >> V;
+        
+    }
+    return 0;
+}
