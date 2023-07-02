c8b9a595365150e108fe703eed6bc2c0f3e06698
Haleshot <hari.leo03@gmail.com>
2023-07-02 20:04:55 +0530

    Updated C++ Version

--- Codechef/Dist_Code/Dist_Code.cpp
+++ Codechef/Dist_Code/Dist_Code.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main()
@@ -9,15 +8,18 @@ int main()
     cin >> T;
     while (T--)
     {
-        char S;
+        string S, st;
         cin >> S;
-        unordered_map<char, int> d;
-        for (auto x : d)
+        unordered_map<string, int> d;
+        
+        for (int i = 0; i < S.length(); i++)
         {
-            cout << x.first << " " <<  x.second << endl;
+            st = S.substr(i, 2);
+            d[S.substr(i, 2)]++;
         }
-
-
+        for (auto x : d)
+        cout << x.first << " " << x.second << endl;
     }
     return 0;
 }
+
