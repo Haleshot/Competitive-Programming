a823ca53039395b7dcd9b094ae578e6565fc5edd
Haleshot <hari.leo03@gmail.com>
2023-07-02 19:21:07 +0530

    Updated

--- Codechef/Dist_Code/Dist_Code.py
+++ Codechef/Dist_Code/Dist_Code.py
@@ -6,4 +6,12 @@ while T:
     c = 0
     for i in range(len(S) - 1):
         x = S[i:i + 2]
-        print(x)
+        d[x] = 0
+    for i in range(len(S) - 1):
+        x = S[i:i + 2]
+        if d[x] == 0:
+            d[x] += 1
+            c += 1
+
+    print(c)
+        
