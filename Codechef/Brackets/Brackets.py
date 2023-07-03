872d24c2e73e3363012069e76c0449a4fe0ce0c7
Haleshot <hari.leo03@gmail.com>
2023-07-03 21:00:22 +0530

    Updated

--- Codechef/Brackets/Brackets.py
+++ Codechef/Brackets/Brackets.py
@@ -2,4 +2,13 @@ T = int(input())
 while T:
     T -= 1
     A = input()
-    
+    balance = 0
+    max_balance = 0
+    for i in range(len(A)):
+        if A[i] == '(':
+            balance += 1
+        if A[i] == ')':
+                balance -= 1
+	    
+        max_balance = max( max_balance, balance )
+    print("("*max_balance+")"*max_balance)
