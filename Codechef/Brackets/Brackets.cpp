cf362dec44a5cb7b97abaaf06d9ed9e58538ddbf
Haleshot <hari.leo03@gmail.com>
2023-07-03 21:02:25 +0530

    Updated C++ Version

--- Codechef/Brackets/Brackets.cpp
+++ Codechef/Brackets/Brackets.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 using namespace std;
 
+int S(string S)
+{
+    int balance = 0, max_balance = 0;
+    for (int i = 0; i < S.size(); i++)
+    {
+
+    }
+}
+
+
 int main()
 {
     int T;
     cin >> T;
     while (T--)
     {
-        
+       string A;
+       cin >> A;
+
     }
     return 0;
 }
