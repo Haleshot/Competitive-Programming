50445ed1205c7d7eee4f77d0f694c8866bea0b27
Haleshot <hari.leo03@gmail.com>
2023-07-04 14:30:25 +0530

    Updated Python version

--- Codechef/Hotel/Hotel.py
+++ Codechef/Hotel/Hotel.py
@@ -4,4 +4,18 @@ while T:
     N = int(input())
     arr = list(map(int, input().split()))
     dept = list(map(int, input().split()))
-    
+    arr.sort()
+    dept.sort()
+    current, maximum, i, j = 0, 0, 0, 0
+
+    while i < N and j < N:
+        if arr[i] < dept[j]:
+            i += 1
+            current += 1
+        else:
+            j += 1
+            current -= 1
+        if current > maximum:
+            maximum = current
+            
+    print(maximum)
