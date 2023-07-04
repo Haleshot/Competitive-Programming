5b978cfda562a3804a11b73bed4c9ebc857f7577
Haleshot <hari.leo03@gmail.com>
2023-07-04 14:34:53 +0530

    Initial Commit

--- Codechef/Hotel/Hotel.cpp
+++ Codechef/Hotel/Hotel.cpp
@@ -7,7 +7,34 @@ int main()
     cin >> T;
     while (T--)
     {
-        
+        int N;
+        cin >> N;
+        int arr[N], dept[N];
+        for (int i = 0; i < N; i++)
+        {
+            cin >> arr[i];
+        }
+        for (int i = 0; i < N; i++)
+        {
+            cin >> dept[i];
+        }
+        int current = 0, maximum = 0, i = 0, j = 0;
+        while (i < N && j < N)
+        {
+            if (arr[i] < dept[j])
+                {
+                    i += 1;
+                    current += 1;
+                }
+            if (current > maximum)
+                maximum = current;
+            else
+                {
+                    j += 1;
+                    current -= 1;
+                }
+            
+        }
     }
     return 0;
 }
