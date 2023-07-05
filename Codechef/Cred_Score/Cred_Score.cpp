461b75879de32bc179f0e1ad248dfee4aec82895
Haleshot <hari.leo03@gmail.com>
2023-07-05 22:23:36 +0530

    Updated C++ Version

--- Codechef/Cred_Score/Cred_Score.cpp
+++ Codechef/Cred_Score/Cred_Score.cpp
@@ -4,5 +4,13 @@ using namespace std;
 int main()
 {
     int X;
+    if (X >= 750)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
     return 0;
 }
