ac632ce0f1aed4694f44325c172b945479bfa354
Haleshot <hari.leo03@gmail.com>
2023-07-07 22:31:47 +0530

    Updated C++ Version

--- Codechef/Four_Tickets/Four_Tickets.cpp
+++ Codechef/Four_Tickets/Four_Tickets.cpp
@@ -5,5 +5,18 @@ int main()
 {
     int T;
     cin >> T;
+    while (T--)
+    {
+        int X;
+        cin >> X;
+        if (4 * X <= 1000)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
     return 0;
 }
