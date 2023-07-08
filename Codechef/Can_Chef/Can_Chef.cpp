a4432d7bd6cb50913a5a4437fcaff6411a53a136
Haleshot <hari.leo03@gmail.com>
2023-07-08 21:56:05 +0530

    Updated C++ Version

--- Codechef/Can_Chef/Can_Chef.cpp
+++ Codechef/Can_Chef/Can_Chef.cpp
@@ -7,7 +7,12 @@ int main()
     cin >> T;
     while (T--)
     {
-        
+        int X, Y;
+        cin >> X >> Y;
+        if ((15 * X) >= (2 * Y))
+        {
+            cout << "YES" << endl;
+        }
     }
     return 0;
 }
