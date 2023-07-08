2ecafc59273c65ad8e47bea445f51ed32e4bfa71
Haleshot <hari.leo03@gmail.com>
2023-07-08 21:55:06 +0530

    Updated Python version

--- Codechef/Can_Chef/Can_Chef.py
+++ Codechef/Can_Chef/Can_Chef.py
@@ -2,7 +2,7 @@ T = int(input())
 while T:
     T -= 1
     X, Y = map(int, input().split())
-    if (15 * X) <= (2 * Y):
+    if (15 * X) >= (2 * Y):
         print("YES")
     else:
         print("NO")
