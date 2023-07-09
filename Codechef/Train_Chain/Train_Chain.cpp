574a9c56f63c667a11e5666a56b759bbc68a42dc
Haleshot <hari.leo03@gmail.com>
2023-07-09 20:09:38 +0530

    Updated C++ Version

--- Codechef/Train_Chain/Train_Chain.cpp
+++ Codechef/Train_Chain/Train_Chain.cpp
@@ -7,7 +7,30 @@ int main()
     cin >> T;
     while (T--)
     {
-        
+        int N;
+        cin >> N;
+        string S[N];
+        int A = 0, B = 0, AB = 0, O = 0;
+        for (int i = 0;  i < N; i ++)
+        {
+            cin >> S[i];
+            if (S[i] == "A")
+            {
+                A += 1;
+            }
+            else if (S[i] == "B")
+            {
+                B += 1;
+            }
+            else if (S[i] == "AB")
+            {
+                AB += 1;
+            }
+            else if (S[i] == "O")
+            {
+                O += 1;
+            }
+        }
     }
     return 0;
 }
