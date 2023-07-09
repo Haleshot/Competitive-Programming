e4a7996fb2c411dc320af50ac976407f9c970012
Haleshot <hari.leo03@gmail.com>
2023-07-09 20:02:44 +0530

    Updated Python version

--- Codechef/Train_Chain/Train_Chain.py
+++ Codechef/Train_Chain/Train_Chain.py
@@ -2,5 +2,12 @@ T = int(input())
 while T:
     T -= 1
     N = int(input())
-    B = list(map(int, input().split()))
-    
+    B = list(map(str, input().split()))
+    A = B.count("A")
+    B = B.count("B")
+    AB = B.count("AB")
+    O = B.count("O")
+    if (A >= B):
+        print(A + AB + O)
+    else:
+        print(B + AB + O)
