9f03d90545753eda0f3e2db6a694a5d8da565081
Haleshot <hari.leo03@gmail.com>
2023-07-12 22:12:45 +0530

    Updated Java version

--- Codechef/Cut_Paper/Cut_Paper.java
+++ Codechef/Cut_Paper/Cut_Paper.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
+
 public class Cut_Paper
 {
     public static void main(String[] args) 
     {
-        
+        int T;
+        Scanner sc = new Scanner(System.in);
+        T = sc.nextInt();
+        while(T > 0)
+        {
+            int N, K;
+            N = sc.nextInt();
+            K = sc.nextInt();
+            System.out.println((N/K) * (N/K));
+        }
+
+        sc.close()
+
     }
 
 }
