c92a475b3e07cdaa81094551cbb432e6af6617f7
Haleshot <hari.leo03@gmail.com>
2023-07-13 23:07:05 +0530

    Initial Commit

---
+++ Codechef/Rating/Rating.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Rating
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0)
+        {
+            T -= 1;
+            int S = sc.nextInt();
+            System.out.println(-S - 1);
+
+        }
+        sc.close();
+    }
+}
