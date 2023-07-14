fbd7ad1c732e52b3a2c519b1a6df3028fffb611f
Haleshot <hari.leo03@gmail.com>
2023-07-14 21:44:52 +0530

    Updated Python version

--- Codechef/VCS/VCS.py
+++ Codechef/VCS/VCS.py
@@ -1,5 +1,14 @@
 T = int(input())
 while T:
     T -= 1
-    N, M, K = map(int, input().spplit())
-    
+    N, M, K = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    first, second = 0, 0
+    for i in range(1, N + 1):
+        if i in A and i in B:
+            first += 1
+        elif i not in A and i not in B:
+            second += 1
+
+    print(str(first), " ", str(second))
