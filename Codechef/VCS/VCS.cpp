38c2e41b625be5862045b61390804901864a5f92
Haleshot <hari.leo03@gmail.com>
2023-07-15 18:40:13 +0530

    Updated C++ Version

--- Codechef/VCS/VCS.cpp
+++ Codechef/VCS/VCS.cpp
@@ -7,9 +7,41 @@ int main()
     cin >> T;
     while (T--)
     {
-        int N, M, K;
+        int N, M, K, first = 0, second = 0;
         cin >> N >> M >> K;
-        
+        int A[M], B[K];
+        for (int i = 0; i < M; i++)
+        {
+            cin >> A[i];
+        }
+        for (int i = 0; i < K; i++)
+        {
+            cin >> B[i];
+        }
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < K; j++)
+            {
+                if (A[i] == B[j])
+                {
+                    first += 1;
+                }
+            }
+        }
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < K; j++)
+            {
+                if (A[i] != B[j])
+                {
+                    second += 1;
+                }
+            }
+        }
+
+        cout << first << " " << second << endl;
+
+
     }
     return 0;
 }
