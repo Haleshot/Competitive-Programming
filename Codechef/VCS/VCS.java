29241f7a04a412cf721203d3a373c7cdec9cbdfc
Haleshot <hari.leo03@gmail.com>
2023-07-15 18:52:04 +0530

    Updated Java version

--- Codechef/VCS/VCS.java
+++ Codechef/VCS/VCS.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Codechef
+class VCS
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
