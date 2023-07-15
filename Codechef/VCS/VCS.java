ba15234004564676f37667e278897e6379d30506
Haleshot <hari.leo03@gmail.com>
2023-07-15 18:48:32 +0530

    Updated Java version

--- Codechef/VCS/VCS.java
+++ Codechef/VCS/VCS.java
@@ -2,6 +2,7 @@
 
 import java.util.*;
 import java.lang.*;
+import java.util.Scanner;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
@@ -9,6 +10,35 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+        Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+        while (T > 0)
+        {
+            int first = 0, second = 0;
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            int K = sc.nextInt();
+            ArrayList <Integer> A = new ArrayList<>();
+            ArrayList <Integer> B = new ArrayList<>();
+
+            for (int i = 0; i < M; i++)
+            {
+                A.add(sc.nextInt());
+            }
+
+            for (int i = 0; i < K; i++)
+            {
+                B.add(sc.nextInt());
+            }
+
+            for (int i = 0; i < M; i++)
+            {
+                A.add(sc.nextInt());
+            }
+
+            
+            
+        }
+
 	}
 }
