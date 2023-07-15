92b0502f216639861047deb4ff2196ad05eef3d4
Haleshot <hari.leo03@gmail.com>
2023-07-15 19:06:42 +0530

    Updated C++ Version

--- Codechef/How_Many/How_Many.cpp
+++ Codechef/How_Many/How_Many.cpp
@@ -7,5 +7,15 @@ int main()
     cin >> N;
     int size = to_string(N).length();
     cout << size;
+
+    if (size == 1)
+    cout << ("1");
+    else if (size == 2)
+        cout << ("2");
+    else if (size == 3)
+        cout << ("3");
+    else
+        cout << ("More than 3 digits");
+
     return 0;
 }
