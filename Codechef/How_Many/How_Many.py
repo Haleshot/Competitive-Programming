d19c8730255b6bf8bef38ab7001c973748e5122c
Haleshot <hari.leo03@gmail.com>
2023-07-15 19:02:06 +0530

    Updated Python version

--- Codechef/How_Many/How_Many.py
+++ Codechef/How_Many/How_Many.py
@@ -2,4 +2,13 @@ T = int(input())
 while T:
     T -= 1
     N = int(input())
-    
+    size = len(str(N))
+
+    if size == 1:
+        print("1")
+    elif size == 2:
+        print("2")
+    elif size == 3:
+        print("3")
+    else:
+        print("More than 3 digits")
