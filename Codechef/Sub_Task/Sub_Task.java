d4fcc6bf3e4b27da53c0053e74640eff12081661
Haleshot <hari.leo03@gmail.com>
2023-07-16 21:00:22 +0530

    Initial Commit

---
+++ Codechef/Sub_Task/Sub_Task.java
@@ -0,0 +1,24 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Sub_Task
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0)
+        {
+            T -= 1;
+            int first = 0, second = 0;
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            int K = sc.nextInt();
+            ArrayList <Integer> A = new ArrayList<>();
+            
+	}
+}
