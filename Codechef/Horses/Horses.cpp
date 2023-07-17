27ed4708bcc0fbe6f498a34d71b95f5bb72a789a
Haleshot <hari.leo03@gmail.com>
2023-07-17 22:12:42 +0530

    Updated C++ Version

--- Codechef/Horses/Horses.cpp
+++ Codechef/Horses/Horses.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
@@ -14,8 +15,17 @@ int main()
         {
             cin >> S[i];
         }
-        int minimum[N];
-        
+        int diff = 10;
+        sort(S, S + N);
+        for (int i = 1; i < N; i++)
+        {
+            if (S[i] - S[i - 1] < diff)
+            {
+                diff = S[i] - S[i - 1];
+            }
+        }
+        cout << diff << endl;
+
     }
     return 0;
 }
