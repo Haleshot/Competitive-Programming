27653f2a350e04c09fa102675137342724b6f877
Haleshot <hari.leo03@gmail.com>
2023-07-17 22:08:13 +0530

    Updated Python version

--- Codechef/Horses/Horses.py
+++ Codechef/Horses/Horses.py
@@ -3,3 +3,10 @@ while T:
     T -= 1
     N = int(input())
     S = list(map(int, input().split()))
+    S.sort()
+    diff = 10
+    minimum = []
+    for i in range(1, N):
+        minimum.append(S[i] - S[i - 1])
+
+    print(min(minimum))
