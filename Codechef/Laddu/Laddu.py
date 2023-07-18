e7cdaa216c288ddb8fe8a7262d2be21c27eba5ee
Haleshot <hari.leo03@gmail.com>
2023-07-18 09:55:56 +0530

    Updated Python version

--- Codechef/Laddu/Laddu.py
+++ Codechef/Laddu/Laddu.py
@@ -2,4 +2,8 @@ T = int(input())
 while T:
     T -= 1
     actities = map(str, input.split())
+    contest_won = map(str, input.split())
+    top_contrib = input()
+    bug_found = map(str, input.split())
+    contest_host = input()
     
