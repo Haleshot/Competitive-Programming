c04a09a7a56fb45e2ee09a7ac8590ded858463c8
Haleshot <hari.leo03@gmail.com>
2023-07-18 11:19:54 +0530

    Updated C++ Version

--- Codechef/Laddu/Laddu.cpp
+++ Codechef/Laddu/Laddu.cpp
@@ -7,17 +7,17 @@ int main()
     cin >> T;
     while (T--)
     {
-        int N, points = 0, A;
+        int N, A;
         string ori;
-
         cin >> N >> ori;
+        int points = 0;
+        string all_inputs;
         for (int i = 0; i < N; i++)
         {
-            string all_inputs;
             cin >> all_inputs;
-            if(all_inputs == "CONTEall_inputsT_WON" || all_inputs == "BUG_FOUND")
+            if(all_inputs == "CONTEST_WON" || all_inputs == "BUG_FOUND")
             cin >> A;
-            if(all_inputs == "CONTEall_inputsT_WON") 
+            if(all_inputs == "CONTEST_WON") 
             {
                 points += 300; 
                 if(A <= 20)
@@ -27,7 +27,7 @@ int main()
                 points += A;
             if(all_inputs == "TOP_CONTRIBUTOR")
                 points += 300;
-            if(all_inputs == "CONTEall_inputsT_HOall_inputsTED")
+            if(all_inputs == "CONTEST_HOSTED")
                 points += 50;
 
         }
