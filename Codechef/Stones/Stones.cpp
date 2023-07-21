9690c5acab2b824252e1e81e5a46fbef0b1a1f69
Haleshot <hari.leo03@gmail.com>
2023-07-21 17:00:27 +0530

    Initial Commit

--- Codechef/Stones/Stones.cpp
+++ Codechef/Stones/Stones.cpp
@@ -12,7 +12,7 @@ int main()
         cin >> J >> S;
         set<char> m;
         int count = 0;
-        for (int i = 0;  i < S.length(); i++)
+        for (int i = 0;  i < J.length(); i++)
         {
             char temp = J[i];
             m.insert(temp);
