dac479c42337b013626b63807f11e5962e130679
Haleshot <hari.leo03@gmail.com>
2023-07-22 22:01:43 +0530

    Updated Java version

--- Codechef/Walk/Walk.java
+++ Codechef/Walk/Walk.java
@@ -2,6 +2,7 @@
 
 import java.util.*;
 import java.lang.*;
+import java.util.Arrays;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
@@ -19,7 +20,11 @@ class Waslk
             {
                 W[i] = sc.nextInt();
             }
-            
+            for (int i = 0; i < N; i++)
+            {
+                W[i] += i;
+            }
+            System.out.println(Arrays.stream(W).max().getAsInt());
         }
 	}
 }
