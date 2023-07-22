109666b6c9fc56367478abef5c8ddbd29ce20a46
Haleshot <hari.leo03@gmail.com>
2023-07-22 21:17:52 +0530

    Updated Python version

--- Codechef/Walk/Walk.py
+++ Codechef/Walk/Walk.py
@@ -3,4 +3,10 @@ while T:
     T -= 1
     N = int(input())
     W = list(map(int, input().split()))
-    
+    max_number = max(W)
+    for i in W:
+        if max_number >= i:
+            max_number -= 1
+        else:
+            max_number += 1
+    print(max_number)
