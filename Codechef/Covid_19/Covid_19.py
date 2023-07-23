cde4775affd87a2c33517850b9bd8ce12100d856
Haleshot <hari.leo03@gmail.com>
2023-07-23 21:46:52 +0530

    Updated Python version

--- Codechef/Covid_19/Covid_19.py
+++ Codechef/Covid_19/Covid_19.py
@@ -3,4 +3,14 @@ while T:
     T -= 1 
     N = int(input())
     X = list(map(int, input().split()))
-    worst, best = 1, 1
+    final_worst, final_best = 1, 1
+    c = 1
+    new_X = []
+    for i in range(N - 1):
+        if X[i + 1] - X[i] <= 2:
+            c += 1
+        else:
+            new_X.append(c)
+            c = 1
+    new_X.append(c)
+    print(min(new_X), max(new_X))
