f0818f2ac7281af18fd71ca0287920c79724333b
Haleshot <hari.leo03@gmail.com>
2023-07-23 22:19:13 +0530

    Updated Java version

--- Codechef/Covid_19/Covid_19.java
+++ Codechef/Covid_19/Covid_19.java
@@ -5,6 +5,7 @@ import java.util.*;
 import javax.print.DocFlavor.INPUT_STREAM;
 
 import java.lang.*;
+import java.lang.Math;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
@@ -14,6 +15,7 @@ class Covid_19
 	{
 		Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
+        int c = 1, minimum = 1000, maximum = 1;
         while (T-- > 0)
         {
             int N = sc.nextInt();
@@ -22,7 +24,23 @@ class Covid_19
             {
                 X[i] = sc.nextInt();
             }
-            
+            for (int i = 0; i < N; i++)
+            {
+                if (Math.abs(X[i + 1] - X[i]) <= 2)
+                {
+                    c += 1;
+                }
+                else
+                {
+                    if (c > maximum)
+                        maximum = c;
+                    if (c < minimum)
+                        {
+                            minimum = c;   
+                            c = 1;
+                        }
+                }
+            }
         }
 	}
 }
