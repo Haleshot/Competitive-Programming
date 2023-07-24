ab1137f0b378fa185d027c8ba3176ed009cc1775
Haleshot <hari.leo03@gmail.com>
2023-07-24 21:39:36 +0530

    Updated Python version

--- Codechef/AIRM/AIRM.py
+++ Codechef/AIRM/AIRM.py
@@ -1,5 +1,19 @@
 T = int(input())
 while T:
     T -= 1
+    N = int(input())
     A = list(map(int, input().split()))
     D = list(map(int, input().split()))
+    s = A + D
+    d = {}
+    for i in s:
+        if i not in d:
+            d[i] = 1
+        else:
+            d[i] += 1
+    
+    c = 0
+    for i in d:
+        if d[i] > c:
+            c = d[i]
+    print(c)
