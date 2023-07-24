170ecb883056ff54a8a963853c387c908e55ac26
Haleshot <hari.leo03@gmail.com>
2023-07-24 21:47:22 +0530

    Initial Commit

---
+++ Codechef/AIRM/AIRM.java
@@ -0,0 +1,22 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class AIRM
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0)
+        {
+            int N = sc.nextInt();
+            int A[] = new int[2 * N];
+            for (int i = 0; i < 2 * N; i++)
+                A[i] = sc.nextInt();
+        }
+	}
+}
