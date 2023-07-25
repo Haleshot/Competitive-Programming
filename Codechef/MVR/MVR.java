73a338d156e20bca2d0d43a8dfa6d22dda617ab6
Haleshot <hari.leo03@gmail.com>
2023-07-25 22:29:42 +0530

    Updated Java version

--- Codechef/MVR/MVR.java
+++ Codechef/MVR/MVR.java
@@ -9,6 +9,12 @@ class MVR
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+
+        
 	}
 }
