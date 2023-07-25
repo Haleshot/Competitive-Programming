f32270f709821b9ac4d719376c18b3c0cd89a61c
Haleshot <hari.leo03@gmail.com>
2023-07-25 22:24:38 +0530

    Updated Python version

--- Codechef/MVR/MVR.py
+++ Codechef/MVR/MVR.py
@@ -1,1 +1,7 @@
 A, B, X, Y = map(int, input().split())
+if (((2 * A) + B) > ((2 * X) + Y)):
+    print("MESSIR")
+elif (((2 * A) + B) < ((2 * X) + Y)):
+    print("RONALDO")
+else:
+    print("EQUAL")
