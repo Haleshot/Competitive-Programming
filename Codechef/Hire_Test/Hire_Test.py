b07cd65b812b674173b79da21ff21094e1792c11
Haleshot <hari.leo03@gmail.com>
2023-07-26 22:22:22 +0530

    Updated Python version

--- Codechef/Hire_Test/Hire_Test.py
+++ Codechef/Hire_Test/Hire_Test.py
@@ -3,4 +3,7 @@ while T:
     T -= 1
     N, M = map(int, input().split())
     X, Y = map(int, input().split())
-    
+    inp = []
+    for i in range(N):
+        ele = input()
+        inp.append(ele)
