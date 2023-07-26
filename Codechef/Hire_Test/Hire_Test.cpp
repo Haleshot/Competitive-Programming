0a6f78b05210cc0bb6bc339408c9230f13e9f900
Haleshot <hari.leo03@gmail.com>
2023-07-26 22:48:36 +0530

    Updated C++ Version

--- Codechef/Hire_Test/Hire_Test.cpp
+++ Codechef/Hire_Test/Hire_Test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 
@@ -10,12 +11,16 @@ int main()
     {
         int N, M, X, Y;
         cin >> N >> M >> X >> Y;
-        int inp[N];
+        string inp[N];
         for (int i = 0; i < N; i++)
         {
             cin >> inp[i];
         }
-        
+        for (int i = 0; i < N; i++)
+        {
+            int F_Count = count(inp[i].begin(), inp[i].end(), "F");
+            int P_Count = count(inp[i].begin(), inp[i].end(), "P");
+        }
     }
     return 0;
 }
