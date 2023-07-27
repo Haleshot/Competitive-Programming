8c87f6da3c633303aa98d5bf1fdb82f5388f9090
Haleshot <hari.leo03@gmail.com>
2023-07-27 20:49:26 +0530

    Updated Java version

--- Codechef/NEW_PIECE/NEW_PIECE.java
+++ Codechef/NEW_PIECE/NEW_PIECE.java
@@ -9,6 +9,20 @@ class NEW_PIECE
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
+		Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0)
+        {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int P = sc.nextInt();
+            int Q = sc.nextInt();
+            if (((A == P) && (B == Q)))
+                System.out.println(0);
+            else if ((A + B) % 2 == (P + Q) % 2)
+                System.out.println(2);
+            else
+                System.out.println(1);
+        }
 	}
 }
