12728be0650b2fe9669c00d8bbec0a670f696db6
Haleshot <hari.leo03@gmail.com>
2023-07-27 20:44:20 +0530

    Initial Commit

---
+++ Codechef/NEW_PIECE/NEW_PIECE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while ( (T--))
+    {
+        int A, B, P, Q;
+        cin >> A >> B >> P >> Q;
+        
+    }
+    
+    return 0;
+}
