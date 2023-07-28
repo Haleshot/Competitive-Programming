f02f9f339325cc5e5e605c5ebebf94158c66d577
Haleshot <hari.leo03@gmail.com>
2023-07-28 21:37:19 +0530

    Updated Python version

--- Codechef/Caesar/Caesar.py
+++ Codechef/Caesar/Caesar.py
@@ -5,4 +5,9 @@ while Q:
     S = input()
     T = input()
     U = input()
-    
+    cipher = abs(ord(S[0]) - ord(T[0]))
+    rotK_U = []
+    for i in range(N):
+        # rotK_U[i] = chr(ord(U[i]) + cipher)
+        print(chr(ord(U[i]) + cipher))
+    # print(rotK_U)
