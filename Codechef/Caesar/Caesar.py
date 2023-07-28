a8b27046056e39b45db2c2f24c2d869e22348af0
Haleshot <hari.leo03@gmail.com>
2023-07-28 22:21:07 +0530

    Updated Python version

--- Codechef/Caesar/Caesar.py
+++ Codechef/Caesar/Caesar.py
@@ -5,18 +5,18 @@ while Q:
     S = input()
     T = input()
     U = input()
-    cipher = abs(ord(S[0]) - ord(T[0]))
+    
     rotK_U = ""
     if (T[0] > S[0]):
+        cipher = ord(T[0]) - ord(S[0])
         for i in range(N):
-            # rotK_U[i] = chr(ord(U[i]) + cipher)
             if (ord(U[i]) + cipher) > 122:
                 rotK_U += chr((ord(U[i]) + cipher)) - 26
             else:
                 rotK_U += chr((ord(U[i]) + cipher))
     elif (T[0] > S[0]):
+        cipher = ord(S[0]) - ord(T[0])
         for i in range(N):
-            # rotK_U[i] = chr(ord(U[i]) + cipher)
             if (ord(U[i]) - cipher) < 97:
                 rotK_U += chr((ord(U[i]) - cipher)) + 26
             else:
