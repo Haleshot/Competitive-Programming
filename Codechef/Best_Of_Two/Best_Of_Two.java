df64cf250caa566981dbcb122dad3ce02b900832
Haleshot <hari.leo03@gmail.com>
2023-07-29 20:54:45 +0530

    Updated Java version

--- Codechef/Best_Of_Two/Best_Of_Two.java
+++ Codechef/Best_Of_Two/Best_Of_Two.java
@@ -1,13 +1,16 @@
 import java.util.*;
 
-public class Best_Of_Two {
-    public static void main(String args[]) {
+public class Best_Of_Two 
+{
+    public static void main(String args[]) 
+    {
         Scanner scn = new Scanner(System.in);
         int T = scn.nextInt();
-        while( T-- > 0 ) {
-            int x = scn.nextInt();
-            int y = scn.nextInt();
-            System.out.println( Math.max( x, y) );
+        while( T-- > 0 ) 
+        {
+            int X = scn.nextInt();
+            int Y = scn.nextInt();
+            System.out.println( Math.max(X, Y) );
         }
     }
 }
