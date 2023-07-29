aa36bfd7362e63876eaf3c5bf9829c5953649cf6
Haleshot <hari.leo03@gmail.com>
2023-07-29 20:52:34 +0530

    Updated C++ Version

--- Codechef/Best_Of_Two/Best_Of_Two.cpp
+++ Codechef/Best_Of_Two/Best_Of_Two.cpp
@@ -3,5 +3,13 @@ using namespace std;
 
 int main()
 {
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int X, Y;
+        cin >> X >> Y;
+        cout << max(X, Y) << endl;
+    }
     return 0;
 }
