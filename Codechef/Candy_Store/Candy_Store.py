4cd04d4aa9c31c37fe519fb9f7e33435ce8e4a37
Haleshot <hari.leo03@gmail.com>
2023-07-30 22:28:43 +0530

    Updated Python version

--- Codechef/Candy_Store/Candy_Store.py
+++ Codechef/Candy_Store/Candy_Store.py
@@ -2,7 +2,7 @@ T = int(input())
 while T:
     T -= 1
     X, Y = map(int, input().split())
-    if (Y >= X):
-        print(X)
+    if (Y <= X):
+        print(Y)
     else:
-        print(X + 2 * Y)
+        print(X + 2 * (Y - X))
