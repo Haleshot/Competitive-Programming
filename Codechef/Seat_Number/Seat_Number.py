d4caab1a944aae5ee6c7f5f7125893b28a9e3d36
Haleshot <hari.leo03@gmail.com>
2023-07-31 11:36:39 +0530

    Updated Python version

--- Codechef/Seat_Number/Seat_Number.py
+++ Codechef/Seat_Number/Seat_Number.py
@@ -2,4 +2,11 @@ T = int(input())
 while T:
     T -= 1
     N = int(input())
-    
+    if (N <= 10):
+        print("LOWER DOUBLE")
+    elif (N >= 11 and N <= 15):
+        print("LOWER SINGLE")
+    elif (N >= 16 and N <= 25):
+        print("UPPER DOUBLE")
+    else:
+        print("UPPER SINGLE")
