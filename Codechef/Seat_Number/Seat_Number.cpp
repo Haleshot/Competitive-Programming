e45e67de0d863eca68d7e2e422dae718f6d4f31d
Haleshot <hari.leo03@gmail.com>
2023-07-31 11:42:07 +0530

    Updated C++ Version

--- Codechef/Seat_Number/Seat_Number.cpp
+++ Codechef/Seat_Number/Seat_Number.cpp
@@ -10,13 +10,13 @@ int main();
         int N;
         cin >> N;
         if (N <= 10)
-            cout << "LOWER DOUBLE" << endl;
+            cout << ("LOWER DOUBLE") << endl;
         else if (N >= 11 && N <= 15)
-            cout << "LOWER SINGLE" << endl;
+            cout << ("LOWER SINGLE") << endl;
         else if (N >= 16 && N <= 25)
-            cout << "UPPER DOUBLE"<< endl;
+            cout << ("UPPER DOUBLE") << endl;
         else
-            cout << "UPPER SINGLE"<< endl;
+            cout << ("UPPER SINGLE") << endl;
     }
     return 0;
 }
