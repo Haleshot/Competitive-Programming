8071dc43bd3db92eeeda6e2ec9618436027a3ead
Haleshot <hari.leo03@gmail.com>
2023-08-01 22:15:57 +0530

    Updated Python version

--- Codechef/Broken_String/Broken_String.py
+++ Codechef/Broken_String/Broken_String.py
@@ -3,4 +3,10 @@ while T:
     T -= 1 
     N = int(input().split())
     S = input()
+    l1 = [], l2 = []
+    for k in range(0, N//2):
+        l1.append(S[k])
+    for j in range(N//2, N):
+        l2.append(S[j])
+
     
