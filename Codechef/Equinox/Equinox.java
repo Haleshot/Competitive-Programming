e06d103ffed7cc1482182a381bfbe26dde089c54
Haleshot <hari.leo03@gmail.com>
2023-08-02 21:21:21 +0530

    Updated Java version

--- Codechef/Equinox/Equinox.java
+++ Codechef/Equinox/Equinox.java
@@ -10,6 +10,15 @@ class Equinox
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc = new Scanner(System.in);
-        
+        int T = sc.nextInt();
+        while (T-- > 0)
+        {
+            int N = sc.nextInt();
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            String[] S= new String[N];
+            
+
+        }
 	}
 }
