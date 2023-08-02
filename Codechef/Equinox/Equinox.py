cee45b6dccf2dd8641c50ab62482f52cbd38cde6
Haleshot <hari.leo03@gmail.com>
2023-08-02 20:39:17 +0530

    Updated Python version

--- Codechef/Equinox/Equinox.py
+++ Codechef/Equinox/Equinox.py
@@ -3,5 +3,17 @@ while T:
     T -= 1
     N, A, B = map(int, input().split())
     S = []
+    c1, c2 = 0, 0
     for i in range(N):
         S.append(input())
+    for i in S:
+        if i in "EQUINOX":
+            c1 += A
+        else:
+            c2 += B
+    if (c1 > c2):
+        print("SARTHAK")
+    elif (c1 < c2):
+        print("ANURADHA")
+    else:
+        print("DRAW")
