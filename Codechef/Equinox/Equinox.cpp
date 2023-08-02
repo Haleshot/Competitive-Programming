b8cff2505067f6130a25e072588de895a46fa70f
Haleshot <hari.leo03@gmail.com>
2023-08-02 21:06:57 +0530

    Updated C++ Version

--- Codechef/Equinox/Equinox.cpp
+++ Codechef/Equinox/Equinox.cpp
@@ -7,7 +7,14 @@ int main()
     cin >> T;
     while (T--)
     {
-        
+        int N, A, B;
+        cin >> N >> A >> B;
+        string S[N];
+        for (int i = 0; i < N; i++)
+        {
+            
+        }
+
     }
     return 0;
 }
