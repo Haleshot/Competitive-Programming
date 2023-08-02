3a176cef4cc29ad0c827ed1845d2fe7773edc0f4
Haleshot <hari.leo03@gmail.com>
2023-08-02 21:09:05 +0530

    Updated C++ Version

--- Codechef/Equinox/Equinox.cpp
+++ Codechef/Equinox/Equinox.cpp
@@ -7,13 +7,23 @@ int main()
     cin >> T;
     while (T--)
     {
-        int N, A, B;
+        int N, A, B, c1 = 0, c2 = 0;
         cin >> N >> A >> B;
         string S[N];
         for (int i = 0; i < N; i++)
         {
-            
+            cin >> S[i];
+            if (S[i] == "E" || S[i] == "Q" || S[i] == "U" ||  S[i] == "I" || S[i] == "N" || S[i] == "O" || S[i] == "X" )
+                c1 += 1;
+            else
+                c2 += 1;
         }
+        if (c1 > c2)
+            cout << "SARTHAK" << endl;
+        else if (c1 < c2)
+            cout << "ANURADHA" << endl;
+        else
+            cout << "DRAW" << endl;
 
     }
     return 0;
