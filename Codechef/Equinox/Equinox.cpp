058ab2fb575195e67a9599d6add0d661966c9c41
Haleshot <hari.leo03@gmail.com>
2023-08-02 21:13:48 +0530

    Updated C++ Version

--- Codechef/Equinox/Equinox.cpp
+++ Codechef/Equinox/Equinox.cpp
@@ -9,10 +9,10 @@ int main()
     {
         int N, A, B, c1 = 0, c2 = 0;
         cin >> N >> A >> B;
-        char S[N];
+        string S;
         for (int i = 0; i < N; i++)
         {
-            cin >> S[i];
+            cin >> S;
             if(S[0] == 'E' || S[0] == 'Q' || S[0] == 'U'|| S[0] == 'I'|| S[0] == 'N'|| S[0] == 'O'|| S[0] == 'X')
                 c1 += A;
             else
