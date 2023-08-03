b60c2055d78166fd3fc34762d76264d10464fbf4
Haleshot <hari.leo03@gmail.com>
2023-08-03 21:05:49 +0530

    Updated Python version

--- Codechef/Sticks/Sticks.py
+++ Codechef/Sticks/Sticks.py
@@ -3,4 +3,13 @@ while T:
     T -= 1
     N = int(input())
     A = list(map(int, input().split()))
+    totalArea = 0
+    for i in range(N):
+        l = A[i]
+        b = l[i + 1]
+        if (A.count(l) == 2 and A.count(b) == 2):
+            area = l * b
+            if (area > totalArea):
+                totalArea = area
     
+    print(totalArea)
