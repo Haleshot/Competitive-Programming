36d37c202b0d9a0431d474763242905ee9db672b
Haleshot <hari.leo03@gmail.com>
2023-08-06 21:12:56 +0530

    Updated Python version

--- Codechef/BMI/BMI.py
+++ Codechef/BMI/BMI.py
@@ -1,4 +1,13 @@
 T = int(input())
 while T:
     T -= 1
-    
+    M, H = map(int, input().split())
+    result = M/H**2
+    if result <= 18:
+        print(1)
+    elif result >= 19 and result <= 24:
+        print(2)
+    elif result >= 25 and result <= 29:
+        print(3)
+    else:
+        print(4)
