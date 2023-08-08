08b27b13253e16adb62299811f51811c142cd290
Haleshot <hari.leo03@gmail.com>
2023-08-08 21:14:15 +0530

    Updated Python version

--- Codechef/Slab/Slab.py
+++ Codechef/Slab/Slab.py
@@ -1,4 +1,9 @@
 T = int(input())
 while T:
     T -= 1
-    
+    N = int(input())
+    if (N <= 250000):
+        print(N)
+    elif (N > 250000 and N <= 500000):
+        net_total = N - 0.05 * N
+        print(net_total)
