a24f2220008ceff1610111d5e7cc195cf4e53c79
Haleshot <hari.leo03@gmail.com>
2023-08-08 21:38:38 +0530

    Updated Python version

--- Codechef/Slab/Slab.py
+++ Codechef/Slab/Slab.py
@@ -4,9 +4,9 @@ while T:
     N = int(input())
     if (N <= 250000):
         net_total = N
+
     elif (N > 250000 and N <= 500000):
-        net_total = N - (0.05 * N)
-       
+        net_total = N - ((N - 250000) * 0.05 )   
     
     elif (N > 500000 and N <= 750000):
         net_total = N - ((250000 * 0.05) + (N - 500000) * 0.1) 
