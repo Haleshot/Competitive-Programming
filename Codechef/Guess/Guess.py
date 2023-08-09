9548fd662f36f8cf5f4043511e4b41bd07d7d722
Haleshot <hari.leo03@gmail.com>
2023-08-09 21:27:42 +0530

    Updated Python version

--- Codechef/Guess/Guess.py
+++ Codechef/Guess/Guess.py
@@ -4,15 +4,19 @@ T = int(input())
 while T:
     T -= 1
     N, M = map(int, input().split())
-    l = []
-    alice_c = 0
-    for i in range(1, N + 1):
-        for j in range(1, M + 1):
-            l.extend([[i, j]])
-    for i in l:
-        if (i[0] + i[1]) % 2 == 1:
-            alice_c += 1
-    common = gcd(alice_c, len(l))
-    alice_c = alice_c // common
-    denom = len(l) // common
-    print(alice_c, "/", denom, sep='')
+    # l = []
+    # alice_c = 0
+    # for i in range(1, N + 1):
+    #     for j in range(1, M + 1):
+    #         l.extend([[i, j]])
+    # for i in l:
+    #     if (i[0] + i[1]) % 2 == 1:
+    #         alice_c += 1
+    # common = gcd(alice_c, len(l))
+    # alice_c = alice_c // common
+    # denom = len(l) // common
+    l = N * M
+    if l % 2 == 0:
+        print("1/2")
+    else:
+        print(str(l // 2), "/", str(l), sep='')
