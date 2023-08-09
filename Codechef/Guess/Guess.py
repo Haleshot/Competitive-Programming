b8b47ab6a7f53a1f1022a9f38b0a5d2718803158
Haleshot <hari.leo03@gmail.com>
2023-08-09 20:55:38 +0530

    Updated Python version

--- Codechef/Guess/Guess.py
+++ Codechef/Guess/Guess.py
@@ -2,4 +2,8 @@ T = int(input())
 while T:
     T -= 1
     N, M = map(int, input().split())
-    
+    l = []
+    for i in range(1, N):
+        for j in range(1, M):
+            l.extend([[i, j]])
+            print(l)
