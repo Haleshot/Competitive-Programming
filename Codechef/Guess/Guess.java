d4b0e916ea5d036b423a02da508b6edac66e7ab8
Haleshot <hari.leo03@gmail.com>
2023-08-09 21:44:33 +0530

    Updated Java version

--- Codechef/Guess/Guess.java
+++ Codechef/Guess/Guess.java
@@ -16,13 +16,11 @@ class Guess
             int N = sc.nextInt();
             int M = sc.nextInt();
             if ((N * M) % 2 == 0)
-            {
                 System.out.println("1/2");
-            }
+            
             else
-            {
                 System.out.println(Math.floor((N * M) / 2) + "/" + (N * M));
-            }
+            
         }
         sc.close();
 	}
