2d5d20a58554f16e927ce0193d655fe9a314ca9a
Haleshot <hari.leo03@gmail.com>
2023-08-09 21:32:24 +0530

    Updated C++ Version

--- Codechef/Guess/Guess.cpp
+++ Codechef/Guess/Guess.cpp
@@ -7,7 +7,16 @@ int main()
     cin >> T;
     while (T--)
     {
-        
+        int N, M;
+        cin >> N >> M;
+        if ((N * M) % 2 == 0)
+        {
+            cout << "1/2" << endl;
+        }
+        else
+        {
+            cout << (N/M) << "/" << (N) << endl;
+        }
     }
     return 0;
 }
