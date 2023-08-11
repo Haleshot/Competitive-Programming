827cf902663e8af8db892bba8118f3d46eef6e66
Haleshot <hari.leo03@gmail.com>
2023-08-11 13:05:58 +0530

    Updated Python version

--- Codechef/Colour/Colour.py
+++ Codechef/Colour/Colour.py
@@ -2,4 +2,8 @@ T = int(input())
 while T:
     T -= 1
     X, Y, Z = map(int, input().split())
+    if (X == 1 and Y == 1 and Z == 1):
+        print(3)
     
+    elif (X == 0 and Y == 0 and Z == 0):
+        print(0)
