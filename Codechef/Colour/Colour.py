a2ecb2a8c59273a765ad610b3f1441b70720b0f4
Haleshot <hari.leo03@gmail.com>
2023-08-11 21:02:18 +0530

    Updated Python version

--- Codechef/Colour/Colour.py
+++ Codechef/Colour/Colour.py
@@ -3,14 +3,28 @@ while T:
     T -= 1
     X, Y, Z = map(int, input().split())
     c = 0
-    if (X == 1 and Y == 1 and Z == 1):
-        print(3)
+    if X > 0:
+        c += 1
+
+    if Y > 0:
+        c += 1
+
+    if Z > 0:
+        c += 1
     
-    elif (X == 0 and Y == 0 and Z == 0):
-        print(0)
+    elif 
 
     else:
         X_remain = X - 1
         Y_remain = Y - 1
         Z_remain = Z - 1
-        
+        r = [X_remain, Y_remain, Z_remain]
+        r.sort()
+
+        if r[0] >= 2:
+            c += 3
+        elif r[0] == 1 and r[2] >= 2:
+            c += 2
+        elif r[1] >= 1:
+            c += 1
+    print(c)
