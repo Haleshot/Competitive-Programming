18d167d4484c688480ddd3a0f46e8f9df7f48afe
Haleshot <hari.leo03@gmail.com>
2023-08-12 20:57:38 +0530

    Updated Python version

--- Codechef/Play_Fit/Play_Fit.py
+++ Codechef/Play_Fit/Play_Fit.py
@@ -3,5 +3,15 @@ while T:
     T -= 1
     N = int(input())
     L = list(map(int, input().split()))
-    for i in L:
-        print(i)
+    maximum = -1
+    for i in range(N):
+        for j in range(i + 1, N):
+            diff = L[j] - L[i]
+            # print(diff)
+            if (diff > maximum):
+                maximum = diff
+                # print("YES")
+    if maximum > 0:
+        print(maximum)
+    else:
+        print("UNFIT")
