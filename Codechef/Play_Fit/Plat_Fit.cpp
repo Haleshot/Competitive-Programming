778634bccc5160044ce31dc32d020e8120dfa666
Haleshot <hari.leo03@gmail.com>
2023-08-12 21:21:02 +0530

    Updated C++ Version

--- Codechef/Play_Fit/Plat_Fit.cpp
+++ Codechef/Play_Fit/Plat_Fit.cpp
@@ -15,7 +15,17 @@ int main()
             cin >> L[i];
         }
         int minimum = L[0], maximum = 0;
-        
+        for (int i = 0; i < N; i++)
+        {
+            if (L[i] < minimum)
+                minimum = L[i];
+            else if (L[i] - minimum > maximum)
+                maximum = L[i] - minimum;
+        }
+        if (maximum > 0)
+            cout << maximum << endl;
+        else
+            cout << "UNFIT" << endl;
     }
     return 0;
 }
