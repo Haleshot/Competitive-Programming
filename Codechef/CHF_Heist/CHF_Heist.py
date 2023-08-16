7d93b41e22882116bb924e709bf5dcf1e2a0a924
Haleshot <hari.leo03@gmail.com>
2023-08-16 21:06:38 +0530

    Updated Python version

--- Codechef/CHF_Heist/CHF_Heist.py
+++ Codechef/CHF_Heist/CHF_Heist.py
@@ -2,3 +2,9 @@ T = int(input())
 while T:
     T -= 1
     D, d, P, Q = map(int, input().split())
+    money = 0
+    for i in range(d):
+        money += P
+    for j in range(i, D, d):
+        money += (P + (j * Q))
+    print(money)
