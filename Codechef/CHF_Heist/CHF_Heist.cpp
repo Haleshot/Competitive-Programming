8620534548749657e7d257cf6a2f9d3755f4460a
Haleshot <hari.leo03@gmail.com>
2023-08-16 21:26:02 +0530

    Updated C++ Version

--- Codechef/CHF_Heist/CHF_Heist.cpp
+++ Codechef/CHF_Heist/CHF_Heist.cpp
@@ -9,7 +9,8 @@ int main()
     {
         long long D, d, P, Q;
         cin >> D >> d >> P >> Q;
-        
+        int res = ((d * (D / d) * (2 * P + ((D / d) - 1) * Q)) / 2) + ((D % d) * (P + (D / d) * Q));
+        cout << res << endl;
     }
     return 0;
 }
