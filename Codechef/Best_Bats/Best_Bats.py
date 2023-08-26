b391086603056f26a026757169f7e5571390cbd7
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-08-26 21:42:27 +0530

    Updated Python version

--- Codechef/Best_Bats/Best_Bats.py
+++ Codechef/Best_Bats/Best_Bats.py
@@ -1,5 +1,14 @@
+import math
 T = int(input())
 while T:
     T -= 1
     L = list(map(int, input().split()))
     K = int(input())
+
+    m = L[11 - K]
+    c1 = L.count(m)
+    c = 0
+    for i in range(11 - K, 11):
+        if L[i] == m:
+            c += 1
+    print(math.comb(c1,c))
