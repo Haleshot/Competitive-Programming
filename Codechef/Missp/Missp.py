b50949568f5601b98bd436abfb9d2350864b9bf6
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-08-29 21:45:44 +0530

    Updated Python version

--- Codechef/Missp/Missp.py
+++ Codechef/Missp/Missp.py
@@ -2,5 +2,10 @@ T = int(input())
 while T:
     T -= 1
     N = int(input())
-    L = list(map(int, input().split()))
-    
+    c = 0
+    for i in range(N):
+        ele = int(input())
+        if ele % 2 == 1:
+            c += 1
+
+    print(c)
