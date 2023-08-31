e8201f86a3c5ea90b11827f13029ea37514cd8ff
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-08-31 17:20:43 +0530

    Updated Python version

--- Codechef/Equal_Ele/Equal_Ele.py
+++ Codechef/Equal_Ele/Equal_Ele.py
@@ -1,6 +1,8 @@
+from collections import Counter
 T = int(input())
 while T:
     T -= 1
     N = int(input())
     A = list(map(int, input().split()))
-    
+    D = Counter(A)
+    print(N - max(D.values()))
