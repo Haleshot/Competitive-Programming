766ddcc451b2cba7a05ce1c7787eae2ce49bb890
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-01 22:26:59 +0530

    Updated Python version

--- Codechef/Dominant_2/Dominant_2.py
+++ Codechef/Dominant_2/Dominant_2.py
@@ -3,4 +3,12 @@ while T:
     T -= 1
     N = int(input())
     A = list(map(int, input().split()))
-    
+    L = []
+    for i in A:
+        ele = A.count(i)
+        L.append(ele)
+    L.sort(reverse=True)
+    if L[0] > L[1]:
+        print("YES")
+    else:
+        print("NO")
