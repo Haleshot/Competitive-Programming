67ca4997d172696771132fc387c038c931079196
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-04 21:54:13 +0530

    Updated Python version

--- Codechef/Bet_Deal/Bet_Deal.py
+++ Codechef/Bet_Deal/Bet_Deal.py
@@ -2,4 +2,11 @@ T = int(input())
 while T:
     T -= 1
     A, B = map(int, input().split())
-    
+    min_A = 100 - A
+    min_B = 200 - B
+    if min_A < min_B:
+        print("FIRST")
+    elif min_A > min_B:
+        print("SECOND")
+    else:
+        print("BOTH")
