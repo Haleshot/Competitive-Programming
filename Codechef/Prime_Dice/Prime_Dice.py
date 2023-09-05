527259673d78fbd8020606095309f271da91d2f7
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-05 22:10:21 +0530

    Updated Python version

--- Codechef/Prime_Dice/Prime_Dice.py
+++ Codechef/Prime_Dice/Prime_Dice.py
@@ -1,4 +1,20 @@
+def isPrime(X):
+    c = 1
+    for i in range(2, X):
+        if (X % i == 0):
+            c += 1
+    if (c == 2):
+        return True
+    else:
+        return False
+
+
 T = int(input())
 while T:
     T -= 1
-    
+    A, B = map(int, input().split())
+    total = A + B
+    if (isPrime(total)):
+        print("ALICE")
+    else:
+        print("BOB")
