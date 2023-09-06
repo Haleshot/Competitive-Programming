45076b810643f83a466cf5809c591b806b07bd19
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-06 21:58:23 +0530

    Updated Python version

--- Codechef/Divide_Make_EQ/Divide_Make_EQ.py
+++ Codechef/Divide_Make_EQ/Divide_Make_EQ.py
@@ -1,6 +1,10 @@
+from math import gcd
 T = int(input())
 while T:
     T -= 1
     N = int(input())
     A = list(map(int, input().split()))
-    
+    if gcd(*A) != min(A):
+        print(len(A))
+    else:
+        print(len(A) - A.count(min(A)))
