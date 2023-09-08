0175a439ee54cd547fcaa98e0c4fc2ce1d0ab227
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-08 21:57:25 +0530

    Updated Python version

--- Codechef/Max_Diff/Max_Diff.py
+++ Codechef/Max_Diff/Max_Diff.py
@@ -3,4 +3,14 @@ while T:
     T -= 1
     N, K = map(int, input().split())
     W = list(map(int, input().split()))
-    
+    W.sort()
+    sum_kid, sum_Chef = 0, 0
+    print(W)
+    # sum_kid = [i for i in W[0:K]]
+    for i in range(K + 1):
+        sum_kid += 1
+    for i in range(K, N + 1):
+        sum_Chef += 1
+
+    print(sum_Chef, " ", sum_kid)
+    # print(sum_Chef - sum_kid)
