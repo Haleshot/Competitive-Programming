4682a1b6ea2438b7f001e8898efe246db5ae952c
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-09 22:01:40 +0530

    Updated Python version

--- Codechef/Perm_Exis/Perm_Exis.py
+++ Codechef/Perm_Exis/Perm_Exis.py
@@ -3,4 +3,13 @@ while T:
     T -= 1
     N = int(input())
     A = list(map(int, input().split()))
-    
+    A.sort()
+    c = 0
+    for i in range(N - 1):
+        j = i + 1
+        if abs(A[j] - A[i]) <= 1:
+            c += 1
+    if c > 0:
+        print("YES")
+    else:
+        print("NO")
