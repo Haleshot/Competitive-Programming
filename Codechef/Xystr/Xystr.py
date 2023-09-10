b5bdaeac2eaf60acf564937fc909a1b7c7a57e39
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-10 22:49:13 +0530

    Updated Python version

--- Codechef/Xystr/Xystr.py
+++ Codechef/Xystr/Xystr.py
@@ -1,5 +1,10 @@
 T = int(input())
 while T:
     T -= 1
-    S = int(input())
-    
+    S = input()
+    L = list(S)
+    c = 0
+    L1 = S.count("xy")
+    L3 = S.replace("xy","")
+    L2 = L3.count("yx")
+    print(L1 + L2)
