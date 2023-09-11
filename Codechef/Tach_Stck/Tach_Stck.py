4e1c498a407ebcebadc68f29808eca761b083576
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-11 22:36:58 +0530

    Updated Python version

--- Codechef/Tach_Stck/Tach_Stck.py
+++ Codechef/Tach_Stck/Tach_Stck.py
@@ -1,4 +1,11 @@
-T = int(input())
-while T:
-    T -= 1
-    
+N, M = map(int, input().split())
+X = [int(input()) for i in range(N)]
+X.sort()
+i, a = 0, 0
+while i < N - 1:
+    if X[i + 1] - X[ i ] <= M:
+        a += 1
+        i += 2 
+    else:
+        i += 1 
+print(a)
