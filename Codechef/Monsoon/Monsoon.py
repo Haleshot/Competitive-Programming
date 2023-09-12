c3f04d30ca01a60654c93af19af3641c9d846e73
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-12 22:15:42 +0530

    Updated Python version

--- Codechef/Monsoon/Monsoon.py
+++ Codechef/Monsoon/Monsoon.py
@@ -4,4 +4,16 @@ while T:
     N, M, H = map(int, input().split())
     X = list(map(int, input().split()))
     Y = list(map(int, input().split()))
-    
+    NM_min = min(N, M)
+
+    X.sort(reverse=True)
+    Y.sort(reverse=True)
+
+    c = 0
+
+    for i in range(NM_min):
+        if X[i] < (Y[i] * H):
+            c = c + X[i]
+        else:
+            c = c+( Y[i] * H)
+    print(c)
