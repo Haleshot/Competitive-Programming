31df856788cb78cc66d9044e3bb93648ba900040
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-13 23:30:06 +0530

    Updated Python version

--- Codechef/Tri_Coin/Tri_Coin.py
+++ Codechef/Tri_Coin/Tri_Coin.py
@@ -1,4 +1,11 @@
 T = int(input())
 while T:
     T -= 1
-    
+    N = int(input())
+    max_height = 0
+
+    while N >= max_height + 1:
+        max_height += 1
+        N -= max_height
+
+    print(max_height)
