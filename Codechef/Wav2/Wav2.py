ba4857c5aeda74b5300106ca4819986638a7a965
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-14 22:28:43 +0530

    Updated Python version

--- Codechef/Wav2/Wav2.py
+++ Codechef/Wav2/Wav2.py
@@ -1,4 +1,12 @@
-T = int(input())
-while T:
-    T -= 1
-    
+import bisect
+N, Q = map(int, input().split())
+B = sorted([int(x) for x in input().split()])
+for _ in range(Q):
+    x = int(input())
+    c = bisect.bisect_left(B, x)
+    if c < N and B[c] == x:
+        print("0")
+    elif c == N or (N - 1 - c) %2 != 0:
+        print("POSITIVE")
+    else:
+        print("NEGATIVE")
