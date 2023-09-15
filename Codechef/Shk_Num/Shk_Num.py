aba35f11dd7c66964594ba27bfac7d195c721adf
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-15 21:19:44 +0530

    Updated Python version

--- Codechef/Shk_Num/Shk_Num.py
+++ Codechef/Shk_Num/Shk_Num.py
@@ -1,5 +1,14 @@
+L = []
+for i in range(31):
+    for j in range(i + 1, 31):
+        L.append(pow(2, i) + pow(2, j))
+L.sort()
+
 T = int(input())
 while T:
     T -= 1
     N = int(input())
-    
+    i = 0 
+    while(L[i] < N):
+        i+=1 
+    print(min(abs(L[i] - N), abs(N - L[i - 1])))
