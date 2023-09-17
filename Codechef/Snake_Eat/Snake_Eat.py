2ea7f57a1659bcd2a4cffa8c58bf6be2466af4c6
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-17 22:16:37 +0530

    Updated Python version

--- Codechef/Snake_Eat/Snake_Eat.py
+++ Codechef/Snake_Eat/Snake_Eat.py
@@ -1,7 +1,24 @@
+import bisect as bs
+
 T = int(input())
 while T:
     T -= 1
-    N, Q = map(int(input()))
+    N, Q = map(int, input().split())
     L = list(map(int, input().split()))
-    K = int(input())
-    
+    L.sort()
+    sum=[0] * N
+    for j in range(1, N):
+        sum[j] = sum[j - 1] + L[j]
+    for i in range(Q):
+        K = int(input())
+        pos = bs.bisect_left(L, K)
+        l = 0 
+        r = pos
+        while l < r:
+            m = (l + r) // 2
+            need = K * (pos -m ) - (sum[pos - 1] - sum[m - 1])
+            if(need <= m):
+                r = m
+            else:
+                l = m + 1
+        print(N - r)        
