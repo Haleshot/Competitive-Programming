f2258c6bb8cfb65eb61cc064b9137dd8cfc234bb
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-18 22:27:42 +0530

    Updated Python version

--- Codechef/Alta_Ray/Alta_Ray.py
+++ Codechef/Alta_Ray/Alta_Ray.py
@@ -4,3 +4,10 @@ while T:
     N = int(input())
     A = list(map(int, input().split()))
     
+    res = [1 for i in range(N)]
+    for i in range(N-2, -1, -1):
+        if A[i + 1] * A[i] < 0:
+            res[i] = res[i + 1] + 1 
+    for i in res[:-1]:
+        print(i, end = " ")
+    print(res[-1])
