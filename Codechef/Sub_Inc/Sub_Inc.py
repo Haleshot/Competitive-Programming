16c337827ca96a606e3b8081a04a71a3edc1c048
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-19 21:31:31 +0530

    Updated Python version

--- Codechef/Sub_Inc/Sub_Inc.py
+++ Codechef/Sub_Inc/Sub_Inc.py
@@ -3,4 +3,14 @@ while T:
     T -= 1
     N = int(input())
     A = list(map(int, input().split()))
-    
+    count, ans = 1,0
+    last = A[0]
+    for i in A[1:]:
+        if i >= last:
+            count += 1
+        else:
+            ans += count * (count + 1) // 2
+            count = 1
+        last = i
+    ans += count*(count + 1) // 2
+    print(ans)
