0ab84f3afc8002380ede5629f26826a7225825e6
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-20 21:38:20 +0530

    Updated Python version

--- Codechef/Sum_Train/Sum_Train.py
+++ Codechef/Sum_Train/Sum_Train.py
@@ -4,3 +4,7 @@ while T:
     N = int(input())
     A = list(map(int, input().split()))
     
+    for j in range(N - 2, -1, -1):
+        for k in range(0, j + 1):
+            A[j][k] += max(A[j + 1][k], A[j + 1][k + 1])
+    print(A[0][0])
