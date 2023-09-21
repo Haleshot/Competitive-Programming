a5363747c470d1e30c32bf38ed5c9695bce074d5
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-21 19:51:51 +0530

    Updated Python version

--- Codechef/Max_Comp/Max_Comp.py
+++ Codechef/Max_Comp/Max_Comp.py
@@ -3,6 +3,10 @@ while T:
     T -= 1
     N = int(input())
     V = [[0 for i in range(49)] for j in range(49)]
-    max_list = [0] * 49
+    maximum_list = [0] * 49
     for i in range(N):
         S, E, C = list(map(int, input().split()))
+    for end in range(49):
+        for begin in range(end):
+            maximum_list[end] = max(maximum_list[end], V[begin][end] + maximum_list[begin])
+    print(maximum_list[48])
