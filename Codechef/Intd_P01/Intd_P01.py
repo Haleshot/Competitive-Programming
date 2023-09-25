8a643b703cc5e1d7b03051f66c29cb9a49d76c39
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-25 19:38:21 +0530

    Updated Python version

--- Codechef/Intd_P01/Intd_P01.py
+++ Codechef/Intd_P01/Intd_P01.py
@@ -1,4 +1,8 @@
 T = int(input())
+X = list(map(int, input().split()))
 while T:
-    T -= 1
-    
+    c = X[:3]
+    for i in range(3, T):
+        S = min(c[-3:]) + X[i]
+        c.append(S)
+    print(min(c[-3:]))
