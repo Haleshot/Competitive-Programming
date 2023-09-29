89471d84f1296fc4eb5c8040276361ee073f0e15
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-09-29 13:20:46 +0530

    Updated Python version

--- Codechef/Vdates/Vdates.py
+++ Codechef/Vdates/Vdates.py
@@ -1,4 +1,10 @@
 T = int(input())
 while T:
     T -= 1
-    
+    D, L, R = map(int, input().split())
+    if(D >= L and D <= R):
+        print("Take second dose now")
+    elif(D > R):
+        print("Too Late")
+    else:
+        print("Too Early")
