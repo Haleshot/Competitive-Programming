686c3635257f0a4751293cb9bf1ca59f53f0d080
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-02 21:43:32 +0530

    Initial Commit

--- Codechef/IMDB/IMDB.py
+++ Codechef/IMDB/IMDB.py
@@ -1,4 +1,10 @@
 T = int(input())
 while T:
     T -= 1
-    
+    N, X = map(int, input().split())
+    G = []
+    for i in range(N):
+        L = list(map(int, input().split()))
+        if L[0] <= X:
+            G.append(L[1])
+    print(max(G))
