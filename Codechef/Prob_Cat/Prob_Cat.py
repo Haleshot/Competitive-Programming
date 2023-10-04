890ce9b377d6c69c2c29dbbb1005aa30103e5493
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-04 21:07:25 +0530

    Updated Python version

--- Codechef/Prob_Cat/Prob_Cat.py
+++ Codechef/Prob_Cat/Prob_Cat.py
@@ -2,9 +2,9 @@ T = int(input())
 while T:
     T -= 1
     X = int(input())
-    if X >= 1 and X <= 100:
+    if X >= 1 and X < 100:
         print("Easy")
-    if X > 100 and X <= 200:
+    elif X >= 100 and X < 200:
         print("Medium")
-    else:
+    elif X >= 200 and X <= 300:
         print("Hard")
