77091a6f0d7f5a3e1955dbbd2d90b87fbf3e7082
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-05 19:36:15 +0530

    Updated Python version

--- Codechef/Prog_Lang/Prog_Lang.py
+++ Codechef/Prog_Lang/Prog_Lang.py
@@ -2,4 +2,9 @@ T = int(input())
 while T:
     T -= 1
     A, B, A1, B1, A2, B2 = map(int, input().split())
-    
+    if A in [A1, B1] and B in [A1, B1]:
+        print(1)
+    elif A in [A2, B2] and B in [A2, B2]:
+        print(2)
+    else:
+        print(0)
