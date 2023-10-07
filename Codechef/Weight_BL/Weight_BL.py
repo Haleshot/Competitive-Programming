9165b700eb0ef40b05bcff38077070885dadb348
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-07 20:23:14 +0530

    Updated Python version

--- Codechef/Weight_BL/Weight_BL.py
+++ Codechef/Weight_BL/Weight_BL.py
@@ -1,3 +1,8 @@
 T = int(input())
 while T:
     T -= 1 
+    W1, W2, X1, X2, M = map(int, input().split())
+    if(W2 >= W1 + (M * X1) and W2 <= W1 + (M * X2)):
+        print(1)
+    else:
+        print(0)
