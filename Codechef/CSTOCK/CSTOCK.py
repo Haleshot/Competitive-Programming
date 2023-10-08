29710be5cb1d9cc4aa64b4378f1d363fac77d6c5
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-08 22:06:57 +0530

    Updated Python version

--- Codechef/CSTOCK/CSTOCK.py
+++ Codechef/CSTOCK/CSTOCK.py
@@ -1,4 +1,10 @@
 T = int(input())
 while T:
     T -= 1
-    
+    A, B, C, D = list(map(int, input().split(' ')))
+    E = (100 + D) / 100 * A
+    if (E >= B and E <= C):
+        print("YES")
+    else:
+        print("NO") 
+ 
