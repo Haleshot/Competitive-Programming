1f71e1a03b53565c7678f1224272191a17ebb895
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-09 21:27:03 +0530

    Updated Python version

--- Codechef/NFS/NFS.py
+++ Codechef/NFS/NFS.py
@@ -2,4 +2,8 @@ T = int(input())
 while T:
     T -= 1
     U, V, A, S = map(int, input().split())
-    
+    measure_v = (U ** 2) - (2 * A * S)
+    if measure_v > (V ** 2):
+        print("No")
+    else:
+        print("Yes")
