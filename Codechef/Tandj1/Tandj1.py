09ed59d03b98c4a71e658cd6ff4cb8393482680a
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-12 22:05:02 +0530

    Updated Python version

--- Codechef/Tandj1/Tandj1.py
+++ Codechef/Tandj1/Tandj1.py
@@ -2,4 +2,10 @@ T = int(input())
 while T:
     T -= 1
     a, b, c, d, K = map(int, input().split())
-    
+    steps = abs(c - a) +abs(d - b)
+    if steps == K:
+        print("Yes")
+    elif K > steps and (K - steps) %2 == 0:
+        print("Yes")
+    else:
+        print("No")
