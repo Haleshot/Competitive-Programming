199a1e5ca704ab8ac6c2129bb47331c09de2a9b5
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-13 20:38:02 +0530

    Updated Python version

--- Codechef/CHFRICH/CHFRICH.py
+++ Codechef/CHFRICH/CHFRICH.py
@@ -2,4 +2,10 @@ T  = int(input())
 while T:
     T -= 1
     A, B, X = map(int, input().split())
-    
+    c = 0
+    while True:
+        A += X
+        c += 1
+        if (A == B):
+            print(c)
+            break
