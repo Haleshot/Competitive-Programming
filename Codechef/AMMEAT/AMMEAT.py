8b22e2ab6c46bcd9edf8c419be44c19619e9f4d8
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-16 21:42:35 +0530

    Updated Python version

--- Codechef/AMMEAT/AMMEAT.py
+++ Codechef/AMMEAT/AMMEAT.py
@@ -1,6 +1,15 @@
 T = int(input())
 while T:
     T -= 1
-    N, M = map(int, input().split())
-    P = list(map(int, input().split()))
-    
+    N, M = map(int, input().sPlit())
+    P = list(map(int, input().sPlit()))
+    P.sort()
+    c = 0
+    for i in range(-1 , -len(P) - 1 , -1):
+        m = m - P[i]
+        c = c + 1
+        if m <= 0:
+            print(c)
+            break
+    else:
+        print(-1)
