050bf4dac4922e7c45f56068f80bdb5907f84f59
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-17 20:50:44 +0530

    Updated Python version

--- Codechef/RATING_PRAC/RATING_PRAC.py
+++ Codechef/RATING_PRAC/RATING_PRAC.py
@@ -1,4 +1,17 @@
 T = int(input())
 while T:
     T -= 1
-    
+    R = 1
+    n = int(input())
+    nums = list(map(int, input().split()))
+    k = []
+    for i in range(len(nums)):
+        k.append(nums[i])
+    k.sort()
+    for j in range(len(k)):
+        if nums[j] != k[j]:
+            R = 0
+    if R == 1:
+        print("YES")
+    else:
+        print("NO")
