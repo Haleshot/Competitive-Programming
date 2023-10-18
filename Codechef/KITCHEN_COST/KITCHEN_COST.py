bfc0fac7ce6977199697fae330182499ad7992f1
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-18 23:00:31 +0530

    Updated Python version

--- Codechef/KITCHEN_COST/KITCHEN_COST.py
+++ Codechef/KITCHEN_COST/KITCHEN_COST.py
@@ -4,3 +4,8 @@ while T:
     N, X = map(int, input().split())
     A = list(map(int, input().split()))
     B = list(map(int, input().split()))
+    c = 0
+    for i in range(N):
+        if(A[i] >= X):
+            c += B[i]
+    print(c)
