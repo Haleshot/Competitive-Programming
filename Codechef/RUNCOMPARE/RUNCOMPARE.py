1be60149c44cb19c5cfa388b2d06fee1a453eece
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-19 22:45:25 +0530

    Updated Python version

--- Codechef/RUNCOMPARE/RUNCOMPARE.py
+++ Codechef/RUNCOMPARE/RUNCOMPARE.py
@@ -4,4 +4,8 @@ while T:
     N = int(input())
     A = list(map(int, input().split()))
     B = list(map(int, input().split()))
-    
+    ans = 0 
+    for i in range(n):
+        if A[i] <= 2 * B[i] and B[i] <= 2 * A[i]:
+            ans += 1 
+    print(ans)
