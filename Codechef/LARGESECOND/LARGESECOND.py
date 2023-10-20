a68989380bb5c6cb2e6e408e93532e1c1a7ecf73
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-20 22:33:40 +0530

    Updated Python version

--- Codechef/LARGESECOND/LARGESECOND.py
+++ Codechef/LARGESECOND/LARGESECOND.py
@@ -2,5 +2,11 @@ T = int(input())
 while T:
     T -= 1
     N = int(input())
-    A = list(map(int, input().split()))
-    
+    A = list(map(str, input().split()))
+    new_A = []
+    for i in A:
+        if i not in new_A:
+            new_A.append(i)
+    new_A = list(map(int, new_A))
+    new_A.sort(reverse=True)
+    print(new_A[0] + new_A[1])
