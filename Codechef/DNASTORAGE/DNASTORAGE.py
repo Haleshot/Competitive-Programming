f5d416043bf9407d757a52798f3827faf984d58c
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-22 23:22:29 +0530

    Initial Commit

--- Codechef/DNASTORAGE/DNASTORAGE.py
+++ Codechef/DNASTORAGE/DNASTORAGE.py
@@ -1,4 +1,16 @@
 T = int(input())
 while T:
     T -= 1
-    
+    A = int(input())
+    Z = ""
+    H = str(input())
+    for j in range(0, A, 2):
+        if(H[j] + H[j + 1] == "00"):
+            Z = Z + "A"
+        elif(H[j] + H[j + 1] == "01"):
+            Z =Z + "T"
+        elif(H[j] + H[j + 1] == "10"):
+            Z = Z + "C"
+        else:
+            Z = Z + "G"
+    print(Z)
