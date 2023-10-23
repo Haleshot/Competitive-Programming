95c97147ea49564d6ef391ed181fe37c6910e0e1
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-23 22:00:21 +0530

    Updated Python version

--- Codechef/TWOSTR/TWOSTR.py
+++ Codechef/TWOSTR/TWOSTR.py
@@ -3,3 +3,13 @@ while T:
     T -= 1
     X = input()
     Y = input()
+    L = len(X)
+    c = 0
+    for i in range (L):
+        if(X[i] != '?' and Y[i] != '?'):
+            if(X[i] != Y[i]):
+                c += 1
+    if (c):
+        print('No')
+    else:
+        print('Yes')
