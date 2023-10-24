bffe180fc77d7881d58e61af5f9b64045a6163d3
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-24 20:47:31 +0530

    Updated Python version

--- Codechef/BLOBBYVOLLEY/BLOBBYVOLLEY.py
+++ Codechef/BLOBBYVOLLEY/BLOBBYVOLLEY.py
@@ -3,4 +3,16 @@ while T:
     T -= 1
     N = int(input())
     S = input()
-    
+    server = 0
+    alice = 0
+    bob = 0
+    for j in range(N):
+        if((server == 0) and (S[j] == 'A')):
+            alice += 1
+        elif((server == 0) and (S[j] == 'B')):
+            server = 1
+        elif((server == 1) and (S[j] == 'B')):
+            bob += 1
+        else:
+            server = 0
+    print(alice, '', bob)
