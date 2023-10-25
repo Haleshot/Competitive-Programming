3002879b4bd345944df371d50eff90331d5531c3
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-25 20:09:48 +0530

    Updated Python version

--- Codechef/HAPPYSTR/HAPPYSTR.py
+++ Codechef/HAPPYSTR/HAPPYSTR.py
@@ -5,11 +5,10 @@ while T:
     vowels = "aeiou"
     length = len(S)
     c = 0
-    
     for i in range(length - 2):
         if S[i] in vowels and S[i+1] in vowels and S[i+2] in vowels:
             c = 1
     if (c == 1):
         print("Happy")
     else:
-        print("SAD")
+        print("Sad")
