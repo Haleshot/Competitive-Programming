e554e4438a351eddc9b14c010982d65b7854eb8d
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-28 20:39:46 +0530

    Updated Python version

--- Codechef/EXPRESSION_3/EXPRESSION_3.py
+++ Codechef/EXPRESSION_3/EXPRESSION_3.py
@@ -1,3 +1,10 @@
 T = int(input())
 while T:
     T -= 1
+    L = list(map(int,input().split()))
+    max_L = max(L)
+    L.remove(max_L)
+    if max_L == sum(L):
+        print("Yes")
+    else:
+        print("No")
