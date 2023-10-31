281b56edd4f69c9471cd6b3bba20ea46437c3347
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-10-31 21:27:45 +0530

    Updated Python version

--- Codechef/EMPR/EMPR.py
+++ Codechef/EMPR/EMPR.py
@@ -2,4 +2,8 @@ T = int(input())
 while T:
     T -= 1
     P, X, Y, Z = map(int, input().split())
-    
+    if Z == 1:
+        P = P+ ( (P * Y) / 100)
+    else:
+        P = P - ((P * X / 100))
+    print('{0:.10f}'.format(P))
