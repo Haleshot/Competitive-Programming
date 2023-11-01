976785f7b24869c927602d522a88da8a28dcf435
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-01 20:46:57 +0530

    Updated Python version

--- Codechef/COLOR8TEST/COLOR8TEST.py
+++ Codechef/COLOR8TEST/COLOR8TEST.py
@@ -1,5 +1,7 @@
+import math
 T = int(input())
 while T:
     T -= 1
-    X1, X2, X3, X4, X5, X6 = list(map(int, input().split()))
-    
+    N, X1, X2, X3, X4, X5, X6 = list(map(int, input().split()))
+    X = (X1 + X2 + X3 + X4 + X5 + X6 ) * math.ceil((N / 2))
+    print(X)
