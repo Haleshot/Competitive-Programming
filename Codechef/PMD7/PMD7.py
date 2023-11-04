60b296d1f9a9a2a3dee26c22809e965228d0c976
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-04 21:53:37 +0530

    Updated Python version

--- Codechef/PMD7/PMD7.py
+++ Codechef/PMD7/PMD7.py
@@ -1,4 +1,13 @@
 T = int(input())
 while T:
     T -= 1
-    
+    X, M = map(int, input().split())
+    numNew = ""
+    for i in str(X):
+        d = int(i)
+        numNew += str(d ** M % 10)
+    revNum = numNew[::-1]
+    if int(revNum) % 7 == 0:
+        print("yes")
+    else:
+        print("no")
