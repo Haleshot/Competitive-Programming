4c5da84054dd21dd28ff75053453f4a5a15a7a75
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-05 20:37:10 +0530

    Updated Python version

--- Codechef/FNDINT/FNDINT.py
+++ Codechef/FNDINT/FNDINT.py
@@ -1,4 +1,14 @@
 T = int(input())
 while T:
     T -= 1
-    
+    X = int(input())
+    X = x + 1
+    X = str(x)
+    s = len(set(x))
+    l = len(x)
+    while(s != l):
+        x = int(x) + 1
+        x = str(x)
+        s = len(set(x))
+        l = len(x)
+    print(int(x))
