59a62550779b5e05e7e8b4de213251e30cf17b0c
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-06 22:28:16 +0530

    Initial Commit

---
+++ Codechef/ASTHIT/ASTHIT.py
@@ -0,0 +1,42 @@
+T = int(input())
+while T:
+    T -= 1
+    n = int(input())
+    dir = []
+    pow = []
+    
+    for i in range(n):
+        ele1, ele2 = list(map(int, input().split()))
+        dir.append(ele1)
+        pow.append(ele2)
+        
+    stack = []
+    a = []
+    
+    
+    for i in range(n):
+        if dir[i] == 1:
+            stack.append(i)
+        else:
+            while stack and pow[i] > pow[stack[-1]]:
+                pow[i] += pow[stack[-1]]
+                stack.pop()
+                
+            if stack:
+                if pow[i] == pow[stack[-1]]:
+                    stack.pop()
+                else:
+                    pow[stack[-1]] += pow[i]
+            else:
+                a.append(i)
+                
+    while stack:
+        a.append(stack.pop())
+    a.sort()
+    print(len(a))
+    
+    if len(a) > 0:
+        for i in a:
+            print(i + 1, end = " ")
+
+                
