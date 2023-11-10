fb6f21738971067ad6b579cb14a8d75588d1dcbf
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-10 17:41:45 +0530

    Updated Python version

--- Codechef/ROTLIST/ROTLIST.py
+++ Codechef/ROTLIST/ROTLIST.py
@@ -1,4 +1,25 @@
-T = int(input())
-while T:
-    T -= 1
+if not head:
+        return None
+
     
+    length = 1
+    current = head
+    while current.next:
+        current = current.next
+        length += 1
+
+
+    k = k % length
+
+    if k == 0:
+        return head;
+
+    
+    current = head
+    for _ in range(length - k - 1):
+        current = current.next
+
+    new_head = current.next
+    current.next = None
+    current = new_head
+    while curre
