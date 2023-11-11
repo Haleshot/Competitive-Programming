a0bcc3b92248c69fbba98d5c0aa170b549f465d1
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-11 18:31:05 +0530

    Updated Python version

--- Codechef/NODEAD/NODEAD.py
+++ Codechef/NODEAD/NODEAD.py
@@ -2,7 +2,7 @@ N, Q = map(int, input().split())
 arr = list(map(int, input().split()))
 for i in range(Q):
     arr2 = list(map(int, input().split()))
-    if len(arr2)==2:
+    if len(arr2) == 2:
         arr.pop(arr2[1] - 1)
     else:
         arr.insert(arr2[1] - 1, arr2[2])
