e66421338beb47f521f3278c26b0d56eb6d743b2
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-12 22:54:54 +0530

    Updated Python version

--- Codechef/SEGEO/SEGEO.py
+++ Codechef/SEGEO/SEGEO.py
@@ -1,4 +1,14 @@
 T = int(input())
 while T:
     T -= 1
-    
+    a = int(input())
+    v = list(map(int, input().split()))
+    even = []
+    odd = []
+    for i in v:
+        if i % 2 == 0:
+            even.append(i)
+        else:
+            odd.append(i)
+    ans = even + odd
+    print(*ans)
