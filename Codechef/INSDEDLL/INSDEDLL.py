fd4568e285a5c08fa193e8500893c9f707d53e80
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-13 19:16:51 +0530

    Updated Python version

--- Codechef/INSDEDLL/INSDEDLL.py
+++ Codechef/INSDEDLL/INSDEDLL.py
@@ -3,3 +3,27 @@ class Node:
         self.data = data
         self.next = None
         self.prev = None
+
+def insertNode(head, position, value):
+    new_node = Node(value)
+    if position == 1:
+        new_node.next = head
+        if head:
+            head.prev = new_node
+        return new_node
+
+    current = head
+    count = 1
+
+    while count < position - 1 and current.next:
+        current = current.next
+        count += 1
+
+    new_node.next = current.next
+    if current.next:
+        current.next.prev = new_node
+
+    current.next = new_node
+    new_node.prev = current
+
+    return head
