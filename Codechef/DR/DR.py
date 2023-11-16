ba2ef24d938289435eade6c965c8b13a7b7a44ed
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-16 22:48:14 +0530

    Updated Python version

--- Codechef/DR/DR.py
+++ Codechef/DR/DR.py
@@ -5,4 +5,9 @@ while T:
     Y = list(map(int,input().split()))
     A = []
     B = []
+    for j in range(0, len(Y)):
+        if Y[j] == Y[j - 1]:
+            A.append(Y[j])
+        else:
+            B.append(Y[j])
     
