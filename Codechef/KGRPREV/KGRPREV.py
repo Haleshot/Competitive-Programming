e756cdd0ec3c793e43baebe370e258f0ab2deeab
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-17 22:34:35 +0530

    Updated Python version

---
+++ Codechef/KGRPREV/KGRPREV.py
@@ -0,0 +1,10 @@
+T = int(input())
+while T:
+    A, B = map(int, input().split())
+    Li = list(map(int, input().split()))
+    res = []
+    for i in range(0, A, B):
+        temp =Li[i : i + B]
+        res += temp[::-1]
+    print(*res)
+    
