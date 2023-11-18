5a6585fc6f5ae8785f6915a23e865c8867a85281
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-18 20:48:18 +0530

    Updated Python version

---
+++ Codechef/CRITLIST/CRITLIST.py
@@ -0,0 +1,12 @@
+def solve(head):
+    count = 0
+    if head.next is None:
+        return 0
+    current = head.next
+    old_val = head.val
+    while not current.next is None:
+        if (old_val < current.val and current.next.val < current.val) or (old_val > current.val and current.next.val > current.val):
+            count += 1 
+        old_val = current.val
+        current = current.next
+    return count
