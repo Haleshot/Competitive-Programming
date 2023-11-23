0c6d9ec0fcc7d5c4ab8058697ceca5cb08306e49
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-23 20:12:40 +0530

    Updated version

--- Codechef/DIVNUMB/DIVNUMB.py
+++ Codechef/DIVNUMB/DIVNUMB.py
@@ -1,4 +1,11 @@
 T = int(input())
 while T:
     T -= 1
-    
+    N, A, B, C = map(int,input().split())
+    lst = set()
+    for i in range(1, N + 1):
+        lst.add(i * A)
+        lst.add(i * B)
+        lst.add(i * C)
+    lst = sorted(lst)
+    print(lst[N - 1])
