622260f87588afe8b115b0d5918fbfc3a4a3ace2
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-24 19:46:10 +0530

    Updated Python version

--- Codechef/ARRINT/ARRINT.py
+++ Codechef/ARRINT/ARRINT.py
@@ -5,4 +5,6 @@ while T:
     A1 = set(map(int, input().split()))
     B1 = set(map(int, input().split()))
     C1 = set(map(int, input().split()))
-    
+    A1 = A1.intersection(B1)
+    A1 = A1.intersection(C1)
+    print(len(A1))
