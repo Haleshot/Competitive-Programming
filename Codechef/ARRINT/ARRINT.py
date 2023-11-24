0cfd8e039c0d48c8551fae89f1d32fc630dcce6e
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-24 19:46:06 +0530

    Updated Python version

--- Codechef/ARRINT/ARRINT.py
+++ Codechef/ARRINT/ARRINT.py
@@ -2,4 +2,7 @@ T = int(input())
 while T:
     T -= 1
     A, B, C = map(int, input().split())
+    A1 = set(map(int, input().split()))
+    B1 = set(map(int, input().split()))
+    C1 = set(map(int, input().split()))
     
