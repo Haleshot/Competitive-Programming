4f09c4b0beb5e0ab8501e199d453522c3b532f10
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-25 18:45:10 +0530

    Updated Python version

--- Codechef/PRTSCH/PRTSCH.py
+++ Codechef/PRTSCH/PRTSCH.py
@@ -4,4 +4,9 @@ while T:
     N, M = map(int, input().split())
     A = set(list(map(int, input().split())))
     B = list(map(int, input().split()))
-    
+    for i in range(M):
+        if(B[i] in A):
+            print("Yes", end = " ")
+        else:
+            print("No", end = " ")
+    print()
