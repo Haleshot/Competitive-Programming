b286801e53a2c93d6bde3844261a53d6fded8528
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-26 19:21:40 +0530

    Updated Python version

--- Codechef/IIT/IIT.py
+++ Codechef/IIT/IIT.py
@@ -3,4 +3,9 @@ while T:
     T -= 1
     A, B = map(int, input().split())
     a1 = set(map(int, input().split()))
-    
+    for i in range(B):
+        b1 = int(input())
+        if b1 in a1:
+            print('Found')
+        else:
+            print('Not Found')
