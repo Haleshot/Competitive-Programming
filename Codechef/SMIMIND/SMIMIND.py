882341dd2f25759ff2faeea759eb6d66f39d9b58
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-27 18:54:21 +0530

    Updated Python version

--- Codechef/SMIMIND/SMIMIND.py
+++ Codechef/SMIMIND/SMIMIND.py
@@ -2,18 +2,13 @@ from bisect import bisect_left
 
 N, Q = map(int, input().split())
 A = list(map(int, ('0 ' + input()).split()))
-
 for i in range(1, N + 1):
     A[i] += A[i - 1]
-
 ans = []
 for _ in range(Q):
     i, x = map(int, input().split())
-    
     j = bisect_left(A, x + A[i - 1])
     if j > N:
         j = -1
-
     ans.append(j)
-
 print(*ans)
