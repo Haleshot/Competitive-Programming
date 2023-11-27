285dae0500c3ee66b379f9ced50d5b8b5a7911da
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-27 18:54:10 +0530

    Updated Python version

--- Codechef/SMIMIND/SMIMIND.py
+++ Codechef/SMIMIND/SMIMIND.py
@@ -5,3 +5,15 @@ A = list(map(int, ('0 ' + input()).split()))
 
 for i in range(1, N + 1):
     A[i] += A[i - 1]
+
+ans = []
+for _ in range(Q):
+    i, x = map(int, input().split())
+    
+    j = bisect_left(A, x + A[i - 1])
+    if j > N:
+        j = -1
+
+    ans.append(j)
+
+print(*ans)
