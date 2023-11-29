45788b930747257cbee0f3a6b7cf31cfd8144828
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-29 18:25:25 +0530

    Initial Commit

---
+++ Codechef/SDIGIT/SDIGIT.py
@@ -0,0 +1,18 @@
+T = int(input())
+while T:
+    T -= 1
+    D, N = map(int, input().split())
+    A = input().split()
+
+    ans = []
+    if D == 1:
+        ans = A[0] * N
+    else:
+        while N:
+            if A[0] != '0' or not ans:
+                N -= 1
+            N, i = divmod(N, D)
+            ans.insert(0, A[i])
+
+
+    print(''.join(ans))
