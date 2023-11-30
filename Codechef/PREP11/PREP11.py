af4e5ab3296ac5ef473192327542b1bab3b01f2c
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-11-30 20:28:26 +0530

    Initial Commit

---
+++ Codechef/PREP11/PREP11.py
@@ -0,0 +1,30 @@
+def is_allocation_possible(books, students, max_pages):
+    students_needed = 1
+    pages_allocated = 0
+
+    for pages in books:
+        if pages > max_pages:
+            return False
+
+        if pages + pages_allocated <= max_pages:
+            pages_allocated += pages
+        else:
+            students_needed += 1
+            pages_allocated = pages
+
+    return students_needed <= students
+
+def allocate_books(books, students):
+    low, high = max(books), sum(books)
+    result = -1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if is_allocation_possible(books, students, mid):
+            result = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return result
