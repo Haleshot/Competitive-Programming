d321fafe967192efd08fddb48b55d3748fd009cd
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-01 08:31:57 +0530

    Initial Commit

---
+++ Codechef/PREP08/PREP08.py
@@ -0,0 +1,30 @@
+def is_painting_possible(walls, painters, time_per_unit, max_time):
+    total_time = 0
+    painters_needed = 1
+
+    for wall in walls:
+        if wall * time_per_unit > max_time:
+            return False
+
+        if total_time + wall * time_per_unit <= max_time:
+            total_time += wall * time_per_unit
+        else:
+            painters_needed += 1
+            total_time = wall * time_per_unit
+
+    return painters_needed <= painters
+
+def minimum_painting_time(walls, painters, time_per_unit):
+    low, high = max(walls) * time_per_unit, sum(walls) * time_per_unit
+    result = -1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if is_painting_possible(walls, painters, time_per_unit, mid):
+            result = mid
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return result
