c291bbc520a24a47f13fa65923722144a5b95ecf
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-03 18:15:58 +0530

    Initial Commit

---
+++ Codechef/PREP39/PREP39.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+long long mod_pow(long long base, long long exp, long long mod) {
+    long long result = 1;
+    while (exp > 0) {
+        if (exp % 2 == 1) {
+            result = (result * base) % mod;
+        }
+        base = (base * base) % mod;
+        exp /= 2;
+    }
+    return result;
+}
