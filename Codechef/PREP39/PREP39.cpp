fef25d194039ea895747373db081eee1f724289c
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-03 18:16:05 +0530

    Updated Python version

--- Codechef/PREP39/PREP39.cpp
+++ Codechef/PREP39/PREP39.cpp
@@ -12,3 +12,16 @@ long long mod_pow(long long base, long long exp, long long mod) {
     }
     return result;
 }
+
+int main() {
+    long T;
+    cin >> T;
+    while (T != 0) {
+        long long N, X, M;
+        cin >> N >> X >> M;
+        long long power_value = mod_pow(N, X, M);
+        cout << power_value << endl;
+        T--;
+    }
+    return 0;
+}
