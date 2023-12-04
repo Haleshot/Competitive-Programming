8723a0cfed37565a9cb6ef771d19a0f6b0e146ba
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-04 21:20:16 +0530

    Updated Python version

--- Codechef/PYCC44/PYCC44.py
+++ Codechef/PYCC44/PYCC44.py
@@ -1,4 +1,9 @@
 N = int(input())
 for i in range(N):
     S, X, Y, Z = [int(x) for x in input().split()]
-    
+    if(X + Y + Z <= S):
+        print(0)
+    elif(Z + min(X, Y) > S):
+        print(2)
+    else:
+        print(1)
