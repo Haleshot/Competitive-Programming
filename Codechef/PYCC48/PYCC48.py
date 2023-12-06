e3551c93bdd7ae3ea2c942df41c8786c137826c9
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-06 22:38:22 +0530

    Updated Python version

--- Codechef/PYCC48/PYCC48.py
+++ Codechef/PYCC48/PYCC48.py
@@ -2,4 +2,6 @@ T = int(input())
 while T:
     T -= 1
     X, Y = map(int, input().split())
+    ans1 = (500 - (X * 2)) + (1000 - ((X + Y) * 4))
+    ans2 = (500 - (X + Y) * 2) + 1000 - (Y * 4)    
     
