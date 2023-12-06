1baf81ffac7509a4ea17d0f3328326373e597227
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-06 22:38:30 +0530

    Updated Python version

--- Codechef/PYCC48/PYCC48.py
+++ Codechef/PYCC48/PYCC48.py
@@ -4,4 +4,6 @@ while T:
     X, Y = map(int, input().split())
     ans1 = (500 - (X * 2)) + (1000 - ((X + Y) * 4))
     ans2 = (500 - (X + Y) * 2) + 1000 - (Y * 4)    
+    A = max(ans1, ans2)
+    print(A)
     
