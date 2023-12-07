680249a320408a0b442ed6cbd7fb3e7e5f0bc251
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-07 20:40:58 +0530

    Updated Python version

--- Codechef/PYCC49/PYCC49.py
+++ Codechef/PYCC49/PYCC49.py
@@ -2,4 +2,8 @@ T = int(input())
 while T:
     T -= 1
     N, X, P = map(int, input().split())
-    
+    total = X * 3 + (N - X) * (-1)
+    if total >= P:
+        print("Pass")
+    else:
+        print("Fail")
