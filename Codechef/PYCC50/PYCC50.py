a3efb9867f50468131cbeb039ea2c04301128da9
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-08 20:46:04 +0530

    Updated Python version

--- Codechef/PYCC50/PYCC50.py
+++ Codechef/PYCC50/PYCC50.py
@@ -2,4 +2,7 @@ T = int(input())
 while T:
     T -= 1
     A, B, C, D = map(int, input().split())
-    
+    if (A + C) == 180 or (B + D) == 180:
+        print("YES")
+    else:
+        print("NO")
