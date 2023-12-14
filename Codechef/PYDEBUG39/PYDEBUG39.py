5bcc42d9540242f3d60cd787a6fb7ec0a3bdfe65
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-14 20:44:17 +0530

    Updated Python version

--- Codechef/PYDEBUG39/PYDEBUG39.py
+++ Codechef/PYDEBUG39/PYDEBUG39.py
@@ -3,4 +3,8 @@ while T:
     T -= 1
     N = int(input())
     A = list(map(int, input().split()))
-    
+    count = 0
+    for i in A:
+        if i >= 10 and i <= 60:
+            count += 1
+    print(count)
