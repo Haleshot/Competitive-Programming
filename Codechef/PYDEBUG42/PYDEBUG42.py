bfefbe86de6d05bf71f12a23c66217bc134916c0
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-17 21:42:11 +0530

    Updated Python version

--- Codechef/PYDEBUG42/PYDEBUG42.py
+++ Codechef/PYDEBUG42/PYDEBUG42.py
@@ -1,4 +1,8 @@
-T = int(input().split())
+T = int(input())
 while T:
     T -= 1
     A, B, C, D = map(int, input().split())
+    if (A + C == 180) and (B + D == 180):
+        print("YES")
+    else:
+        print("NO")
