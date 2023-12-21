eb533b29f0aaf248d80481e8a34862921011dc5c
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-21 17:48:13 +0530

    Updated Python version

--- Codechef/EZSPEAK/EZSPEAK.py
+++ Codechef/EZSPEAK/EZSPEAK.py
@@ -1,6 +1,18 @@
 T = int(input())
 while T:
     T -= 1
-    N, S = map(int, input().split())
+    N = int(input())
     S = input()
-    
+    c = 0
+    ans = False
+    for i in str(S):
+        if i != 'a' and i != 'e' and i != 'i' and i != 'o' and i != 'u':
+            c += 1
+        else:
+            c = 0
+        if (c >= 4):
+            ans = True
+    if (ans == False):
+        print("YES")
+    else:
+        print("NO")
