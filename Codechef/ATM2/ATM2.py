b403b533a77c2d5cbe5a263132fe6b8d080781cf
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-22 19:38:02 +0530

    Updated Python version

--- Codechef/ATM2/ATM2.py
+++ Codechef/ATM2/ATM2.py
@@ -2,4 +2,12 @@ T = int(input())
 while T:
     T -= 1
     N, K = map(int, input().split())
-    
+    A = list(map(int, input().split()))
+    tid = []
+    for i in range(N):
+        if A[i] <= K:
+            K -= A[i]
+            tid.append(1)
+        else:
+            tid.append(0)
+    print(*tid, sep="")
