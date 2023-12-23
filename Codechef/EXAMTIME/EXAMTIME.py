8ce64139526f2a7b0949e33cf4597934bfc6efc7
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-23 20:11:34 +0530

    Updated Python version

--- Codechef/EXAMTIME/EXAMTIME.py
+++ Codechef/EXAMTIME/EXAMTIME.py
@@ -3,3 +3,16 @@ while T:
     T -= 1
     DDSA, DTOC, DDM = map(int, input().split())
     SDSA, STOC, SDM = map(int, input().split())
+    D_tot = DDSA + DTOC + DDM
+    S_tot = SDSA + STOC + SDM
+
+    if (D_tot > S_tot):
+        print("Dragon")
+    elif (S_tot > D_tot):
+        print("Sloth")
+    elif (D_tot == S_tot and DDSA > SDSA):
+        print("Dragon")
+    elif (D_tot == S_tot and SDSA > DDSA):
+        print("Sloth")
+    elif (D_tot == S_tot and SDSA == DDSA):
+        print("TIE")
