5b4e83e8759888cab35464e6ae37ee096cb3c598
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-25 19:27:18 +0530

    Updated Python version

--- Codechef/DIET/DIET.py
+++ Codechef/DIET/DIET.py
@@ -3,4 +3,14 @@ while T:
     T -= 1
     N, K = map(int, input().split())
     A = list(map(int, input().split()))
-    
+    total = 0
+    c = 0
+    for i in range(N):
+        total += A[i] - K
+        if total < 0:
+            c += 1
+            break
+    if c > 0:
+        print("NO", (i + 1))
+    else:
+        print("YES")
