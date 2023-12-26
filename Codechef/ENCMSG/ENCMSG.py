88826ae9a529aae1023126ad77b71e6eb9c58dec
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-26 19:53:07 +0530

    Updated Python version

--- Codechef/ENCMSG/ENCMSG.py
+++ Codechef/ENCMSG/ENCMSG.py
@@ -3,4 +3,15 @@ while T:
     T -= 1
     N = int(input())
     S = input()
-    
+    FS = ''
+    i = 0
+    S = list(S)
+    while i < N - 1:
+        j = ''
+        j += S[i]
+        S[i] = S[i + 1]
+        S[i + 1] = j
+        i += 2
+    for k in S:
+        FS += chr(122 - ord(k) + 97)
+    print(FS)
