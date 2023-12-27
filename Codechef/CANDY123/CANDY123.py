d7b3cea49460f3ad3b40ae1db8cf1b35c31b1cef
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-27 19:36:25 +0530

    Updated Python version

--- Codechef/CANDY123/CANDY123.py
+++ Codechef/CANDY123/CANDY123.py
@@ -2,3 +2,16 @@ T = int(input())
 while T:
     T -= 1
     A, B = map(int, input().split())
+    c = 0
+    for i in range(1, 1000):
+        if i % 2 == 1:
+            A = A - i
+            if(A < 0):
+                print("Bob")
+                break
+        else:
+            B = B - i
+            if(B < 0):
+                print("Limak")
+                break
+            
