3e53dc285a537d548c8a8fa9731d6056078f2a5d
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-28 17:20:03 +0530

    Updated Python version

--- Codechef/ELECTIONS/ELECTIONS.py
+++ Codechef/ELECTIONS/ELECTIONS.py
@@ -2,4 +2,11 @@ T = int(input())
 while T:
     T -= 1
     Xa, Xb, Xc = map(int, input().split())
-    
+    if (Xa > 50 and (Xb < 50 and Xc < 50)):
+        print("A")
+    elif (Xb > 50 and (Xa < 50 and Xc < 50)):
+        print("B")
+    elif (Xc > 50 and (Xa < 50 and Xb < 50)):
+        print("C")
+    else:
+        print("NOTA")
