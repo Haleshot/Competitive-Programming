bc71eca4292156d7bfa85270697170515b836bde
Haleshot <57552973+Haleshot@users.noreply.github.com>
2023-12-30 19:28:17 +0530

    Updated Python version

--- Codechef/REMOVECARDS/REMOVECARDS.py
+++ Codechef/REMOVECARDS/REMOVECARDS.py
@@ -1,6 +1,9 @@
+from statistics import mode
 T = int(input())
 while T:
     T -= 1
     N = int(input())
     A = list(map(int, input().split()))
-    
+    J = mode(A)
+    CN = A.count(J)
+    print(N - CN)
