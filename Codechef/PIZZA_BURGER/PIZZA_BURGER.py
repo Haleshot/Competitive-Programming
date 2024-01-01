0cbadcdbf7aaeeac052464a43a3ecaf0384861a0
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-01 17:42:34 +0530

    Updated Python version

--- Codechef/PIZZA_BURGER/PIZZA_BURGER.py
+++ Codechef/PIZZA_BURGER/PIZZA_BURGER.py
@@ -2,7 +2,7 @@ T = int(input())
 while T:
     T -= 1
     X, Y, Z = map(int, input().split())
-    if (Y <= Z and X >= Y):
+    if (X >= Y):
         print("PIZZA")
     elif (X >= Z and Y > Z):
         print("BURGER")
