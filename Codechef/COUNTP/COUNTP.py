5843467ca70ffa9fd221cba1449973b5d5c63b8f
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-03 17:56:01 +0530

    Updated Python version

--- Codechef/COUNTP/COUNTP.py
+++ Codechef/COUNTP/COUNTP.py
@@ -3,4 +3,11 @@ while T:
     T -= 1
     N = int(input())
     A = list(map(int, input().split()))
-    
+    F, C = 0, 0
+    for i in A:
+        if(i % 2 == 1):
+            C += 1
+    if(C % 2 == 1 or C == 0):
+        print('NO')
+    else:
+        print('YES')
