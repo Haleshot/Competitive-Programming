522eb1e683b2c813854363dce05419494a117a7a
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-04 11:40:44 +0530

    Updated Python version

--- Codechef/HEADBOB/HEADBOB.py
+++ Codechef/HEADBOB/HEADBOB.py
@@ -3,5 +3,17 @@ while T:
     T -= 1
     N = int(input())
     S = input()
-
-    
+    c1, c2, c3 = 0, 0, 0
+    for i in range(N):
+        if S[i] == "Y":
+            c1 += 1
+        elif S[i] == "N":
+            c2 += 1
+        elif S[i] == "I":
+            c3 += 1
+    if (c2 == N):
+        print("NOT SURE")
+    elif (c1 > 0):
+        print("NOT INDIAN")
+    elif (c3 > 0):
+        print("INDIAN")
