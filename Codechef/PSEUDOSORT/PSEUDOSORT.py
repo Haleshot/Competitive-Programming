dd9a5bf7d9ff02b3eec45fe4d4db79f981724f5b
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-05 22:37:34 +0530

    Updated Python version

--- Codechef/PSEUDOSORT/PSEUDOSORT.py
+++ Codechef/PSEUDOSORT/PSEUDOSORT.py
@@ -3,8 +3,11 @@ while T:
     T -= 1
     N = int(input())
     A = list(map(int, input().split()))
-    A1 = A
-    A1.sort()
-    if A1 == A:
+    for i in range(N - 1):
+        if(A[i + 1] < A[i]):
+            A[i + 1], A[i] = A[i], A[i + 1]
+            break
+    if (sorted(A) == A):
         print("YES")
-    
+    else:
+        print("NO")
