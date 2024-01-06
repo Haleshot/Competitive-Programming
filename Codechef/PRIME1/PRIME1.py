87bc5df3aaa9d6fd3d570a973ce62fd21220d743
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-06 21:24:42 +0530

    Updated Python version

--- Codechef/PRIME1/PRIME1.py
+++ Codechef/PRIME1/PRIME1.py
@@ -1,5 +1,17 @@
-T = int(input())
-while T:
-    T -= 1
-    M, N = map(int, input().split())
-    
+def prime(n):
+    if n < 2:
+        return False
+    elif n == 2 or n == 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    for i in range(5, int(n ** 0.5) + 1, 6):
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+    return True
+
+for _ in range(int(input())):
+    N1, N2 = list(map(int, input().split()))
+    for i in range(N1, N2 + 1):
+        if prime(i):
+            print(i)
