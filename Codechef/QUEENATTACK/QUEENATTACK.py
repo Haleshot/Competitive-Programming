1d7c4c466a911793a882163fc4eaed291b35b658
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-09 21:21:44 +0530

    Updated Python version

--- Codechef/QUEENATTACK/QUEENATTACK.py
+++ Codechef/QUEENATTACK/QUEENATTACK.py
@@ -5,5 +5,4 @@ while T:
     cells_in_row_column = (N - 1) * 2
     cells_in_diagonals = min(N - X, N - Y) + min(X - 1, Y - 1) + min(N - X, Y - 1) + min(X - 1, N - Y)
     cells_under_attack = cells_in_row_column + cells_in_diagonals
-
     print(cells_under_attack)
