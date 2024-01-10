18975e2b5cb19eb791c934a6b1f93e438cee5ddf
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-10 20:55:43 +0530

    Updated Python version

--- Codechef/COVID19/COVID19.py
+++ Codechef/COVID19/COVID19.py
@@ -2,4 +2,8 @@ T = int(input())
 while T:
     T -= 1
     N, M = map(int, input().split())
-    
+    c = 0
+    for i in range(1, N + 1, 2):
+        for j in range(1, M + 1, 2):
+            c += 1
+    print(c)
