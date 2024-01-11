bbf66de0f727913208bd0521c52959a728b5763e
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-11 20:56:37 +0530

    Updated Python version

--- Codechef/DIGSMPAR/DIGSMPAR.py
+++ Codechef/DIGSMPAR/DIGSMPAR.py
@@ -2,4 +2,11 @@ T = int(input())
 while T:
     T -= 1
     N = int(input())
-    
+    res1 = list(map(int, str(N)))
+    res2 = list(map(int, str(N + 1)))
+    s1 = sum(res1)
+    s2 = sum(res2)
+    if s1 % 2 == 0 and s2 % 2 == 0:
+        print(N + 2)
+    else:
+        print(N + 1)
