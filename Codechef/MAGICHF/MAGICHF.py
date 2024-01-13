5ec3a6cc9c87afb11c2c22a4f69244707de365bf
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-13 22:20:24 +0530

    Updated Python version

--- Codechef/MAGICHF/MAGICHF.py
+++ Codechef/MAGICHF/MAGICHF.py
@@ -2,7 +2,11 @@ T = int(input())
 while T:
     T -= 1
     N, X, S = map(int, input().split())
+    new_x = X
     for i in range(S):
-        new_x = 0
         A, B = map(int, input().split())
-        
+        if new_x == A:
+            new_x = B
+        elif new_x == B:
+            new_x = A
+    print(new_x)
