5f6cdf917e0e39554c3e8aed13cfd8cdd139a526
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-13 21:54:35 +0530

    Updated Python version

--- Codechef/MAGICHF/MAGICHF.py
+++ Codechef/MAGICHF/MAGICHF.py
@@ -2,5 +2,7 @@ T = int(input())
 while T:
     T -= 1
     N, X, S = map(int, input().split())
-    A, B = map(int, input().split())
-    
+    for i in range(S):
+        new_x = 0
+        A, B = map(int, input().split())
+        
