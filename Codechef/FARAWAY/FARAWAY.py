bac29e282efb25d90777121b7007bc6d5427aa8f
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-14 21:03:42 +0530

    Updated Python version

--- Codechef/FARAWAY/FARAWAY.py
+++ Codechef/FARAWAY/FARAWAY.py
@@ -3,4 +3,12 @@ while T:
     T -= 1
     N, M = map(int, input().split())
     A = list(map(int, input().split()))
-    
+    summation, maximum = 0, 0
+    for i in range(N):
+        if A[i] > M // 2:
+            maximum = abs(A[i] - 1)
+        else:
+            maximum = abs(A[i] - M)
+        summation = summation + maximum
+        maximum = 0
+    print(summation)
