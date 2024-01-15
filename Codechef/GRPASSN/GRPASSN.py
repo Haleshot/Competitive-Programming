762923d5a91d0ad1b8d96b6e91406a7d89ebac6d
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-15 21:45:10 +0530

    Updated Python version

--- Codechef/GRPASSN/GRPASSN.py
+++ Codechef/GRPASSN/GRPASSN.py
@@ -3,4 +3,11 @@ while T:
     T -= 1
     N = int(input())
     P = list(map(int, input().split()))
-    
+    c = 0
+    for i in range(N):
+        if P[i] != P.count(P[i]):
+            c += 1
+    if c > 0:
+        print("NO")
+    else:
+        print("YES")
