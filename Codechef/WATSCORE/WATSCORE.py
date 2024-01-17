627bf1b49855594e1cad7d42c8deb732a3da1cf4
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-17 22:49:39 +0530

    Updated Python version

--- Codechef/WATSCORE/WATSCORE.py
+++ Codechef/WATSCORE/WATSCORE.py
@@ -1,4 +1,10 @@
 T = int(input())
 while T:
     T -= 1
-    
+    N = int(input())
+    S = [0] * 8
+    for j in range(N):
+        X, Y = map(int, input().split())
+        if X <= 8:
+            S[X - 1] = max(S[S - 1], Y)
+    print(sum(S))
