1f2ed3cf194da508a7e089f5e0742854947a3e25
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-18 20:15:17 +0530

    Updated Python version

--- Codechef/CHEFSTR1/CHEFSTR1.py
+++ Codechef/CHEFSTR1/CHEFSTR1.py
@@ -1,4 +1,9 @@
 T = int(input())
 while T:
     T -= 1
-    
+    N = int(input())
+    A = list(map(int,input().split()))
+    c = 0
+    for i in range(N - 1):
+        c += (abs(A[i + 1] - A[i]) - 1)
+    print(c)
