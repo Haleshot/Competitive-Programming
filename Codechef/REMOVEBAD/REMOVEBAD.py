d6eca308ae10780c1aff387d3fe073a9c026896a
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-20 22:58:02 +0530

    Updated Python version

--- Codechef/REMOVEBAD/REMOVEBAD.py
+++ Codechef/REMOVEBAD/REMOVEBAD.py
@@ -1,6 +1,10 @@
+from collections import Counter
 T = int(input())
 while T:
     T -= 1
     N = int(input())
     A = list(map(int, input().split()))
-    
+    C = Counter(A)
+    for A, B in C.most_common(1):
+        M = B
+    print(N - M)
