da7d7993519749d071385c34cf1262d43617b85c
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-21 22:56:33 +0530

    Updated Python version

--- Codechef/MAKEMONEY/MAKEMONEY.py
+++ Codechef/MAKEMONEY/MAKEMONEY.py
@@ -3,4 +3,9 @@ while T:
     T -= 1
     N, X, C = map(int, input().split())
     A = list(map(int, input().split()))
-    
+    c = 0
+    for i in range(N):
+        if A[i] < X and (X - A[i] > C):
+            A[i] = X
+            c += 1
+    print(sum(A) - (c * C))
