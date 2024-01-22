74edf49d913d83e00e865f16431199828a5696bf
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-22 20:47:42 +0530

    Updated Python version

--- Codechef/MSNSADM1/MSNSADM1.py
+++ Codechef/MSNSADM1/MSNSADM1.py
@@ -3,4 +3,10 @@ while T:
     T -= 1
     N = int(input())
     A = list(map(int, input().split()))
-    
+    B = list(map(int, input().split()))
+    sum = [] * N
+    for i in range(N):
+        sum.append((20 * A[i]) - (10 * B[i]))
+        if (sum[i] < 0):
+            sum[i] = 0
+    print(max(sum))
