c3dc897e09f2ef33736692905157a41be459b3b0
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-23 23:26:50 +0530

    Updated Python version

--- Codechef/ABSTRING/ABSTRING.py
+++ Codechef/ABSTRING/ABSTRING.py
@@ -1,6 +1,15 @@
+import collections
 T  = int(input())
 while T:
     T -= 1
     N = int(input())
     S = input()
-    
+    c = 0
+    freq = collections.Counter(S)
+    for i in freq.values():
+        if i == 1:
+            c += 1
+    if c > 0:
+        print("NO")
+    else:
+        print("YES")
