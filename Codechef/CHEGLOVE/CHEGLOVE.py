ce077e9f7a8215853aac14e215f795c0a65a98ae
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-25 21:37:23 +0530

    Updated Python version

--- Codechef/CHEGLOVE/CHEGLOVE.py
+++ Codechef/CHEGLOVE/CHEGLOVE.py
@@ -4,3 +4,17 @@ while T:
     N = int(input())
     L = list(map(int, input().split()))
     G = list(map(int, input().split()))
+    f, b = True, True
+    for i in range(N):
+        if L[i] > G[i]:
+            f = False
+        if L[i] > G[N - i - 1]:
+            b = False
+    if (f and b):
+        print("both")
+    elif (f):
+        print("front")
+    elif (b):
+        print("back")
+    else:
+        print("none")
