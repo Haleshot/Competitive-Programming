f821cda2f1522a96795670e273d29dfb932346d9
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-01-27 20:51:28 +0530

    Updated Python version

--- Codechef/LAPTOPREC/LAPTOPREC.py
+++ Codechef/LAPTOPREC/LAPTOPREC.py
@@ -1,6 +1,21 @@
-T = int(input())
-while T:
-    T -= 1
+from collections import Counter
+for _ in range(int(input())):
     N = int(input())
     A = list(map(int, input().split()))
-    
+    freq = Counter(A)
+    most_rec_value = max(freq.values())
+    end = []
+    for values in freq.values():
+        end.append(values)
+    end.sort(reverse=True)
+    if end[0] == end[1] and end[0] == most_rec_value:
+        print("CONFUSED")
+    else:
+        # temp1 = freq.keys()
+        # temp2 = freq.values()
+        # temp = list(temp2).index(most_rec_value)
+        # print(temp1[temp])
+        value = [i for i in freq if freq[i] == most_rec_value]
+        print(str(value)[1:-1])
+    # print(end)
+    # print(end[0][1])
