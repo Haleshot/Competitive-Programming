710dade56ac7529f87b7326d1ca522c4bf9524ac
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-02-05 21:47:07 +0530

    Updated Python version

--- Codechef/PLAYSTR/PLAYSTR.py
+++ Codechef/PLAYSTR/PLAYSTR.py
@@ -2,6 +2,11 @@ T = int(input())
 while T:
     T -= 1
     N = int(input())
-    S = input()
-    R = input()
-    
+    S = list(input())
+    R = list(input())
+    c = 0
+    if S == R:
+        print("NO")
+    else:
+        # one_count = list(S).count("1")
+        # print(one_count)
