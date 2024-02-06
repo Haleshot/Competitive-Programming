6c9613f896bb62b0e34561fa443c9d7c591ed70c
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-02-06 15:37:17 +0530

    Updated Python version

--- Codechef/PLAYSTR/PLAYSTR.py
+++ Codechef/PLAYSTR/PLAYSTR.py
@@ -4,7 +4,7 @@ while T:
     N = int(input())
     S = list(input())
     R = list(input())
-    if ((S.count(1) == R.count(1)) and (S.count(0) == R.count(0))):
+    if ((S.count('1') == R.count('1')) and (S.count('0') == R.count('0'))):
         print("YES")
     else:
         print("NO")
