2811df1f6b9c3fa419e262a5805554d5293d610f
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-02-08 18:55:21 +0530

    Updated Python version

--- Codechef/CHEFSTEP/CHEFSTEP.py
+++ Codechef/CHEFSTEP/CHEFSTEP.py
@@ -1,6 +1,11 @@
 T = int(input())
 while T:
     T -= 1
-    N = int(input())
+    N, K = map(int, input().split())
     D = list(map(int, input().split()))
-    
+    for i in D:
+        if int(i) % K == 0:
+            print(1, end='')
+        else:
+            print(0, end='')
+    print()
