6e0412ea3eff9c61faea831cd531c49f2dc04070
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-02-13 21:18:30 +0530

    Updated Python version

--- Codechef/VOTERS/VOTERS.py
+++ Codechef/VOTERS/VOTERS.py
@@ -10,24 +10,20 @@ for i in range(N2):
 for i in range(N3):
     N3_vot.append(int(input()))
 
-cou = 0
 final_l = []
 for i in range(N1):
     c = N1_vot[i]
     if c in N2_vot or c in N3_vot:
-        # cou += 1
         final_l.append(c)
 
 for i in range(N2):
     c = N2_vot[i]
     if c in N1_vot or c in N3_vot:
-        # cou += 1
         final_l.append(c)
 
 for i in range(N3):
     c = N3_vot[i]
     if c in N1_vot or c in N2_vot:
-        # cou += 1
         final_l.append(c)
 print(len(set(final_l)))
 # print([i, "\n" for i in set(final_l)])
