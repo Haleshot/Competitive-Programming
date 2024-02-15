d8a3a3d6c59434d6e5cb0cf79416a660d1c9a2e4
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-02-15 20:39:21 +0530

    Updated Python version

--- Codechef/ALTSTR/ALTSTR.py
+++ Codechef/ALTSTR/ALTSTR.py
@@ -1,4 +1,14 @@
 for _ in range(int(input())):
     N = int(input())
-    S = input()
-    
+    S = list(input())
+    z_c = S.count("0")
+    o_c = S.count("1")
+    if z_c == 1 or o_c == 1:
+        print(1)
+    else:
+        d = [] * 100
+        s = 2
+        for i in range(2, 100):
+            d[i] = [S]
+            s += 2
+        print(d)
