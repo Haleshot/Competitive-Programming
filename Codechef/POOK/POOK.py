7ec4bdc1884c4bbcd8d6d386019cf2216fa4f7d8
Haleshot <57552973+Haleshot@users.noreply.github.com>
2024-02-20 21:09:58 +0530

    Updated Python version

--- Codechef/POOK/POOK.py
+++ Codechef/POOK/POOK.py
@@ -1,5 +1,10 @@
-for _ in range(int(input())):
-    N = int(input())
+def pookQueens(N):
     L = [ [0] ] * N
     # print(L)
     
+    return _
+
+
+for _ in range(int(input())):
+    N = int(input())
+    print(pookQueens(N))
