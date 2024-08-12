5bc732980a0ced1b2a5ae0bfc70a003f5a5fe55f
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-12 21:30:09 +0530

    Update logic

--- Leetcode/Find_Closest_Number_to_Zero/Find_Closest_Number_to_Zero.py
+++ Leetcode/Find_Closest_Number_to_Zero/Find_Closest_Number_to_Zero.py
@@ -4,4 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+        nums.sort()
+        pos, neg = [i for i in nums if i > 0], [i for i in nums if i < 0]
+        min_pos, max_neg = min(pos), max(neg)
+        print(min_pos, "\n", max_neg)
+        return max(min_pos, max_neg)
