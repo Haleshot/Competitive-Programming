1328a0730a23f17df03be78ba29944e05b913e2b
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-12 22:06:24 +0530

    Update logic

--- Leetcode/Find_Closest_Number_to_Zero/Find_Closest_Number_to_Zero.py
+++ Leetcode/Find_Closest_Number_to_Zero/Find_Closest_Number_to_Zero.py
@@ -11,24 +11,18 @@ class Solution(object):
             return 0
         else:
             pos, neg = [i for i in nums if i > 0], [i for i in nums if i < 0]
-            # if len(pos) != 0 and len(neg) != 0:
-            #     min_pos, max_neg = min(pos), max(neg)
-            #     # print(min_pos, "\n", max_neg)
-            #     if min_pos == abs(max_neg):
-            #         return min_pos
-            #     else:
-            #         diff1, diff2 = abs(0 - min_pos), abs(0 - max_neg)
-            #         return min(diff1, diff2)
             if len(pos) != 0 and len(neg) == 0:
-                print(min(pos))
+                # print(min(pos))
                 return min(pos)
             elif len(pos) == 0 and len(neg) != 0:
-                print(max(neg))
+                # print(max(neg))
                 return max(neg)
             else:
                 min_pos, max_neg = min(pos), max(neg)
                 if min_pos == abs(max_neg):
                     return min_pos
                 else:
-                    diff1, diff2 = abs(0 - min_pos), abs(0 - max_neg)
-                    return min(diff1, diff2)
+                    diff1, diff2 = min_pos, 0 - max_neg
+                    if diff1 < diff2:
+                        return min_pos
+                    return max_neg
