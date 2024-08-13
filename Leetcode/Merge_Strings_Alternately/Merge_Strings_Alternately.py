4a29e7e95548fc89d0e8d23bcb9ea0f3b330a34b
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-13 21:52:02 +0530

    Update logic

--- Leetcode/Merge_Strings_Alternately/Merge_Strings_Alternately.py
+++ Leetcode/Merge_Strings_Alternately/Merge_Strings_Alternately.py
@@ -5,4 +5,13 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
+        result = []
+        i = 0
+        while i < len(word1) or i < len(word2):
+            if i < len(word1):
+                result.append(word1[i])
+            if i < len(word2):
+                result.append(word2[i])
+            i += 1
+        return ''.join(result)
         
