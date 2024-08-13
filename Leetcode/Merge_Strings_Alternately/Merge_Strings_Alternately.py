8e81e9f95c291cd1a70a2120f7fd28fdbe581d8a
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-13 21:52:26 +0530

    Update logic

--- Leetcode/Merge_Strings_Alternately/Merge_Strings_Alternately.py
+++ Leetcode/Merge_Strings_Alternately/Merge_Strings_Alternately.py
@@ -5,13 +5,18 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        result = []
-        i = 0
-        while i < len(word1) or i < len(word2):
-            if i < len(word1):
-                result.append(word1[i])
-            if i < len(word2):
-                result.append(word2[i])
-            i += 1
-        return ''.join(result)
-        
+        mergedword = ""
+        if len(word1) < len(word2):
+            for i in range(len(word1)):
+                mergedword += word1[i] + word2[i]
+            mergedword += word2[len(word1):len(word2)]
+            return mergedword
+        elif len(word1) > len(word2):
+            for i in range(len(word2)):
+                mergedword += word1[i] + word2[i]
+            mergedword += word1[len(word2):len(word1)]
+            return mergedword
+        else:
+            for i in range(len(word2)):
+                mergedword += word1[i] + word2[i]
+            return mergedword
