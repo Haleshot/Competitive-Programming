6e97bea9a1cee2148407bb0b6f43d1d8d07b76a0
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-15 00:12:02 +0530

    init commit

--- Algomap/roman_to_integer/roman_to_integer.py
+++ Algomap/roman_to_integer/roman_to_integer.py
@@ -4,4 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        d = {
+            "I" : 1,
+            "V" : 5,
+            "X" : 10,
+            'L' : 50,
+            "C" : 100,
+            'D' : 500,
+            "M":  1000
+            }
+        print(d.keys(), "\n", d.values())
+        sum = 0
         
+        for i in s:
+            sum += d[i]
+        return sum
