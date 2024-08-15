e227c96538fb005a43ff417f00a0c95cc4c44d3b
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-15 20:09:37 +0530

    Update logic

--- Algomap/roman_to_integer/roman_to_integer.py
+++ Algomap/roman_to_integer/roman_to_integer.py
@@ -14,14 +14,14 @@ class Solution(object):
             "M":  1000
             }
         # print(d.keys(), "\n", d.values())
-        sum = 0
+        summ = 0
         i = 0
         while i < len(s):
             if i < len(s) - 1 and d[s[i]] < d[s[i + 1]]:
-                sum += d[s[i + 1]] - d[s[i]]
+                summ += d[s[i + 1]] - d[s[i]]
                 i += 2
             else:
-                sum += d[s[i]]
+                summ += d[s[i]]
                 i += 1
-        return sum
+        return summ
     
