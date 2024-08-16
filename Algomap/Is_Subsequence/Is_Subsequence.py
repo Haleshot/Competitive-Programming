eba02caa42f4ea4f32e43750b0aae2c7603f82ee
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-16 11:10:53 +0530

    Update logic

--- Algomap/Is_Subsequence/Is_Subsequence.py
+++ Algomap/Is_Subsequence/Is_Subsequence.py
@@ -5,4 +5,27 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        
+        # Step 1
+        if not s:
+            return True
+
+        i, j = 0, 0
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+            
+        return i == len(s)
+    
+        # # Step 2:
+        # if s == "": return True
+        # if len(s) > len(t): return False
+
+        # j = 0
+
+        # for i in range(len(t)):
+        #     if s[j] == t[i]:
+        #         if j == len(s) - 1:
+        #             return True
+        #         j += 1
+        # return False
