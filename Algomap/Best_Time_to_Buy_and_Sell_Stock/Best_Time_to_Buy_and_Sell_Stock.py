af61f22ac2aa168cc347fff9a0012746019dc38e
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-18 16:01:33 +0530

    Update Logic

--- Algomap/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock.py
+++ Algomap/Best_Time_to_Buy_and_Sell_Stock/Best_Time_to_Buy_and_Sell_Stock.py
@@ -4,3 +4,24 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        # # Step 1:
+        # cheapest_choice, maxP = float('inf'), 0
+        # for price in prices:
+        #     if price < cheapest_choice:
+        #         cheapest_choice = price
+        #     profit = price - cheapest_choice
+        #     if profit > maxP:
+        #         maxP = max(profit, maxP)
+        # return maxP
+
+        # # Step 2:
+        left, right = 0, 1
+        maxP = 0
+        while right < len(prices):
+            if prices[left] < prices[right]:
+                profit = prices[right] - prices[left]
+                maxP = max(profit, maxP)
+            else:
+                left = right
+            right += 1
+        return maxP
