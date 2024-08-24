3de546ca1c5f51fb57e3343034b64a9177a348e5
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-24 21:31:12 +0530

    init commit

--- Algomap/Longest_Common_Prefix/Longest_Common_Prefix.py
+++ Algomap/Longest_Common_Prefix/Longest_Common_Prefix.py
@@ -1,6 +1,30 @@
 class Solution(object):
-    def longestCommonPrefix(self, strs):
+    def longestCommonPrefix(self, strs=None):
         """
         :type strs: List[str]
         :rtype: str
         """
+        # for i in strs:
+        #     c = 0
+        #     long_com_pre = ""
+        #     flag = True
+        #     while flag:
+        #         long_com_pre += i[c]
+        #         other = [j for j in strs[strs.index(i):strs]]
+        #         if long_com_pre in others:
+        #             continue
+        #         else:
+        #             flag = False
+        #             break
+        #         c += 1
+        # return long_com_pre
+    
+        log = ["flower", "flight", "flowing", "flight", "floom"]
+        if "f" in log:
+            print("True")
+        else:
+            print("False")
+
+param1 = ["flower","flow","flight"]
+   
+Solution.longestCommonPrefix(param1)
