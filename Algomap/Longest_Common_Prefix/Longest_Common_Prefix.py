5866342f35d96f70acb23a73f330c79b60149cc5
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-24 21:49:10 +0530

    Format using `black`

--- Algomap/Longest_Common_Prefix/Longest_Common_Prefix.py
+++ Algomap/Longest_Common_Prefix/Longest_Common_Prefix.py
@@ -13,6 +13,19 @@ class Solution(object):
                 return ans
             ans += min_len[i]
         return ans
-    
+
         # # Step 2:
-        
+        # min_length = float('inf')
+
+        # for s in strs:
+        #     if len(s) < min_length:
+        #         min_length = len(s)
+
+        # i = 0
+        # while i < min_length:
+        #     for s in strs:
+        #         if s[i] != strs[0][i]:
+        #             return s[:i]
+        #     i += 1
+
+        # return strs[0][:i]
