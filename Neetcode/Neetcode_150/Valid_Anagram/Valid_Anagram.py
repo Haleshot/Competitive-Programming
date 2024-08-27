78c32ebcc04ddf6163ba1e1359ed8e6badcd3f7e
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-27 23:28:24 +0530

    Format using `black`

--- Neetcode/Neetcode_150/Valid_Anagram/Valid_Anagram.py
+++ Neetcode/Neetcode_150/Valid_Anagram/Valid_Anagram.py
@@ -19,3 +19,9 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        countS, countT = dict(), dict()
+        for i in range(len(s)):
+            countS[s[i]] = countS.get(s[i], 0) + 1
+        for i in range(len(t)):
+            countT[t[i]] = countT.get(t[i], 0) + 1
+        return countS == countT
