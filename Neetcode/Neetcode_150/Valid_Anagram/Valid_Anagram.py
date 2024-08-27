353f1019900bd279c92011fc6b517a96f330ef90
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-08-27 23:19:19 +0530

    Update Logic

--- Neetcode/Neetcode_150/Valid_Anagram/Valid_Anagram.py
+++ Neetcode/Neetcode_150/Valid_Anagram/Valid_Anagram.py
@@ -1,4 +1,11 @@
 # Neetcode
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        pass
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = countS.get(s[i], 0) + 1
+            countT[t[i]] = countT.get(t[i], 0) + 1
+        return countS == countT
