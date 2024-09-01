50d78f26dc98d84243a68ea3689b5516b75a0064
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-09-01 11:08:52 +0530

    Add Logic

--- Neetcode/Neetcode_150/Group_Anagrams/Group_Anagrams.py
+++ Neetcode/Neetcode_150/Group_Anagrams/Group_Anagrams.py
@@ -1,4 +1,13 @@
 # Neetcode
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        pass
+        results = []
+        for i in strs:
+            for j in range(strs.index(i) + 1, len(strs)):
+                if sorted(i) == sorted(strs[j]):
+                    results.append([i, strs[j]])
+        for i in results:
+            for j in range(len(i) - 1):
+                if i[j] == i[j + 1]:
+                    print(i[j])
+        return results
