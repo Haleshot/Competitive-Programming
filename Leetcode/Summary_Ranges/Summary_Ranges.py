d76099bde8bf13cfc55b20f8b8ac708269f23471
Srihari Thyagarajan <hari.leo03@gmail.com>
2024-09-21 22:05:56 +0530

    Add Logic

--- Leetcode/Summary_Ranges/Summary_Ranges.py
+++ Leetcode/Summary_Ranges/Summary_Ranges.py
@@ -4,3 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[str]
         """
+        result, i = [], 0
+        while i < len(nums):
+            start = nums[i]
+            while i < len(nums) - 1 and (nums[i] + 1) == nums[i + 1]:
+                i += 1
+            if start != nums[i]:
+                result.append(str(start) + "->" + str(nums[i]))
+            else:
+                result.append(str(nums[i]))
+            i += 1
+        return result
